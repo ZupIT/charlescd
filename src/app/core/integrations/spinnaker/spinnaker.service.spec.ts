c4198324e8db9ac6fa0793dbac2f8eed1d890a48
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-06 20:33:07 -0300

    Removing semicolons

--- src/app/core/integrations/spinnaker/spinnaker.service.spec.ts
+++ src/app/core/integrations/spinnaker/spinnaker.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SpinnakerService } from './spinnaker.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { SpinnakerService } from './spinnaker.service'
 
 describe('SpinnakerService', () => {
-  let service: SpinnakerService;
+  let service: SpinnakerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SpinnakerService],
-    }).compile();
+    }).compile()
 
-    service = module.get<SpinnakerService>(SpinnakerService);
-  });
+    service = module.get<SpinnakerService>(SpinnakerService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
