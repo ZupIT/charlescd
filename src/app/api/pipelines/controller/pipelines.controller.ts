e9277f566c4c8bf4aa8132e137507b6db9ad3f05
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-06 15:41:43 -0300

    Organizing code

--- src/app/api/pipelines/controller/pipelines.controller.ts
+++ src/app/api/pipelines/controller/pipelines.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PipelinesService } from '../service/pipelines.service';
 
 @Controller('pipelines')
-export class PipelinesController {}
+export class PipelinesController {
+
+  constructor(private readonly pipelinesService: PipelinesService) {}
+
+  @Get('/')
+  public getPipelines(): string[] {
+    return this.pipelinesService.getPipelines();
+  }
+}
--- src/app/api/pipelines/service/pipelines.service.ts
+++ src/app/api/pipelines/service/pipelines.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PipelinesService {}
+export class PipelinesService {
+
+  public getPipelines(): string[] {
+    return [
+      'pipeline 1',
+      'pipeline 2'
+    ];
+  }
+}
