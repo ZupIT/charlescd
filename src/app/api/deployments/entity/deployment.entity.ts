a8e13e817d322874bbe70107438e3cf245b76acf
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-06 22:47:00 -0300

    TypeORM config

--- src/app/api/deployments/entity/deployment.entity.ts
+++ src/app/api/deployments/entity/deployment.entity.ts
@@ -1,1 +1,22 @@
-export class DeploymentEntity {}
+import { DeploymentModule } from './deployment-module.entity'
+import {
+  Entity,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+
+@Entity('deployments')
+export class Deployment {
+
+  @PrimaryGeneratedColumn('uuid')
+  public id: string
+
+  public modules: DeploymentModule[]
+
+  public authorId: string
+
+  public description: string
+
+  public callbackUrl: string
+
+  public circleHeader: string
+}
