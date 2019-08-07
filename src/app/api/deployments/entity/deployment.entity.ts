e8ba70863337e8d2949ca5d1e326e4d9eb55aaa3
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-07 01:07:20 -0300

    Creating migrations

--- src/app/api/deployments/entity/deployment.entity.ts
+++ src/app/api/deployments/entity/deployment.entity.ts
@@ -1,22 +1,32 @@
 import { DeploymentModule } from './deployment-module.entity'
 import {
   Entity,
-  PrimaryGeneratedColumn
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToMany, Column
 } from 'typeorm'
 
 @Entity('deployments')
-export class Deployment {
+export class Deployment extends BaseEntity {
 
   @PrimaryGeneratedColumn('uuid')
   public id: string
 
+  @OneToMany(
+    type => DeploymentModule,
+    deploymentModule => deploymentModule.deployment
+  )
   public modules: DeploymentModule[]
 
+  @Column({ name: 'user_id' })
   public authorId: string
 
+  @Column({ name: 'description'} )
   public description: string
 
+  @Column({ name: 'callback_url'} )
   public callbackUrl: string
 
+  @Column({ name: 'circle_header'} )
   public circleHeader: string
 }
