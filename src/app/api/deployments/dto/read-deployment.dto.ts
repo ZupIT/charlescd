33e9c0826b37a01a1679f9920872500080909fe7
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-08 18:38:20 -0300

    Creating read DTOs

--- src/app/api/deployments/dto/read-deployment.dto.ts
+++ src/app/api/deployments/dto/read-deployment.dto.ts
@@ -1,28 +1,29 @@
-import { DeploymentModuleResponse } from '../interface'
+import { ReadModuleDeploymentDto } from './read-module-deployment.dto'
+import { ReadCircleDeploymentDto } from './read-circle-deployment.dto'
 
 export class ReadDeploymentDto {
 
   public readonly id: string
 
-  public readonly modules: DeploymentModuleResponse[]
+  public readonly modules: ReadModuleDeploymentDto[]
 
   public readonly authorId: string
 
   public readonly description: string
 
-  public readonly circleHeader: string
+  public readonly circles: ReadCircleDeploymentDto
 
   constructor(
     id: string,
-    modules: DeploymentModuleResponse[],
+    modules: ReadModuleDeploymentDto[],
     authorId: string,
     description: string,
-    circleHeader: string
+    circles: ReadCircleDeploymentDto[]
   ) {
     this.id = id
     this.modules = modules
     this.authorId = authorId
     this.description = description
-    this.circleHeader = circleHeader
+    this.circles = circles
   }
 }
