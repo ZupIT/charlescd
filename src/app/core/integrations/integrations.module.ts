a255849a0fa777bc7cd7d3a54b8f3da15bb83791
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-09 17:03:07 -0300

    Spinnaker deployment

--- src/app/core/integrations/integrations.module.ts
+++ src/app/core/integrations/integrations.module.ts
@@ -1,9 +1,16 @@
 import { HttpModule, Module } from '@nestjs/common'
 import { SpinnakerService } from './spinnaker'
+import { DeploymentConfigurationService } from './configuration'
 
 @Module({
   imports: [HttpModule],
-  providers: [SpinnakerService],
-  exports: [SpinnakerService]
+  providers: [
+    SpinnakerService,
+    DeploymentConfigurationService
+  ],
+  exports: [
+    SpinnakerService,
+    DeploymentConfigurationService
+  ]
 })
 export class IntegrationsModule {}
