48db592465341f620e15aaf3e23dd377eac4ca61
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-10 17:07:53 -0300

    database environment variables

--- src/app/core/databases/databases.config.ts
+++ src/app/core/databases/databases.config.ts
@@ -2,11 +2,11 @@ import { ConnectionOptions } from 'typeorm'
 
 const postgresqlOptions: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST || 'localhost',
   port: 5432,
-  username: 'darwin',
-  password: 'darwin',
-  database: 'darwin',
+  username: process.env.POSTGRES_USER || 'darwin',
+  password: process.env.POSTGRES_PASS || 'darwin',
+  database: process.env.POSTGRES_DB_NAME || 'darwin',
   entities: ['src/**/*.entity.ts'],
   migrationsTableName: 'darwin-deploy-migrations',
   migrations: ['src/app/resources/migrations/*.ts'],
