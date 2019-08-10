262bd3346c1a09d2d6b4cc15a5dc0efa42d35078
AdauT_0 <aragorn1308@gmail.com>
2019-08-10 17:59:55 -0300

    Master merge conflicts resolution

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,17 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app/app.module'
-import { ValidationPipe } from '@nestjs/common'
-
-<<<<<<< HEAD
-async function consulBootstrap() {
-  const config = { baseUrl: `${process.env.CONSUL}/v1` };
-
-  await consul(config).kv.get(`config/${process.env.MODULE}/data`, async (err, item) => {
-    if (err) {
-      console.log('Fail to load consul, verify the consul server.')
-      process.exit(1)
-    }
-
-    if (!item) {
-      console.log('Fail to load config from url.')
-      process.exit(1)
-    }
-
-    const consulEnvs = item.Value
-  })
-}
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from './app/app.module'
+import {ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
-  
     const app = await NestFactory.create(AppModule)
 
     app.useGlobalPipes(
-      new ValidationPipe({
-        transform: true
-      })
+        new ValidationPipe({
+            transform: true
+        })
     )
 
     await app.listen(3000)
-=======
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true
-    })
-  )
->>>>>>> master
-
-  await app.listen(3000)
 }
 
-consulBootstrap()
 bootstrap()
