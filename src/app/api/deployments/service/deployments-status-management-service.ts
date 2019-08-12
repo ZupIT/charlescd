a8f429d875f0187405a35b6cca4cf3ad1085a5c1
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-12 11:40:03 -0300

    Set status as FAILED when deployment request fails

--- src/app/api/deployments/service/deployments-status-management-service.ts
+++ src/app/api/deployments/service/deployments-status-management-service.ts
@@ -14,29 +14,29 @@ export class DeploymentsStatusManagementService {
         private readonly moduleDeploymentRepository: Repository<ModuleDeploymentEntity>,
         @InjectRepository(ComponentDeploymentEntity)
         private readonly componentDeploymentRepository: Repository<ComponentDeploymentEntity>
-    ){}
+    ) {}
 
     public async deepUpdateDeploymentStatusByDeploymentId(deploymentId: string, status: DeploymentStatusEnum) {
-        const deployment: DeploymentEntity = 
+        const deployment: DeploymentEntity =
             await this.deploymentsRepository.findOne({
                 where: { id: deploymentId },
                 relations: ['modules']
             })
 
-        await this.deploymentsRepository.update(deployment.id, {status: status})
+        await this.deploymentsRepository.update(deployment.id, { status })
         return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
     }
 
-    public async deepUpdateDeploymentStatus(deployment: DeploymentEntity, status: DeploymentStatusEnum) {            
-        await this.deploymentsRepository.update(deployment.id, {status: status})
+    public async deepUpdateDeploymentStatus(deployment: DeploymentEntity, status: DeploymentStatusEnum) {
+        await this.deploymentsRepository.update(deployment.id, { status })
         return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
     }
-    
+
     public async deepUpdateModuleStatus(module: ModuleDeploymentEntity, status: DeploymentStatusEnum) {
-        await this.moduleDeploymentRepository.update(module.id, {status: status})
+        await this.moduleDeploymentRepository.update(module.id, { status })
         return Promise.all(
-            module.components.map(c => 
-            this.componentDeploymentRepository.update(c.id, {status: status})))
+            module.components.map(c =>
+            this.componentDeploymentRepository.update(c.id, { status })))
     }
 
 }
--- src/app/api/deployments/service/index.ts
+++ src/app/api/deployments/service/index.ts
@@ -1,5 +1,7 @@
 import { DeploymentsService } from './deployments.service'
+import { DeploymentsStatusManagementService } from './deployments-status-management-service'
 
 export {
-  DeploymentsService
+  DeploymentsService,
+  DeploymentsStatusManagementService
 }
