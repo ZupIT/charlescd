b7609d9d1a21da7ff447185a9b9f7aa8153f1ee2
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-26 21:26:39 -0300

    Adding create date and moving the queue on webhook success

--- src/app/api/deployments/repository/queued-deployments.repository.ts
+++ src/app/api/deployments/repository/queued-deployments.repository.ts
@@ -5,6 +5,15 @@ import { QueuedDeploymentStatusEnum } from '../enums'
 @EntityRepository(QueuedDeploymentEntity)
 export class QueuedDeploymentsRepository extends Repository<QueuedDeploymentEntity> {
 
+  public async getAllByComponentIdQueuedAscending(componentId: string): Promise<QueuedDeploymentEntity[]> {
+    return this.createQueryBuilder('queued_deployment')
+      .where(
+        'queued_deployment.component_id = :componentId AND queued_deployment.status = :status',
+        { componentId, status: QueuedDeploymentStatusEnum.QUEUED })
+      .orderBy('queued_deployment.id', 'ASC')
+      .getMany()
+  }
+
   public async getAllByComponentIdAscending(componentId: string): Promise<QueuedDeploymentEntity[]> {
     return this.createQueryBuilder('queued_deployment')
       .where('queued_deployment.component_id = :componentId', { componentId })
--- src/app/core/services/deployments-status-management-service.ts
+++ src/app/core/services/deployments-status-management-service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import {
-  ModuleDeploymentEntity,
-  DeploymentEntity,
-  ComponentDeploymentEntity
-} from '../../api/deployments/entity'
+import { ComponentDeploymentEntity, DeploymentEntity, ModuleDeploymentEntity } from '../../api/deployments/entity'
 import { DeploymentStatusEnum } from '../../api/deployments/enums'
 
 @Injectable()
@@ -21,25 +17,119 @@ export class DeploymentsStatusManagementService {
     ) {}
 
     public async deepUpdateDeploymentStatusByDeploymentId(deploymentId: string, status: DeploymentStatusEnum) {
-        const deployment: DeploymentEntity =
-            await this.deploymentsRepository.findOne({
-                where: { id: deploymentId },
-                relations: ['modules']
-            })
+      const deployment: DeploymentEntity =
+          await this.deploymentsRepository.findOne({
+              where: { id: deploymentId },
+              relations: ['modules']
+          })
 
-        await this.deploymentsRepository.update(deployment.id, { status })
-        return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
+      await this.deploymentsRepository.update(deployment.id, { status })
+      return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
     }
 
     public async deepUpdateDeploymentStatus(deployment: DeploymentEntity, status: DeploymentStatusEnum) {
-        await this.deploymentsRepository.update(deployment.id, { status })
-        return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
+      await this.deploymentsRepository.update(deployment.id, { status })
+      return Promise.all(deployment.modules.map(m => this.deepUpdateModuleStatus(m, status)))
     }
 
     public async deepUpdateModuleStatus(module: ModuleDeploymentEntity, status: DeploymentStatusEnum) {
-        await this.moduleDeploymentRepository.update(module.id, { status })
-        return Promise.all(
-            module.components.map(c =>
-            this.componentDeploymentRepository.update(c.id, { status })))
+      await this.moduleDeploymentRepository.update(module.id, { status })
+      return Promise.all(
+          module.components.map(c =>
+          this.componentDeploymentRepository.update(c.id, { status })))
+    }
+
+    private getDeploymentFinishedModules(
+      deployment: DeploymentEntity
+    ): ModuleDeploymentEntity[] {
+
+      return deployment.modules.filter(
+        moduleDeployment => moduleDeployment.status === DeploymentStatusEnum.FINISHED
+      )
+    }
+
+    private getModuleFinishedComponents(
+      moduleDeployment: ModuleDeploymentEntity
+    ): ComponentDeploymentEntity[] {
+
+      return moduleDeployment.components.filter(
+        componentDeployment => componentDeployment.status === DeploymentStatusEnum.FINISHED
+      )
+    }
+
+    private async updateDeploymentStatus(
+      deploymentId: string,
+      status: DeploymentStatusEnum
+    ): Promise<void> {
+
+      await this.deploymentsRepository.update(
+        { id: deploymentId },
+        { status }
+      )
+    }
+
+    private async propagageSuccessStatusChangeToDeployment(
+      deployment: DeploymentEntity
+    ): Promise<void> {
+
+      const finishedModules: ModuleDeploymentEntity[] =
+        this.getDeploymentFinishedModules(deployment)
+
+      if (finishedModules.length === deployment.modules.length) {
+        await this.updateDeploymentStatus(deployment.id, DeploymentStatusEnum.FINISHED)
+      }
+    }
+
+    private async updateModuleDeploymentStatus(
+      moduleDeploymentId: string,
+      status: DeploymentStatusEnum
+    ): Promise<void> {
+
+      await this.moduleDeploymentRepository.update(
+        { id: moduleDeploymentId },
+        { status }
+      )
+    }
+
+    private async propagateSuccessStatusChangeToModule(
+      moduleDeployment: ModuleDeploymentEntity
+    ): Promise<void> {
+
+      const finishedComponents: ComponentDeploymentEntity[] =
+        this.getModuleFinishedComponents(moduleDeployment)
+
+      if (finishedComponents.length === moduleDeployment.components.length) {
+        await this.updateModuleDeploymentStatus(moduleDeployment.id, DeploymentStatusEnum.FINISHED)
+      }
+    }
+
+    private async propagateSuccessStatusChange(
+      componentDeploymentEntity: ComponentDeploymentEntity
+    ): Promise<void> {
+
+      await this.propagateSuccessStatusChangeToModule(componentDeploymentEntity.moduleDeployment)
+      await this.propagageSuccessStatusChangeToDeployment(componentDeploymentEntity.moduleDeployment.deployment)
+    }
+
+    private async updateComponentDeploymentStatus(
+      componentDeploymentId: string,
+      status: DeploymentStatusEnum
+    ): Promise<void> {
+
+      await this.componentDeploymentRepository.update(
+        { id: componentDeploymentId },
+        { status }
+      )
+    }
+
+    public async setComponentDeploymentStatusAsFinished(componentDeploymentId: string): Promise<void> {
+      const componentDeploymentEntity: ComponentDeploymentEntity =
+        await this.componentDeploymentRepository.findOne({
+          where: { id: componentDeploymentId },
+          relations: ['moduleDeployment', 'moduleDeployment.deployment']
+        })
+
+      await this.updateComponentDeploymentStatus(componentDeploymentId, DeploymentStatusEnum.FINISHED)
+      await this.propagateSuccessStatusChange(componentDeploymentEntity)
     }
 }
