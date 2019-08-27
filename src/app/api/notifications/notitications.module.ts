9a8e7683030b2d14e9cab4f91c5b8f700efa91a9
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-26 23:05:18 -0300

    Fixing imports

--- src/app/api/notifications/notitications.module.ts
+++ src/app/api/notifications/notitications.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common'
 import { NotificationsController } from './controller'
 import { DeploymentsModule } from '../deployments/deployments.module'
+import { NotificationsService } from './services'
+import { IntegrationsModule } from '../../core/integrations/integrations.module'
+import { LogsModule } from '../../core/logs/logs.module'
+import { ServicesModule } from '../../core/services/services.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ComponentDeploymentEntity, DeploymentEntity } from '../deployments/entity'
 
 @Module({
   imports: [
-    DeploymentsModule
+    DeploymentsModule,
+    IntegrationsModule,
+    LogsModule,
+    ServicesModule,
+    TypeOrmModule.forFeature([
+      ComponentDeploymentEntity,
+      DeploymentEntity
+    ])
   ],
-  controllers: [NotificationsController]
+  controllers: [NotificationsController],
+  providers: [NotificationsService]
 })
 export class NotificationsModule {}
