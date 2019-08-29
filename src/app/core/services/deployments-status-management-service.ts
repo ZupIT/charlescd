26aa0966943a0d90e2e93628db48116bbebcc456
Lucas Borges <lucasbfernandes94@gmail.com>
2019-08-29 20:55:00 -0300

    Querying components

--- src/app/core/services/deployments-status-management-service.ts
+++ src/app/core/services/deployments-status-management-service.ts
@@ -126,7 +126,7 @@ export class DeploymentsStatusManagementService {
       const componentDeploymentEntity: ComponentDeploymentEntity =
         await this.componentDeploymentRepository.findOne({
           where: { id: componentDeploymentId },
-          relations: ['moduleDeployment', 'moduleDeployment.deployment']
+          relations: ['moduleDeployment', 'moduleDeployment.components', 'moduleDeployment.deployment']
         })
 
       await this.updateComponentDeploymentStatus(componentDeploymentId, DeploymentStatusEnum.FINISHED)
