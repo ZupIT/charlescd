e1b6b20671d867898281ff600b315c099d727c6c
Lucas Borges <lucasbfernandes94@gmail.com>
2019-09-25 11:36:08 -0300

    Using constants for application creation on spinnaker

--- src/app/core/constants/deployments/deployments-configuration.constants.ts
+++ src/app/core/constants/deployments/deployments-configuration.constants.ts
@@ -3,10 +3,10 @@ export const DeploymentsConfigurationConstants = {
   DEFAULT_CIRCLE_HEADER_NAME: 'x-circle-id',
   SPINNAKER_URL: 'https://darwin-spinnaker-gate.continuousplatform.com',
   MOOVE_URL: 'https://backend-darwin.apirealwave.io',
-  TEMPLATE_GITHUB_AUTH: '914c22322bf4c53f1111176fbcfa6e9ad24f31ca',
-  TEMPLATE_GITHUB_USER: 'lucassaleszup',
-  TEMPLATE_GITHUB_REPO: 'templates-charts',
-  TEMPLATE_GITHUB_FOLDER: 'demoApp',
   DARWIN_DEPLOY_URL: 'https://darwin-deploy.continuousplatform.com',
-  DARWIN_NOTIFICATION_URL: 'https://darwin-deploy.continuousplatform.com/notifications'
+  DARWIN_NOTIFICATION_URL: 'https://darwin-deploy.continuousplatform.com/notifications',
+  SPINNAKER_CREATE_APPLICATION_JOB_TYPE: 'createApplication',
+  SPINNAKER_CREATE_APPLICATION_DEFAULT_CLOUD: 'kubernetes',
+  SPINNAKER_CREATE_APPLICATION_PORT: 80,
+  SPINNAKER_CREATE_APPLICATION_DEFAULT_EMAIL: 'darwin@zup.com'
 }
