cebde21a7217ab4a09d452f120ffe174e4e5f778
Lucas Borges <lucasbfernandes94@gmail.com>
2019-09-25 16:38:59 -0300

    Changing environment variables

--- src/app/core/constants/deployments/deployments-configuration.constants.ts
+++ src/app/core/constants/deployments/deployments-configuration.constants.ts
@@ -2,11 +2,11 @@ export const DeploymentsConfigurationConstants = {
 
   DEFAULT_CIRCLE_HEADER_NAME: 'x-circle-id',
   SPINNAKER_URL: 'https://darwin-spinnaker-gate.continuousplatform.com',
-  MOOVE_URL: 'https://backend-darwin.apirealwave.io',
+  MOOVE_URL: 'http://darwin-application',
   TEMPLATE_GITHUB_AUTH: '914c22322bf4c53f1111176fbcfa6e9ad24f31ca',
   TEMPLATE_GITHUB_USER: 'lucassaleszup',
   TEMPLATE_GITHUB_REPO: 'templates-charts',
   TEMPLATE_GITHUB_FOLDER: 'demoApp',
   DARWIN_DEPLOY_URL: 'https://darwin-deploy.continuousplatform.com',
-  DARWIN_NOTIFICATION_URL: 'https://darwin-deploy.continuousplatform.com/notifications'
+  DARWIN_NOTIFICATION_URL: 'https://darwin-api.continuousplatform.com/deploy/notifications'
 }
