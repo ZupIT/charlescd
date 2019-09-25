9aae88dea4025681b7748d7f4cce8047f8c7c252
Lucas Borges <lucasbfernandes94@gmail.com>
2019-09-25 18:43:01 -0300

    Adding port

--- src/app/core/constants/deployments/deployments-configuration.constants.ts
+++ src/app/core/constants/deployments/deployments-configuration.constants.ts
@@ -2,7 +2,7 @@ export const DeploymentsConfigurationConstants = {
 
   DEFAULT_CIRCLE_HEADER_NAME: 'x-circle-id',
   SPINNAKER_URL: 'https://darwin-spinnaker-gate.continuousplatform.com',
-  MOOVE_URL: 'http://darwin-application',
+  MOOVE_URL: 'http://darwin-application:80',
   TEMPLATE_GITHUB_AUTH: '914c22322bf4c53f1111176fbcfa6e9ad24f31ca',
   TEMPLATE_GITHUB_USER: 'lucassaleszup',
   TEMPLATE_GITHUB_REPO: 'templates-charts',
