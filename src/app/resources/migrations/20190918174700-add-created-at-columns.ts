c354905d5e23136521a778ae364f6ab415f65629
Lucas Borges <lucasbfernandes94@gmail.com>
2019-10-01 17:27:15 -0300

    Adding created_at migrations

--- src/app/resources/migrations/20190918174700-add-created-at-columns.ts
+++ src/app/resources/migrations/20190918174700-add-created-at-columns.ts
@@ -5,31 +5,36 @@ export class AddCreatedAtColumns20190918174700 implements MigrationInterface {
   public async up(queryRunner: QueryRunner) {
     await queryRunner.addColumn('deployments', new TableColumn({
       name: 'created_at',
-      type: 'date'
+      type: 'date',
+      default: 'now()'
     }))
 
     await queryRunner.addColumn('module_deployments', new TableColumn({
       name: 'created_at',
-      type: 'date'
+      type: 'date',
+      default: 'now()'
     }))
 
     await queryRunner.addColumn('component_deployments', new TableColumn({
       name: 'created_at',
-      type: 'date'
+      type: 'date',
+      default: 'now()'
     }))
 
     await queryRunner.dropColumn('modules', 'module_id')
 
     await queryRunner.addColumn('modules', new TableColumn({
       name: 'created_at',
-      type: 'date'
+      type: 'date',
+      default: 'now()'
     }))
 
     await queryRunner.dropColumn('components', 'component_id')
 
     await queryRunner.addColumn('components', new TableColumn({
       name: 'created_at',
-      type: 'date'
+      type: 'date',
+      default: 'now()'
     }))
   }
 
