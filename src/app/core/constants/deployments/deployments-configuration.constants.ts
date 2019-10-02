d4c667b950879b480f3c5c289c6ee436a254d453
Lucas Borges <lucasbfernandes94@gmail.com>
2019-10-02 14:36:24 -0300

    Setting lowercase header

--- src/app/core/constants/deployments/deployments-configuration.constants.ts
+++ src/app/core/constants/deployments/deployments-configuration.constants.ts
@@ -1,6 +1,6 @@
 export const DeploymentsConfigurationConstants = {
 
-  DEFAULT_CIRCLE_HEADER_NAME: 'X-Circle-Id',
+  DEFAULT_CIRCLE_HEADER_NAME: 'x-circle-id',
   SPINNAKER_URL: 'https://darwin-spinnaker-gate.continuousplatform.com',
   MOOVE_URL: 'http://darwin-application:8080',
   DARWIN_DEPLOY_URL: 'https://darwin-deploy.continuousplatform.com',
