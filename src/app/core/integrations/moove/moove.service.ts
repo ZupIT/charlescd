eaabf490ab133545074a4cc5d5c72b8d7032ff0e
Lucas Borges <lucasbfernandes94@gmail.com>
2019-10-18 13:01:36 -0300

    Adding stub entries

--- src/app/core/integrations/moove/moove.service.ts
+++ src/app/core/integrations/moove/moove.service.ts
@@ -14,7 +14,13 @@ export class MooveService {
       private readonly consulConfiguration: IConsulKV
     ) {}
 
-    public async notifyDeploymentStatus(deploymentId: string, status: string, callbackUrl: string, circleId: string): Promise<void> {
+    public async notifyDeploymentStatus(
+      deploymentId: string,
+      status: string,
+      callbackUrl: string,
+      circleId: string
+    ): Promise<void> {
+
       try {
         this.consoleLoggerService.log('START:NOTIFY_DEPLOYMENT_STATUS', { deploymentId, status, callbackUrl })
         await this.httpService.post(
