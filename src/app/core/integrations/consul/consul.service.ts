1e109a250592c6351e378b0f5fff27101b836933
Lucas Borges <lucasbfernandes94@gmail.com>
2019-10-18 15:34:46 -0300

    Using stubby4j for consul mocking

--- src/app/core/integrations/consul/consul.service.ts
+++ src/app/core/integrations/consul/consul.service.ts
@@ -8,31 +8,35 @@ export class ConsulService {
 
   public static async getAppConfiguration(): Promise<IConsulKV> {
 
-    return AppConstants.NODE_ENV === 'dev' ?
-      ConsulService.getDefaultConfiguration() :
-      await ConsulService.getKV(AppConstants.CONSUL_KEY_PATH)
+    return await ConsulService.getKV(AppConstants.CONSUL_KEY_PATH)
   }
 
   private static async getKV(key: string): Promise<IConsulKV> {
 
     return new Promise((resolve, reject) => {
       ConsulService.consulConnection.kv.get(key, (err, item) => {
-        err ? reject(err) : resolve(JSON.parse(item.Value))
+        err ?
+          reject(err) :
+          resolve(ConsulService.parseKV(item.Value))
       })
     })
   }
 
-  private static getDefaultConfiguration(): IConsulKV {
-    return AppConstants.CONSUL_DEFAULT_VALUES
+  private static parseKV(value: string | IConsulKV): IConsulKV {
+
+    return typeof value === 'string' ?
+      JSON.parse(value) : value
   }
 
   private static getConnection() {
+
     return consul(
       ConsulService.getConnectionOptions()
     )
   }
 
   private static getConnectionOptions(): IConsulConnectionOptions {
+
     return {
       host: AppConstants.CONSUL_HOST
     }
