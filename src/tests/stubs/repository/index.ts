86b002f095b6f49666d21b9976a655038eaab83b
lucasbfernandes <lucasbfernandes94@gmail.com>
2019-11-13 20:15:10 -0300

    Creating create-undeployment-request usecase test structure

---
+++ src/tests/stubs/repository/index.ts
@@ -0,0 +1,9 @@
+import { DeploymentsRepositoryStub } from './deployments-repository.stub'
+import { QueuedDeploymentsRepositoryStub } from './queued-deployments-repository.stub'
+import { UndeploymentsRepositoryStub } from './undeployments-repository.stub'
+
+export {
+    DeploymentsRepositoryStub,
+    QueuedDeploymentsRepositoryStub,
+    UndeploymentsRepositoryStub
+}
--- src/tests/stubs/services/index.ts
+++ src/tests/stubs/services/index.ts
@@ -2,10 +2,14 @@ import { DeploymentsServiceStub } from './deployments-service.stub'
 import { HttpServiceStub } from './http-service.stub'
 import { StatusManagementServiceStub } from './status-management-service.stub'
 import { ConsoleLoggerServiceStub } from './console-logger-service.stub'
+import { PipelineQueuesServiceStub } from './pipeline-queues-service.stub'
+import { PipelinesServiceStub } from './pipelines-service.stub'
 
 export {
     DeploymentsServiceStub,
     HttpServiceStub,
     StatusManagementServiceStub,
-    ConsoleLoggerServiceStub
+    ConsoleLoggerServiceStub,
+    PipelineQueuesServiceStub,
+    PipelinesServiceStub
 }
