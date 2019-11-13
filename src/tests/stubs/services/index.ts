cbaa4d8e80de9442802c2e1a9972309ff1ae43c0
lucasbfernandes <lucasbfernandes94@gmail.com>
2019-11-13 18:55:58 -0300

    Fixing minor errors

--- src/tests/stubs/services/index.ts
+++ src/tests/stubs/services/index.ts
@@ -1,11 +1,11 @@
 import { DeploymentsServiceStub } from './deployments-service.stub'
 import { HttpServiceStub } from './http-service.stub'
-import { DeploymentsStatusManagementServiceStub } from './deployments-status-management-service.stub'
+import { StatusManagementServiceStub } from './status-management-service.stub'
 import { ConsoleLoggerServiceStub } from './console-logger-service.stub'
 
 export {
     DeploymentsServiceStub,
     HttpServiceStub,
-    DeploymentsStatusManagementServiceStub,
+    StatusManagementServiceStub,
     ConsoleLoggerServiceStub
 }
