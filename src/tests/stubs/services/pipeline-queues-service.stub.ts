b64dbf7c0374cee6dec87f85aa4e6128d1c57073
lucasbfernandes <lucasbfernandes94@gmail.com>
2019-11-14 11:15:04 -0300

    Testing dto contract generation

--- src/tests/stubs/services/pipeline-queues-service.stub.ts
+++ src/tests/stubs/services/pipeline-queues-service.stub.ts
@@ -1,3 +1,13 @@
+import { QueuedPipelineStatusEnum } from '../../../app/api/deployments/enums'
+import { QueuedUndeploymentEntity } from '../../../app/api/deployments/entity'
+
 export class PipelineQueuesServiceStub {
 
+    public async getQueuedPipelineStatus(): Promise<QueuedPipelineStatusEnum> {
+        return Promise.resolve({} as QueuedPipelineStatusEnum)
+    }
+
+    public async enqueueUndeploymentExecution(): Promise<QueuedUndeploymentEntity> {
+        return Promise.resolve({} as QueuedUndeploymentEntity)
+    }
 }
--- src/tests/stubs/services/pipelines-service.stub.ts
+++ src/tests/stubs/services/pipelines-service.stub.ts
@@ -1,3 +1,6 @@
 export class PipelinesServiceStub {
 
+    public async triggerUndeployment(): Promise<void> {
+        return Promise.resolve()
+    }
 }
