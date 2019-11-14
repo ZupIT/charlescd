85bedc9bc63675cb5f9b9228d7c5b71f6d529560
lucasbfernandes <lucasbfernandes94@gmail.com>
2019-11-14 16:28:02 -0300

    Creating receive undeployment callback usecase  test structure

--- src/tests/stubs/repository/index.ts
+++ src/tests/stubs/repository/index.ts
@@ -4,6 +4,8 @@ import { UndeploymentsRepositoryStub } from './undeployments-repository.stub'
 import { ModulesRepositoryStub } from './modules-repository.stub'
 import { ComponentDeploymentsRepositoryStub } from './component-deployments-repository.stub'
 import { ComponentsRepositoryStub } from './components-repository.stub'
+import { QueuedUndeploymentsRepositoryStub } from './queued-undeployments-repository.stub'
+import { ComponentUndeploymentsRepositoryStub } from './component-undeployments-repository.stub'
 
 export {
     DeploymentsRepositoryStub,
@@ -11,5 +13,7 @@ export {
     UndeploymentsRepositoryStub,
     ModulesRepositoryStub,
     ComponentDeploymentsRepositoryStub,
-    ComponentsRepositoryStub
+    ComponentsRepositoryStub,
+    QueuedUndeploymentsRepositoryStub,
+    ComponentUndeploymentsRepositoryStub
 }
