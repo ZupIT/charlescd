dbdd8725af1744f0d796a0c7bce94b05ae293255
Thalles Freitas <thalles.freitas@zup.com.br>
2020-02-06 17:08:01 -0300

    adding corrections

--- src/app/api/components/use-cases/component-queue.usecase.ts
+++ src/app/api/components/use-cases/component-queue.usecase.ts
@@ -1,15 +1,27 @@
 import {Injectable} from '@nestjs/common'
 import {PipelineQueuesService} from '../../deployments/services'
 import {ReadQueuedDeploymentDto} from '../../deployments/dto'
+import {QueuedDeploymentEntity} from '../../deployments/entity'
+import {QueuedDeploymentsRepository} from '../../deployments/repository'
 
 @Injectable()
 export class ComponentQueueUseCase {
-    constructor(private readonly pipelineQueuesService: PipelineQueuesService) {
-    }
+    constructor(private readonly pipelineQueuesService: PipelineQueuesService,
+                private readonly queuedDeploymentsRepository: QueuedDeploymentsRepository
+    ) {}
 
     public async execute(id: string): Promise<ReadQueuedDeploymentDto[]> {
-        return this.pipelineQueuesService.getComponentDeploymentQueue(id).
-        then(queuedDeployments => queuedDeployments.map(queuedDeployment => queuedDeployment.toReadDto()))
+        return this.getComponentDeploymentQueue(id).
+        then(queuedDeployments => queuedDeployments.map(
+             queuedDeployment => queuedDeployment.toReadDto()
+            )
+        )
+    }
+    public async getComponentDeploymentQueue(
+        componentId: string
+    ): Promise<QueuedDeploymentEntity[]> {
+
+        return this.queuedDeploymentsRepository.getAllByComponentIdAscending(componentId)
     }
 
 }
