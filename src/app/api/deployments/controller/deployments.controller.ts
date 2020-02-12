c7f24850ed6c49fb06aff3823b8fdf31313c5180
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-12 13:14:03 -0300

    Remove unnecessary imports

--- src/app/api/deployments/controller/deployments.controller.ts
+++ src/app/api/deployments/controller/deployments.controller.ts
@@ -10,7 +10,6 @@ import {
 import {
   CreateCircleDeploymentRequestDto,
   CreateDefaultDeploymentRequestDto,
-  CreateDeploymentRequestDto,
   CreateUndeploymentDto,
   ReadDeploymentDto,
   ReadUndeploymentDto
