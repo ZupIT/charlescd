c352c7f744863f4f8a0db8e2c798a04a1df5348a
Thalles Freitas <thalles.freitas@zup.com.br>
2020-02-12 18:13:53 -0300

    correct component validation

--- src/app/api/components/controller/components.controller.ts
+++ src/app/api/components/controller/components.controller.ts
@@ -4,7 +4,7 @@ import {
   Param, UsePipes
 } from '@nestjs/common'
 import { ReadQueuedDeploymentDto } from '../../deployments/dto'
-import { ComponentsPipe } from '../pipe/components.pipe'
+import { ComponentsExistencePipe } from '../pipe/components.pipe'
 import { GetComponentQueueUseCase } from '../use-cases/get-component-queue.usecase'
 
 @Controller('components')
@@ -13,7 +13,7 @@ export class ComponentsController {
   constructor(private readonly getComponentQueueUseCase: GetComponentQueueUseCase) {}
 
   @Get(':id/queue')
-  @UsePipes(ComponentsPipe)
+  @UsePipes(ComponentsExistencePipe)
   public async getComponentDeploymentQueue(@Param('id') id: string): Promise<ReadQueuedDeploymentDto[]> {
     return await this.getComponentQueueUseCase.execute(id)
   }
