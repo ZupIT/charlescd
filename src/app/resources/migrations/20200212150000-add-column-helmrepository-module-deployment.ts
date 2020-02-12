1c35be3aff586a03f7bd6f025e93c4c75a174406
jeiassoareszup <jeias.soares@zup.com.br>
2020-02-12 15:51:16 -0300

    fix table name in migration

--- src/app/resources/migrations/20200212150000-add-column-helmrepository-module-deployment.ts
+++ src/app/resources/migrations/20200212150000-add-column-helmrepository-module-deployment.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
 export class AddColumnHelmrepositoryModuleDeployment20200212150000 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner) {
-        await queryRunner.addColumn('module_deployment', new TableColumn({
+        await queryRunner.addColumn('module_deployments', new TableColumn({
             name: 'helm_repository',
             type: 'varchar',
             isNullable: true
@@ -12,7 +12,7 @@ export class AddColumnHelmrepositoryModuleDeployment20200212150000 implements Mi
 
     public async down(queryRunner: QueryRunner) {
         await queryRunner.dropColumn(
-            'module_deployment',
+            'module_deployments',
             'helm_repository'
         )
     }
