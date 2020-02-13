765d64b187890e34fd59dcda44b30cc2b835ec0e
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-13 10:09:02 -0300

    Create default deployment request usecase test

--- src/tests/stubs/services/pipeline-deployments-service.stub.ts
+++ src/tests/stubs/services/pipeline-deployments-service.stub.ts
@@ -3,4 +3,8 @@ export class PipelineDeploymentsServiceStub {
     public async triggerCircleDeployment(): Promise<void> {
         return Promise.resolve()
     }
+
+    public async triggerDefaultDeployment(): Promise<void> {
+        return Promise.resolve()
+    }
 }
