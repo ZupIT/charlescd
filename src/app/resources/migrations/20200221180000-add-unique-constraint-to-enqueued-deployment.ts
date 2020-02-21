cdc43f87abfa59ad8086dcfc1894892f561e4d3b
Cassio Godinho <cassiopgodinho@gmail.com>
2020-02-21 16:50:13 -0300

    add component_id to the partial index

--- src/app/resources/migrations/20200221180000-add-unique-constraint-to-enqueued-deployment.ts
+++ src/app/resources/migrations/20200221180000-add-unique-constraint-to-enqueued-deployment.ts
@@ -3,7 +3,10 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
 export class AddUniqueConstraintToEnqueuedDeployment20200221180000 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner) {
-        queryRunner.query('CREATE UNIQUE INDEX queued_deployments_status_running_uniq ON queued_deployments (status) where status = \'RUNNING\'')
+        queryRunner.query(`
+            CREATE UNIQUE INDEX queued_deployments_status_running_uniq ON queued_deployments (component_id, status) where status = 'RUNNING'
+        `
+        )
     }
 
     public async down(queryRunner: QueryRunner) {
