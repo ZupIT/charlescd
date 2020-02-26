7ef2502ea6f87a599b0e447d81acac7e2b0bf1b4
Cassio Godinho <cassiopgodinho@gmail.com>
2020-02-26 18:49:38 -0300

    handle race condition on default deployment usecase

--- src/tests/unit/use-cases/create-default-deployment-request-usecase.spec.ts
+++ src/tests/unit/use-cases/create-default-deployment-request-usecase.spec.ts
@@ -33,7 +33,7 @@ import {
     CreateCircleDeploymentDto,
     CreateCircleDeploymentRequestDto
 } from '../../../app/api/deployments/dto/create-deployment'
-import { Repository } from 'typeorm'
+import { Repository, QueryFailedError } from 'typeorm'
 import { QueuedPipelineStatusEnum } from '../../../app/api/deployments/enums'
 
 describe('CreateDefaultDeploymentRequestUsecase', () => {
@@ -138,6 +138,19 @@ describe('CreateDefaultDeploymentRequestUsecase', () => {
                 .mockImplementation(() => Promise.resolve(deployment))
             jest.spyOn(queuedDeploymentsRepository, 'save')
                 .mockImplementation(() => Promise.resolve(queuedDeployment))
+
+            expect(await createDefaultDeploymentRequestUsecase.execute(createDeploymentDto, 'dummy-deployment-id'))
+                .toEqual(deployment.toReadDto())
+        })
+
+        it('should handle duplicated module default deployment', async () => {
+
+            jest.spyOn(deploymentsRepository, 'save')
+                .mockImplementation(() => Promise.resolve(deployment))
+            jest.spyOn(queuedDeploymentsRepository, 'save')
+                .mockImplementationOnce(
+                    () => { throw new QueryFailedError('query', [], { constraint: 'queued_deployments_status_running_uniq' }) }
+                ).mockImplementationOnce(() => Promise.resolve(queuedDeployment))
 
             expect(await createDefaultDeploymentRequestUsecase.execute(createDeploymentDto, 'dummy-deployment-id'))
                 .toEqual(deployment.toReadDto())
