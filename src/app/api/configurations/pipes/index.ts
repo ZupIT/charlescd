f12ce5185dbb45736dc06b62d9caf3611add8a26
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-28 02:55:53 -0300

    Delete unnecessary code

--- src/app/api/configurations/pipes/index.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleExistencePipe } from './module-existence.pipe'
-import { ModuleUniquenessPipe } from './module-uniqueness.pipe'
-
-export {
-    ModuleExistencePipe,
-    ModuleUniquenessPipe
-}
