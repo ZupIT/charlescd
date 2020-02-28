bcfd58b7dfc7fff07de7af3551662a112e4b7838
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-28 19:48:27 -0300

    Create k8s configuration usecase test

--- src/tests/stubs/repository/k8s-configurations-repository.stub.ts
+++ src/tests/stubs/repository/k8s-configurations-repository.stub.ts
@@ -1,8 +1,19 @@
-import { K8sConfigurationDataEntity } from '../../../app/api/configurations/entity'
+import {
+    K8sConfigurationDataEntity,
+    K8sConfigurationEntity
+} from '../../../app/api/configurations/entity'
 
 export class K8sConfigurationsRepositoryStub {
 
     public findDecrypted(): Promise<K8sConfigurationDataEntity> {
         return Promise.resolve({} as K8sConfigurationDataEntity)
+    }
+
+    public save(): Promise<K8sConfigurationEntity> {
+        return Promise.resolve({} as K8sConfigurationEntity)
+    }
+
+    public saveEncrypted(): Promise<K8sConfigurationEntity> {
+        return Promise.resolve({} as K8sConfigurationEntity)
     }
 }
