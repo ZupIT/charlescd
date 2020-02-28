ea25d3417d9a76fa01fe6ef070b74acaf2473c02
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-28 18:54:30 -0300

    Testing controllers

--- src/tests/stubs/use-cases/index.ts
+++ src/tests/stubs/use-cases/index.ts
@@ -1,9 +1,13 @@
 import { CreateUndeploymentRequestUsecaseStub } from './create-undeployment-request-usecase.stub'
 import { CreateCircleDeploymentRequestUsecaseStub } from './create-circle-deployment-request-usecase.stub'
 import { CreateDefaultDeploymentRequestUsecaseStub } from './create-default-deployment-request-usecase.stub'
+import { CreateK8sConfigurationUsecaseStub } from './create-k8s-configuration-usecase.stub'
+import { GetK8sConfigurationUsecaseStub } from './get-k8s-configuration-usecase.stub'
 
 export {
     CreateUndeploymentRequestUsecaseStub,
     CreateCircleDeploymentRequestUsecaseStub,
-    CreateDefaultDeploymentRequestUsecaseStub
+    CreateDefaultDeploymentRequestUsecaseStub,
+    CreateK8sConfigurationUsecaseStub,
+    GetK8sConfigurationUsecaseStub
 }
