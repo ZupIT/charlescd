ce986e075e8fef5f1dfe471bd02df9bbda07e313
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-02-28 19:13:09 -0300

    CreateModuleUsecase controller test

--- src/tests/stubs/use-cases/index.ts
+++ src/tests/stubs/use-cases/index.ts
@@ -3,11 +3,13 @@ import { CreateCircleDeploymentRequestUsecaseStub } from './create-circle-deploy
 import { CreateDefaultDeploymentRequestUsecaseStub } from './create-default-deployment-request-usecase.stub'
 import { CreateK8sConfigurationUsecaseStub } from './create-k8s-configuration-usecase.stub'
 import { GetK8sConfigurationUsecaseStub } from './get-k8s-configuration-usecase.stub'
+import { CreateModuleUsecaseStub } from './create-module-usecase.stub'
 
 export {
     CreateUndeploymentRequestUsecaseStub,
     CreateCircleDeploymentRequestUsecaseStub,
     CreateDefaultDeploymentRequestUsecaseStub,
     CreateK8sConfigurationUsecaseStub,
-    GetK8sConfigurationUsecaseStub
+    GetK8sConfigurationUsecaseStub,
+    CreateModuleUsecaseStub
 }
