096e1ebc270878c03afb7ae698e13126feec81f7
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-03-02 15:51:32 -0300

    Specifying cipher algorithm

--- src/app/api/configurations/repository/k8s-configurations.repository.ts
+++ src/app/api/configurations/repository/k8s-configurations.repository.ts
@@ -14,18 +14,18 @@ import { AppConstants } from '../../../core/constants'
 export class K8sConfigurationsRepository extends Repository<K8sConfigurationEntity> {
 
     public async saveEncrypted(
-        k8sConfiguration: K8sConfigurationEntity
+        k8sConfig: K8sConfigurationEntity
     ): Promise<K8sConfigurationEntity> {
 
         const queryResult: InsertResult = await this.createQueryBuilder('k8s_configurations')
             .insert()
             .values({
-                id: k8sConfiguration.id,
-                name: k8sConfiguration.name,
+                id: k8sConfig.id,
+                name: k8sConfig.name,
                 configurationData: () =>
-                    `PGP_SYM_ENCRYPT('${JSON.stringify(k8sConfiguration.configurationData)}', '${AppConstants.ENCRYPTION_KEY}')`,
-                authorId: k8sConfiguration.authorId,
-                applicationId: k8sConfiguration.applicationId
+                    `PGP_SYM_ENCRYPT('${JSON.stringify(k8sConfig.configurationData)}', '${AppConstants.ENCRYPTION_KEY}', 'cipher-algo=aes256')`,
+                authorId: k8sConfig.authorId,
+                applicationId: k8sConfig.applicationId
             })
             .returning('id, name, user_id, application_id, created_at')
             .execute()
@@ -49,7 +49,7 @@ export class K8sConfigurationsRepository extends Repository<K8sConfigurationEnti
     public async findDecrypted(id: string): Promise<K8sConfigurationDataEntity> {
 
         const queryResult: { configurationData: string } = await this.createQueryBuilder('k8s_configurations')
-            .select(`PGP_SYM_DECRYPT(configuration_data::bytea, '${AppConstants.ENCRYPTION_KEY}')`, 'configurationData')
+            .select(`PGP_SYM_DECRYPT(configuration_data::bytea, '${AppConstants.ENCRYPTION_KEY}', 'cipher-algo=aes256')`, 'configurationData')
             .where('k8s_configurations.id = :id', { id })
             .getRawOne()
 
