4f569c83de30b09b3c2649850ab332157e3136ae
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-03-02 17:13:34 -0300

    Removing default value

--- src/app/core/constants/application/configuration.constants.ts
+++ src/app/core/constants/application/configuration.constants.ts
@@ -2,7 +2,7 @@ export const ConfigurationConstants = {
 
   NODE_ENV: process.env.NODE_ENV || 'dev',
   DEFAULT_CIRCLE_ID: 'f5d23a57-5607-4306-9993-477e1598cc2a',
-  ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || 'dev-key'
+  ENCRYPTION_KEY: process.env.ENCRYPTION_KEY
 }
 
 export type DefaultCircleId = 'f5d23a57-5607-4306-9993-477e1598cc2a'
