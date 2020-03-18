c84376cd85969ce22b712e781cd61febb9e67717
Cassio Godinho <cassiopgodinho@gmail.com>
2020-03-18 14:47:19 -0300

    implement octopipe service to the strategy

--- src/app/core/integrations/cd/cd-strategy.factory.ts
+++ src/app/core/integrations/cd/cd-strategy.factory.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@nestjs/common'
 import { CdTypeEnum } from '../../../api/configurations/enums'
 import { SpinnakerService } from './spinnaker'
+import { OctopipeService } from '../octopipe'
 
 @Injectable()
 export class CdStrategyFactory {
 
     constructor(
-        private readonly spinnakerService: SpinnakerService
+        private readonly spinnakerService: SpinnakerService,
+        private readonly octopipeService: OctopipeService
     ) {}
 
     // TODO return type that specify cd contract. For now, we'll return SpinnakeService | OctopipeService
-    public create(type: CdTypeEnum): SpinnakerService {
+    public create(type: CdTypeEnum): SpinnakerService | OctopipeService {
 
         switch (type) {
             case CdTypeEnum.SPINNAKER:
                 return this.spinnakerService
+            case CdTypeEnum.OCTOPIPE:
+                return this.octopipeService
             default:
                 throw new Error('Invalid cd type value')
         }
