123314ae5905c0eacce369841f88eebcfa441127
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-03-18 16:24:33 -0300

    Adding OctopipeDependency

--- src/app/core/integrations/integrations.module.ts
+++ src/app/core/integrations/integrations.module.ts
@@ -22,6 +22,7 @@ import { ServicesModule } from '../services/services.module'
 import { DeploymentsModule } from '../../api/deployments/deployments.module'
 import { CdConfigurationsRepository } from '../../api/configurations/repository'
 import { CdStrategyFactory } from './cd'
+import { OctopipeService } from './octopipe'
 
 @Module({
   imports: [
@@ -41,6 +42,7 @@ import { CdStrategyFactory } from './cd'
   ],
   providers: [
     SpinnakerService,
+    OctopipeService,
     MooveService,
     DatabasesService,
     CdStrategyFactory
