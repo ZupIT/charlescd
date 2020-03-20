a79d52cab36bf5ca265c3255e2dcea3339a105fe
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-03-20 14:56:13 -0300

    Fix migration

--- src/app/resources/migrations/20200316153600-change-k8s-config-to-cd-config.ts
+++ src/app/resources/migrations/20200316153600-change-k8s-config-to-cd-config.ts
@@ -4,7 +4,7 @@ export class ChangeK8sConfigToCdConfig20200316153600 implements MigrationInterfa
 
     public async up(queryRunner: QueryRunner) {
         await queryRunner.query('ALTER TABLE k8s_configurations RENAME TO cd_configurations;', [])
-        await queryRunner.renameColumn('modules', 'k8s_configuration_id', 'cd_configuration_id')
+        await queryRunner.query('ALTER TABLE modules RENAME COLUMN k8s_configuration_id TO cd_configuration_id;')
         await queryRunner.query('ALTER TABLE modules DROP CONSTRAINT module_k8s_configuration_fk;', [])
         await queryRunner.query(
             'ALTER TABLE modules ADD CONSTRAINT module_cd_configuration_fk FOREIGN KEY (cd_configuration_id) REFERENCES cd_configurations(id);',
@@ -14,7 +14,7 @@ export class ChangeK8sConfigToCdConfig20200316153600 implements MigrationInterfa
 
     public async down(queryRunner: QueryRunner) {
         await queryRunner.query('ALTER TABLE cd_configurations RENAME TO k8s_configurations;', [])
-        await queryRunner.renameColumn('modules', 'cd_configuration_id', 'k8s_configuration_id')
+        await queryRunner.query('ALTER TABLE modules RENAME COLUMN cd_configuration_id TO k8s_configuration_id;')
         await queryRunner.query(
             'ALTER TABLE modules ADD CONSTRAINT module_k8s_configuration_fk FOREIGN KEY (k8s_configuration_id) REFERENCES k8s_configurations(id) ON DELETE CASCADE;',
             []
