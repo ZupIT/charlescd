4a74c0d78d320288394680b5a3448dc5d55bc475
Maycon <mayconjrpacheco@gmail.com>
2020-04-06 12:19:52 -0300

    Add eks provider implementation

--- pkg/cloudprovider/incluster/incluster.go
+++ pkg/cloudprovider/incluster/incluster.go
@@ -1,11 +1,13 @@
 package incluster
 
+import "k8s.io/client-go/dynamic"
+
 type InCluster struct{}
 
 func NewInCluster() *InCluster {
 	return &InCluster{}
 }
 
-func (inCluster *InCluster) Connect() {
+func (inCluster *InCluster) Connect() (dynamic.Interface, error) {
 
 }
