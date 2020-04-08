69a33c46a094cb09cf33decbba7404db4a8526cd
Cassio Godinho <cassiopgodinho@gmail.com>
2020-04-08 18:20:36 -0300

    add default schema validation

--- src/app/core/validations/schemas/index.ts
+++ src/app/core/validations/schemas/index.ts
@@ -1,9 +1,11 @@
 import { SpinnakerConfigurationDataSchema } from './spinnaker-configuration-data.schema'
 import { OctopipeGenericConfigurationDataSchema } from './octopipe-generic-configuration-data.schema'
 import { OctopipeEKSConfigurationDataSchema } from './octopipe-eks-configuration-data.schema'
+import { OctopipeDefaultConfigurationDataSchema } from './octopipe-default-configuration-data.schema'
 
 export {
     SpinnakerConfigurationDataSchema,
     OctopipeEKSConfigurationDataSchema,
-    OctopipeGenericConfigurationDataSchema
+    OctopipeGenericConfigurationDataSchema,
+    OctopipeDefaultConfigurationDataSchema
 }
