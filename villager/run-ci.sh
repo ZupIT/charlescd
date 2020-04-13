f668a4ea2a8c5c4e040aae881cd2333c2881891e
Ricardo <ricardorames@gmail.com>
2020-04-13 19:38:02 -0300

    Removing unused files

--- villager/run-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-#This Source Code Form is subject to the terms of the
-#Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
-#with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-set -e
-
-mvn -Dmaven.repo.local=/home/maven/.m2/repository​ -f /home/application/pom.xml clean install
