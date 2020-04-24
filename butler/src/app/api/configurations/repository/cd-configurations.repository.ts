e616050b6c7d8f57d3be1eb2394b65dce132555b
Mônica Ribeiro <monicarib1@gmail.com>
2020-04-24 09:13:38 -0300

    Implementing query to delete cd configuration by id.

--- butler/src/app/api/configurations/repository/cd-configurations.repository.ts
+++ butler/src/app/api/configurations/repository/cd-configurations.repository.ts
@@ -1,11 +1,7 @@
-import {
-    EntityRepository,
-    InsertResult,
-    Repository
-} from 'typeorm'
-import { CdConfigurationEntity, } from '../entity'
-import { plainToClass } from 'class-transformer'
-import { AppConstants } from '../../../core/constants'
+import {DeleteResult, EntityRepository, InsertResult, Repository} from 'typeorm'
+import {CdConfigurationEntity,} from '../entity'
+import {plainToClass} from 'class-transformer'
+import {AppConstants} from '../../../core/constants'
 
 @EntityRepository(CdConfigurationEntity)
 export class CdConfigurationsRepository extends Repository<CdConfigurationEntity> {
@@ -38,7 +34,7 @@ export class CdConfigurationsRepository extends Repository<CdConfigurationEntity
             .addSelect('user_id', 'authorId')
             .addSelect('workspace_id', 'workspaceId')
             .addSelect('created_at', 'createdAt')
-            .where('cd_configurations.workspace_id = :workspaceId', { workspaceId })
+            .where('cd_configurations.workspace_id = :workspaceId', {workspaceId})
             .getRawMany()
 
         return queryResult.map(configuration => plainToClass(CdConfigurationEntity, configuration))
@@ -52,7 +48,7 @@ export class CdConfigurationsRepository extends Repository<CdConfigurationEntity
             .addSelect('workspace_id', 'workspaceId')
             .addSelect('created_at', 'createdAt')
             .addSelect(`PGP_SYM_DECRYPT(configuration_data::bytea, '${AppConstants.ENCRYPTION_KEY}', 'cipher-algo=aes256')`, 'configurationData')
-            .where('cd_configurations.id = :id', { id })
+            .where('cd_configurations.id = :id', {id})
             .getRawOne()
 
         if (queryResult && queryResult.configurationData) {
@@ -60,5 +56,14 @@ export class CdConfigurationsRepository extends Repository<CdConfigurationEntity
         }
 
         return queryResult ? plainToClass(CdConfigurationEntity, queryResult) : undefined
+    }
+
+    public async delete(cdConfigurationId: string): Promise<DeleteResult> {
+
+        return await this.createQueryBuilder('cd_configurations')
+            .delete()
+            .where('id = :cdConfigurationId', {cdConfigurationId})
+            .execute()
+
     }
 }
