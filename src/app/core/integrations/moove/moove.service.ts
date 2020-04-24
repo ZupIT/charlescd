41c5b859a33458714cb179a2f2446c009292919d
Cassio Godinho <cassiopgodinho@gmail.com>
2020-04-24 10:43:02 -0300

    explicit rxjs error type

--- src/app/core/integrations/moove/moove.service.ts
+++ src/app/core/integrations/moove/moove.service.ts
@@ -1,61 +1,61 @@
 import { HttpService, Inject, Injectable } from '@nestjs/common'
+import { AxiosResponse } from 'axios'
+import { Observable, of, throwError } from 'rxjs'
+import { concatMap, delay, map, retryWhen, tap } from 'rxjs/operators'
+import { AppConstants } from '../../constants'
+import { IoCTokensConstants } from '../../constants/ioc'
 import { ConsoleLoggerService } from '../../logs/console'
 import IEnvConfiguration from '../configuration/interfaces/env-configuration.interface'
-import { AxiosResponse } from 'axios'
-import { IoCTokensConstants } from '../../constants/ioc'
-import { Observable, of, throwError } from 'rxjs';
-import { concatMap, delay, map, retryWhen, tap } from 'rxjs/operators';
-import { AppConstants } from '../../constants';
 
 @Injectable()
 export class MooveService {
 
-    constructor(
-      private readonly httpService: HttpService,
-      private readonly consoleLoggerService: ConsoleLoggerService,
-      @Inject(IoCTokensConstants.ENV_CONFIGURATION)
-      private readonly envConfiguration: IEnvConfiguration
-    ) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly consoleLoggerService: ConsoleLoggerService,
+    @Inject(IoCTokensConstants.ENV_CONFIGURATION)
+    private readonly envConfiguration: IEnvConfiguration
+  ) { }
 
-    public notifyDeploymentStatus(
-      deploymentId: string,
-      status: string,
-      callbackUrl: string,
-      circleId: string
-    ): Observable<AxiosResponse> {
+  public notifyDeploymentStatus(
+    deploymentId: string,
+    status: string,
+    callbackUrl: string,
+    circleId: string
+  ): Observable<AxiosResponse> {
 
-      try {
-        this.consoleLoggerService.log('START:NOTIFY_DEPLOYMENT_STATUS', { deploymentId, status, callbackUrl })
-        return this.httpService.post(
-          callbackUrl,
-          { deploymentStatus: status },
-          { ...(circleId && { headers: { 'x-circle-id': circleId } }) }
-        ).pipe(
-            map(response=>response),
-            retryWhen(error=> this.getNotificationRetryCondition(error))
-        )
-        this.consoleLoggerService.log('FINISH:NOTIFY_DEPLOYMENT_STATUS')
-      } catch (error) {
-        this.consoleLoggerService.error('ERROR:NOTIFY_DEPLOYMENT_STATUS', error)
-        throw error
-      }
+    try {
+      this.consoleLoggerService.log('START:NOTIFY_DEPLOYMENT_STATUS', { deploymentId, status, callbackUrl })
+      return this.httpService.post(
+        callbackUrl,
+        { deploymentStatus: status },
+        { ...(circleId && { headers: { 'x-circle-id': circleId } }) }
+      ).pipe(
+        map(response => response),
+        retryWhen(error => this.getNotificationRetryCondition(error))
+      )
+      this.consoleLoggerService.log('FINISH:NOTIFY_DEPLOYMENT_STATUS')
+    } catch (error) {
+      this.consoleLoggerService.error('ERROR:NOTIFY_DEPLOYMENT_STATUS', error)
+      throw error
     }
-    private getNotificationRetryCondition(deployError) {
+  }
+  private getNotificationRetryCondition(deployError: Observable<any>) {
 
-        return deployError.pipe(
-            concatMap((error, attempts) => {
-                return attempts >= AppConstants.MOOVE_NOTIFICATION_MAXIMUM_RETRY_ATTEMPTS ?
-                    throwError('Reached maximum attemps.') :
-                    this.getNotificationRetryPipe(error, attempts)
-            })
-        )
-    }
+    return deployError.pipe(
+      concatMap((error, attempts) => {
+        return attempts >= AppConstants.MOOVE_NOTIFICATION_MAXIMUM_RETRY_ATTEMPTS ?
+          throwError('Reached maximum attemps.') :
+          this.getNotificationRetryPipe(error, attempts)
+      })
+    )
+  }
 
-    private getNotificationRetryPipe(error, attempts: number) {
+  private getNotificationRetryPipe(error: Observable<any>, attempts: number) {
 
-        return of(error).pipe(
-            tap(() => this.consoleLoggerService.log(`Deploy attempt #${attempts + 1}. Retrying deployment: ${error}`)),
-            delay(AppConstants.MOOVE_NOTIFICATION_MILLISECONDS_RETRY_DELAY)
-        )
-    }
+    return of(error).pipe(
+      tap(() => this.consoleLoggerService.log(`Deploy attempt #${attempts + 1}. Retrying deployment: ${error}`)),
+      delay(AppConstants.MOOVE_NOTIFICATION_MILLISECONDS_RETRY_DELAY)
+    )
+  }
 }
