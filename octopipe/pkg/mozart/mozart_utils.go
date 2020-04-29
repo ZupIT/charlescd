3b2cecd93c7941abf54169333d1f4e38b032ac67
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-04-29 03:50:53 -0300

    Rollback deployments

--- octopipe/pkg/mozart/mozart_utils.go
+++ octopipe/pkg/mozart/mozart_utils.go
@@ -22,23 +22,24 @@ func getStages(deployment *deployment.Deployment) [][]*pipeline.Step {
 }
 
 func getDeployedVersionsStepsByDeployment(deployment *deployment.Deployment) []*pipeline.Step {
-	return getStepsByVersions(deployment, deployment.Versions, deployer.DeployAction)
+	return getStepsByVersions(deployment, deployment.Versions, deployer.DeployAction, deployer.UndeployAction)
 }
 
 func getUndeployedVersionsStepsByDeployment(deployment *deployment.Deployment) []*pipeline.Step {
-	return getStepsByVersions(deployment, deployment.UnusedVersions, deployer.UndeployAction)
+	return getStepsByVersions(deployment, deployment.UnusedVersions, deployer.UndeployAction, deployer.DeployAction)
 }
 
 func getStepsByVersions(
-	deployment *deployment.Deployment, versions []*deployment.Version, action string,
+	deployment *deployment.Deployment, versions []*deployment.Version, action string, rollbackAction string,
 ) []*pipeline.Step {
 	steps := []*pipeline.Step{}
 	for _, version := range versions {
 		steps = append(steps, &pipeline.Step{
-			Name:        version.Version,
-			ModuleName:  deployment.Name,
-			Namespace:   deployment.Namespace,
-			Action:      action,
+			Name:        	version.Version,
+			ModuleName:  	deployment.Name,
+			Namespace:   	deployment.Namespace,
+			Action:      	action,
+			RollbackAction: rollbackAction,
 			Webhook:     deployment.Webhook,
 			ForceUpdate: false,
 			Git: &pipeline.Git{
@@ -81,4 +82,14 @@ func getIstioComponentsSteps(deployment *deployment.Deployment) []*pipeline.Step
 	}
 
 	return steps
+}
+
+func convertToRollbackSteps(steps []*pipeline.Step) []*pipeline.Step {
+	var rollbackSteps []*pipeline.Step
+	for _, step := range steps {
+		rollbackStep := *step
+		rollbackStep.Action, rollbackStep.RollbackAction = step.RollbackAction, step.Action
+		rollbackSteps = append(rollbackSteps, &rollbackStep)
+	}
+	return rollbackSteps
 }
