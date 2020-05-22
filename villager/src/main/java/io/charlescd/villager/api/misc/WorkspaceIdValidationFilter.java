3ec930cc8d334f0bdf064cee6caa30ff67cbdb28
Mateus Cruz <emiteze@hotmail.com>
2020-05-22 10:52:31 -0300

    Renaming packages to io.charlescd
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

---
+++ villager/src/main/java/io/charlescd/villager/api/misc/WorkspaceIdValidationFilter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.charlescd.villager.api.misc;
+
+import io.charlescd.villager.infrastructure.filter.RequestContext;
+import io.charlescd.villager.util.Constants;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class WorkspaceIdValidationFilter implements ContainerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkspaceIdValidationFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext reqContext) {
+        String contentType = reqContext.getHeaders().getFirst(Constants.X_WORKSPACE_ID);
+
+        if (StringUtils.isEmpty(contentType)) {
+            var msg = "Header x-workspace-id is required!";
+            LOGGER.error(RequestContext.getTag(), msg);
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+}
---
+++ villager/src/main/java/io/charlescd/villager/interactor/build/CreateBuildInput.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.charlescd.villager.interactor.build;
+
+import io.charlescd.villager.api.resources.registry.ComponentRequestPart;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CreateBuildInput {
+    @NotEmpty
+    private Set<Module> modules;
+    @NotBlank
+    private String tagName;
+    @NotBlank
+    private String callbackUrl;
+
+    private CreateBuildInput(Builder builder) {
+        modules = builder.modules;
+        tagName = builder.tagName;
+        callbackUrl = builder.callbackUrl;
+    }
+
+    public Set<Module> getModules() {
+        return modules;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Set<Module> modules = new LinkedHashSet<>();
+        private String tagName;
+        private String callbackUrl;
+
+        private Builder() {
+        }
+
+        public Builder withModule(String id, String name, String registryConfigurationId, Set<ComponentRequestPart> components) {
+            Module module = new Module(
+                    id,
+                    name,
+                    registryConfigurationId,
+                    components.stream().map(
+                            c -> new Component(c.getName(), c.getTagName())).collect(Collectors.toSet()
+                    )
+            );
+            modules.add(module);
+            return this;
+        }
+
+        public Builder withTagName(String val) {
+            tagName = val;
+            return this;
+        }
+
+        public Builder withCallbackUrl(String val) {
+            callbackUrl = val;
+            return this;
+        }
+
+        public CreateBuildInput build() {
+            return new CreateBuildInput(this);
+        }
+    }
+
+    public static class Module {
+
+        private String id;
+        private String name;
+        private String registryConfigurationId;
+        private Set<Component> components;
+
+        Module(String id, String name, String registryConfigurationId, Set<Component> components) {
+            this.id = id;
+            this.name = name;
+            this.components = components;
+            this.registryConfigurationId = registryConfigurationId;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getRegistryConfigurationId() {
+            return registryConfigurationId;
+        }
+
+        public Set<Component> getComponents() {
+            return components;
+        }
+    }
+
+    public static class Component {
+
+        private String name;
+        private String tagName;
+
+        Component(String name, String tagName) {
+            this.name = name;
+            this.tagName = tagName;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getTagName() {
+            return tagName;
+        }
+    }
+}
