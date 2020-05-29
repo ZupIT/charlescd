4dbf2fb522e8540205a8fee3b88b4c4dafa309a4
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-05-29 17:05:59 -0300

    Remove k8s and web folders
    Signed-off-by: lucasbfernandes <lucasbfernandes94@gmail.com>

--- octopipe/web/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './Main'
-import './library'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<Main />, document.getElementById('root'))
