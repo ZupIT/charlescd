ee6b32746cf113edcb2fadce8ba5a391fd53fb23
Leandro Queiroz <leandroqueiroz92@gmail.com>
2020-06-03 01:47:17 -0300

    remove v2 from routes and define new variable environments
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

---
+++ ui/public/config.js
@@ -0,0 +1,1 @@
+window.ENVIRONMENT = {PUBLIC_URL:"/",REACT_APP_API_URI:"https://charles-prod.continuousplatform.com",REACT_APP_AUTH_URI:"https://charles-prod.continuousplatform.com/keycloak",REACT_APP_AUTH_REALM:"charlescd",REACT_APP_AUTH_CLIENT_ID:"charlescd-client",}
