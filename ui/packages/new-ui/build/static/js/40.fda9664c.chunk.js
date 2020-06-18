2dbb171f1cce3dc71f72e94a6bf7ffb1a90afa56
Lucas Sales <lucas.sales@zup.com.br>
2020-06-17 22:24:31 -0300

    remove node-modules from repo (old new-ui)
    Signed-off-by: Lucas Sales <lucas.sales@zup.com.br>
    Signed-off-by: Célio José dos Santos Filho <cjsantosf@gmail.com>

--- ui/packages/new-ui/build/static/js/40.fda9664c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnew-ui"]=this["webpackJsonpnew-ui"]||[]).push([[40],{299:function(s,e,i){s.exports=i.p+"static/media/segments.a7c0453f.svg"}}]);
-//# sourceMappingURL=40.fda9664c.chunk.js.map
--- ui/packages/new-ui/node_modules/react-hook-form/dist/utils/isHTMLElement.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (value: any) => value is HTMLElement;
-export default _default;
