ec013745a8ad3380cd8882dd4365f586de3f14ab
Lucas Sales <lucas.sales@zup.com.br>
2020-06-17 22:24:31 -0300

    remove node-modules from repo (old new-ui)
    Signed-off-by: Lucas Sales <lucas.sales@zup.com.br>

--- ui/packages/new-ui/build/static/js/29.8777b28c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnew-ui"]=this["webpackJsonpnew-ui"]||[]).push([[29],{288:function(p,s,e){p.exports=e.p+"static/media/help.fac86cab.svg"}}]);
-//# sourceMappingURL=29.8777b28c.chunk.js.map
