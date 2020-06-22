b8318a747423c3507f232d7747428417422f8dea
Lucas Borges <lucasbfernandes94@gmail.com>
2020-06-22 17:13:40 -0300

    out of cluster structure

--- tests/load/octopipe/out_of_cluster.py
+++ tests/load/octopipe/out_of_cluster.py
@@ -1,10 +1,17 @@
 import random
+import json
 import fixtures
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant
 
 class OctopipeOutOfClusterTest(HttpUser):
     wait_time = constant(0.01)
 
     @task
     def deploy(self):
-        pass
+        request_data = fixtures.get_outofcluster_request()
+        headers = {'content-type': 'application/json'}
+        with self.client.post("/api/v1/pipelines", data = json.dumps(request_data), headers = headers, catch_response = True) as response:
+            if response.status_code == 201:
+                response.success()
+            else:
+                response.failure(response.text)
