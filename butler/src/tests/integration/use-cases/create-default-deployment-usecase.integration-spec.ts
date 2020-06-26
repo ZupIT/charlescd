acf24452bba142eace2eac3a51a1dc0d501d361a
Thalles Freitas <thalles.freitas@zup.com.br>
2020-06-26 09:24:08 -0300

    insert validation of repeated components in deployment
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- butler/src/tests/integration/use-cases/create-default-deployment-usecase.integration-spec.ts
+++ butler/src/tests/integration/use-cases/create-default-deployment-usecase.integration-spec.ts
@@ -621,6 +621,46 @@ describe('CreateDefaultDeploymentUsecase', () => {
     expect(deployment.modules[1].components[1].status).toBe(DeploymentStatusEnum.FAILED)
   })
 
+  it(`/POST deployments/default with repeated components should return bad request exception`, async () => {
+    const createDeploymentRequest = {
+      deploymentId: '5ba3691b-d647-4a36-9f6d-c089f114e476',
+      applicationName: 'c26fbf77-5da1-4420-8dfa-4dea235a9b1e',
+      modules: [
+        {
+          moduleId: 'e2c937cb-d77e-48db-b1ea-7d3df16fd02c',
+          helmRepository: 'helm-repository.com',
+          components: [
+            {
+              componentId: 'c41f029d-186c-4097-ad43-1b344b2e8041',
+              componentName: 'component-name',
+              buildImageUrl: 'image-url',
+              buildImageTag: 'image-tag'
+            },
+            {
+              componentId: 'f4c4bcbe-58a9-41cc-ad8b-7177121905de',
+              componentName: 'component-name2',
+              buildImageUrl: 'image-url2',
+              buildImageTag: 'image-tag2'
+            },
+            {
+              componentId: 'f4c4bcbe-58a9-41cc-ad8b-7177121905de',
+              componentName: 'component-name2',
+              buildImageUrl: 'image-url2',
+              buildImageTag: 'image-tag2'
+            }
+          ]
+        }
+      ],
+      authorId: 'author-id',
+      description: 'Deployment from Charles C.D.',
+      callbackUrl: 'http://localhost:8883/moove',
+      cdConfigurationId: '4046f193-9479-48b5-ac29-01f419b64cb5',
+      circle: null
+    }
+    await request(app.getHttpServer()).post('/deployments/default').send(createDeploymentRequest).expect(400)
+
+  })
+
   afterAll(async () => {
     await app.close()
   })
