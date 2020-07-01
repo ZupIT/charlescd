726e9385ece9350651df61c19824107411c17ee5
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2020-07-01 19:47:36 -0300

    Fix unused components deployment phase
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- butler/src/app/api/deployments/services/pipeline-queues.service.ts
+++ butler/src/app/api/deployments/services/pipeline-queues.service.ts
@@ -65,6 +65,7 @@ export class PipelineQueuesService {
       await this.queuedDeploymentsRepository.getNextQueuedDeployment(finishedComponentDeployment.componentId)
     const runningDeployment: QueuedDeploymentEntity | undefined =
       await this.queuedDeploymentsRepository.getOneByComponentIdRunning(finishedComponentDeployment.componentId)
+
     if (nextQueuedDeployment && !runningDeployment) {
       nextQueuedDeployment.type === QueuedPipelineTypesEnum.QueuedDeploymentEntity ?
         await this.triggerQueuedDeployment(nextQueuedDeployment) :
