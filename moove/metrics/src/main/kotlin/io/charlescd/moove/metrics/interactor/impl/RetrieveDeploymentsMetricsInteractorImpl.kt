e0986f5a623b9038045ade727700b9887e2adfaf
apucci <arthur.pucci@gmail.com>
2020-07-03 10:27:52 -0300

    Adjusting deployment metrics query
    Signed-off-by: apucci <arthur.pucci@gmail.com>

--- moove/metrics/src/main/kotlin/io/charlescd/moove/metrics/interactor/impl/RetrieveDeploymentsMetricsInteractorImpl.kt
+++ moove/metrics/src/main/kotlin/io/charlescd/moove/metrics/interactor/impl/RetrieveDeploymentsMetricsInteractorImpl.kt
@@ -16,12 +16,16 @@
 
 package io.charlescd.moove.metrics.interactor.impl
 
+import io.charlescd.moove.domain.DeploymentAverageTimeStats
 import io.charlescd.moove.domain.DeploymentStatusEnum
 import io.charlescd.moove.domain.repository.DeploymentRepository
 import io.charlescd.moove.metrics.api.PeriodType
 import io.charlescd.moove.metrics.api.response.DeploymentMetricsRepresentation
 import io.charlescd.moove.metrics.interactor.RetrieveDeploymentsMetricsInteractor
 import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.LocalDate
+import java.util.*
 
 @Component
 class RetrieveDeploymentsMetricsInteractorImpl(val deploymentRepository: DeploymentRepository) : RetrieveDeploymentsMetricsInteractor {
@@ -40,6 +44,12 @@ class RetrieveDeploymentsMetricsInteractorImpl(val deploymentRepository: Deploym
         val deploymentsAverageTime = deploymentRepository
             .averageDeployTimeBetweenTodayAndDaysPastGroupingByCreationDate(workspaceId, circlesIds ?: emptyList(), period.numberOfDays)
 
+        if (deploymentsAverageTime.isNotEmpty()) {
+            val deploymentsStack = deploymentsAverageTime.sortedByDescending { it.date }
+            val firstDay = LocalDate.now().minusDays(period.numberOfDays.toLong())
+
+        }
+
         return DeploymentMetricsRepresentation.from(
             successfulDeployments,
             failedDeployments,
@@ -48,4 +58,22 @@ class RetrieveDeploymentsMetricsInteractorImpl(val deploymentRepository: Deploym
             deploymentsAverageTime
         )
     }
+
+    private fun test(presentStats: DeploymentAverageTimeStats, statsToCome: Iterator<DeploymentAverageTimeStats>){
+        if (!statsToCome.hasNext()){
+            if (presentStats.date < LocalDate.now()){
+                test(DeploymentAverageTimeStats(Duration.ZERO, presentStats.date.plusDays(1)), statsToCome)
+            }else{
+                val list = LinkedList<DeploymentAverageTimeStats>()
+                list.addFirst(presentStats)
+            }
+        }
+
+        val nextDayStats = statsToCome.next()
+        if (presentStats.date.until(nextDayStats.date).days > 1){
+
+        }
+
+    }
+
 }
