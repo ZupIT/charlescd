ccf3c4886e5f7b8d4021128a18ad592faf13e3d4
apucci <arthur.pucci@gmail.com>
2020-07-07 09:17:05 -0300

    Deployment metrics file unifying
    Signed-off-by: apucci <arthur.pucci@gmail.com>

--- moove/domain/src/main/kotlin/io/charlescd/moove/domain/DeploymentMetrics.kt
+++ moove/domain/src/main/kotlin/io/charlescd/moove/domain/DeploymentMetrics.kt
@@ -17,6 +17,19 @@
 package io.charlescd.moove.domain
 
 import java.time.Duration
+import java.time.LocalDate
+
+data class DeploymentStats(
+    val total: Int,
+    val deploymentStatus: DeploymentStatusEnum,
+    val averageTime: Duration,
+    val date: LocalDate
+)
+
+data class DeploymentAverageTimeStats(
+    val averageTime: Duration,
+    val date: LocalDate
+)
 
 data class DeploymentGeneralStats(
     val total: Int,
