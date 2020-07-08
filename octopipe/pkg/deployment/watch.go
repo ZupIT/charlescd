4f384fbc03cb43a33610792a4e55f49add957ad9
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-07-08 11:41:40 -0300

    Remove watch to undeploy
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- octopipe/pkg/deployment/watch.go
+++ octopipe/pkg/deployment/watch.go
@@ -44,6 +44,22 @@ func getTimeoutDuration() time.Duration {
 	return time.Duration(value)
 }
 
+func isResourController(resource *unstructured.Unstructured) bool {
+	_, isRsourceController, _ := unstructured.NestedInt64(resource.Object, "status", "replicas")
+	return isRsourceController
+}
+
+func getReplicasByResourceControllet(resource *unstructured.Unstructured) int64 {
+	replicas, _, _ := unstructured.NestedInt64(resource.Object, "status", "replicas")
+	return replicas
+}
+
+func isCreatedOrUpdatedResourceController(resource *unstructured.Unstructured) bool {
+	availableReplicas, found, _ := unstructured.NestedInt64(resource.Object, "status", "availableReplicas")
+	_, foundUnavailableReplicas, _ := unstructured.NestedInt64(resource.Object, "status", "unavailableReplicas")
+	return found && int64(availableReplicas) == getReplicasByResourceControllet(resource) && !foundUnavailableReplicas
+}
+
 func newCreateOrUpdateWatcher(manifest *unstructured.Unstructured, resourceInterface dynamic.ResourceInterface) error {
 	ticker := time.NewTicker(500 * time.Millisecond)
 	timeout := time.After(getTimeoutDuration() * time.Second)
@@ -58,20 +74,12 @@ func newCreateOrUpdateWatcher(manifest *unstructured.Unstructured, resourceInter
 				return err
 			}
 
-			replicas, found, err := unstructured.NestedInt64(resource.Object, "status", "replicas")
-			if err != nil {
-				ticker.Stop()
-				return err
-			}
-
-			if !found {
+			if !isResourController(resource) {
 				ticker.Stop()
 				return nil
 			}
 
-			availableReplicas, found, _ := unstructured.NestedInt64(resource.Object, "status", "availableReplicas")
-			_, foundUnavailableReplicas, _ := unstructured.NestedInt64(resource.Object, "status", "unavailableReplicas")
-			if found && int64(availableReplicas) == replicas && !foundUnavailableReplicas {
+			if isCreatedOrUpdatedResourceController(resource) {
 				ticker.Stop()
 				return nil
 			}
@@ -86,8 +94,9 @@ func newTerminatingWatcher(manifest *unstructured.Unstructured, resourceInterfac
 		select {
 		case <-timeout:
 			ticker.Stop()
-			return errors.New("terminating timeout")
+			return errors.New("Terminating timeout")
 		case <-ticker.C:
+
 			_, err := resourceInterface.Get(context.TODO(), manifest.GetName(), metav1.GetOptions{})
 			if err != nil && k8sErrors.IsNotFound(err) {
 				ticker.Stop()
