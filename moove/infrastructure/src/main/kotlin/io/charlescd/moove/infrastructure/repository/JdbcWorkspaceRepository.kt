a51ad83c2272c0ae481fd9b3bc3ef4395f37ed27
Mônica Ribeiro <monicarib1@gmail.com>
2020-07-09 16:48:58 -0300

    Fixing ktlint.
    Signed-off-by: Mônica Ribeiro <monicarib1@gmail.com>

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcWorkspaceRepository.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcWorkspaceRepository.kt
@@ -25,9 +25,9 @@ import io.charlescd.moove.domain.*
 import io.charlescd.moove.domain.repository.WorkspaceRepository
 import io.charlescd.moove.infrastructure.repository.mapper.UserPermissionsExtractor
 import io.charlescd.moove.infrastructure.repository.mapper.WorkspaceExtractor
+import java.util.*
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 class JdbcWorkspaceRepository(
