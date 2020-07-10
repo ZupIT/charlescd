d8b2eaa337813ee21e4c9263ea0525a272ae7384
Leandro Queiroz <leandroqueiroz92@gmail.com>
2020-07-10 11:34:20 -0300

    test invalid create release component
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- ui/src/modules/Circles/Release/Create/__tests__/CreateRelease.spec.tsx
+++ ui/src/modules/Circles/Release/Create/__tests__/CreateRelease.spec.tsx
@@ -25,30 +25,33 @@ import CreateRelease from '../index';
 
 jest.mock('lodash/debounce', () => jest.fn(fn => fn));
 
-test('form should be valid', async () => {
-  (fetch as FetchMock).mockResponseOnce(JSON.stringify({
-    content: [
-      {
-        id: 'module-1',
-        name: 'module-1',
-        components: [
-          {
-            id: 'component-1',
-            name: 'component-1'
-          }
-        ]
-      }
-    ]
-  }));
+const mockGetModules = JSON.stringify({
+  content: [
+    {
+      id: 'module-1',
+      name: 'module-1',
+      components: [
+        {
+          id: 'component-1',
+          name: 'component-1'
+        }
+      ]
+    }
+  ]
+});
 
-  (fetch as FetchMock).mockResponseOnce(JSON.stringify(
-    [
-      {
-        name: 'image-1.0.0',
-        artifact: 'domain.com:image-1.0.0'
-      }
-    ]
-  ));
+const mockGetTags = JSON.stringify(
+  [
+    {
+      name: 'image-1.0.0',
+      artifact: 'domain.com:image-1.0.0'
+    }
+  ]
+);
+
+test('form should be valid', async () => {
+  (fetch as FetchMock).mockResponseOnce(mockGetModules);
+  (fetch as FetchMock).mockResponseOnce(mockGetTags);
 
   const { getByTestId, getByText } = render(
     <CreateRelease circleId="123" onDeployed={() => { }} />
@@ -69,4 +72,29 @@ test('form should be valid', async () => {
   const submit = await waitForElement(() => getByTestId('button-default-submit'));
 
   expect(submit).not.toBeDisabled();
+});
+
+test('form should be invalid when version name not found', async () => {
+  (fetch as FetchMock).mockResponseOnce(mockGetModules);
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify([]));
+
+  const { getByTestId, getByText } = render(
+    <CreateRelease circleId="123" onDeployed={() => { }} />
+  );
+  
+  const nameInput = getByTestId('input-text-releaseName');
+  fireEvent.change(nameInput, { target: { value: 'release-name' }});
+  
+  const moduleLabel = getByText('Select a module');
+  await act(async() => selectEvent.select(moduleLabel, 'module-1'));
+
+  const componentLabel = getByText('Select a component');
+  await act(async() => selectEvent.select(componentLabel, 'component-1'));
+
+  const versionInput = getByTestId('input-text-modules[0].version');
+  fireEvent.change(versionInput, { target: { value: 'version-1' }});
+
+  const submit = await waitForElement(() => getByTestId('button-default-submit'));
+
+  expect(submit).toBeDisabled();
 });
