e0cecdeca83b7aaf0d6649b91e55880d5f85c2f0
Thalles Freitas <thalles.freitas@zup.com.br>
2020-07-14 14:35:35 -0300

    improve
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- butler/src/app/api/deployments/services/modules.service.ts
+++ butler/src/app/api/deployments/services/modules.service.ts
@@ -25,7 +25,9 @@ export class ModulesService {
 
   constructor(
         @InjectRepository(ModuleEntity)
-        private readonly moduleEntityRepository: Repository<ModuleEntity>
+        private readonly moduleEntityRepository: Repository<ModuleEntity>,
+        @InjectRepository(ComponentEntity)
+        private readonly componentEntityRepository: Repository<ComponentEntity>
   ) { }
 
   public async createModules(moduleEntities: ModuleEntity[]): Promise<void> {
@@ -45,7 +47,20 @@ export class ModulesService {
     if (module && newComponents.length === 0) {
       return
     }
-    await this.moduleEntityRepository.save(moduleEntity)
+
+    if (!module) {
+      await this.moduleEntityRepository.save(moduleEntity)
+    }else{
+      newComponents.forEach(
+        newComponent => this.updateAndSaveComponent(newComponent,module)
+      )
+    }
+
+  }
+
+  private updateAndSaveComponent(newComponent: ComponentEntity, module: ModuleEntity) {
+    newComponent.module = module
+    this.componentEntityRepository.save(newComponent)
   }
 
 }
