d6357f3a346664764821cc4a2039c9b9dde7cdb2
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-07-17 16:31:47 -0300

    Fix jdbc component snapshot
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcBuildRepository.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcBuildRepository.kt
@@ -546,6 +546,8 @@ class JdbcBuildRepository(private val jdbcTemplate: JdbcTemplate, private val bu
                    component_snapshots.created_at           AS component_snapshot_created_at,
                    component_snapshots.workspace_id         AS component_snapshot_workspace_id,
                    component_snapshots.module_snapshot_id   AS component_snapshot_module_snapshot_id,
+                   component_snapshots.host_value           AS component_snapshot_host_value,
+                   component_snapshots.gateway_name         AS component_snapshot_gateway_name,
                    artifact_snapshots.id                    AS artifact_snapshot_id,
                    artifact_snapshots.artifact              AS artifact_snapshot_artifact,
                    artifact_snapshots.version               AS artifact_snapshot_version,
