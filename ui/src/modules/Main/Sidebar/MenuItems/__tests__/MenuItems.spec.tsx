8625d536568a2bad9013e95769942f3436b86723
luisaborgeszup <luisa.borges@zup.com.br>
2020-07-17 09:53:38 -0300

    Improving tests
    Signed-off-by: luisaborgeszup <luisa.borges@zup.com.br>

--- ui/src/modules/Main/Sidebar/MenuItems/__tests__/MenuItems.spec.tsx
+++ ui/src/modules/Main/Sidebar/MenuItems/__tests__/MenuItems.spec.tsx
@@ -15,82 +15,67 @@
  */
 
 import React from 'react';
-import { render, wait, fireEvent } from 'unit-test/testUtils';
+import { render, fireEvent, wait } from 'unit-test/testUtils';
 import routes from 'core/constants/routes';
 import { genMenuId } from 'core/utils/menu';
-import Sidebar from '../index';
+import MenuItems from '../index';
 
-const originalWindow = { ...window };
-
-beforeEach(() => {
-  delete window.location;
-
-  window.location = {
-    ...window.location,
-    pathname: routes.workspaces
-  };
-});
-
-afterEach(() => {
-  window = originalWindow;
-});
-
-test('renders sidebar component', () => {
+test('renders sidebar menu Items', async () => {
   const { getByTestId } = render(
-    <Sidebar isExpanded={true} onClickExpand={null} />
+    <MenuItems isExpanded expandMenu={() => jest.fn()} />
   );
+
   const links = getByTestId('sidebar-links');
 
   const workspacesId = genMenuId(routes.workspaces);
   const accountId = genMenuId(routes.account);
-
-  wait();
 
   expect(getByTestId(workspacesId)).toBeInTheDocument();
   expect(getByTestId(accountId)).toBeInTheDocument();
   expect(links.children.length).toBe(3);
 });
 
-test('renders sidebar component with selected workspace', () => {
-  const { getByTestId, getByText } = render(
-    <Sidebar isExpanded={true} onClickExpand={null} selectedWorkspace="test" />
-  );
+test('testing outside click menu Items', async () => {
+  const onOutSideCick = jest.fn();
+  const props = {
+    isExpanded: true
+  };
 
+  const { getByTestId } = render(
+    <div onClick={onOutSideCick} data-testid="external-div">
+      <MenuItems isExpanded={props.isExpanded} expandMenu={() => jest.fn()} />
+    </div>
+  );
+  const externalDiv = getByTestId('external-div');
   const links = getByTestId('sidebar-links');
-  const workspaceName = getByText('test');
 
   const workspacesId = genMenuId(routes.workspaces);
   const accountId = genMenuId(routes.account);
 
-  expect(workspaceName).toBeInTheDocument();
   expect(getByTestId(workspacesId)).toBeInTheDocument();
   expect(getByTestId(accountId)).toBeInTheDocument();
   expect(links.children.length).toBe(3);
+  fireEvent.click(externalDiv);
+  wait(() => expect(props.isExpanded).toBeTruthy());
 });
 
-test('renders sidebar component expanded', () => {
+test('testing expand menu click', async () => {
   const onClickExpand = jest.fn();
 
-  const { getByTestId, getByText } = render(
-    <Sidebar
-      isExpanded={true}
-      onClickExpand={onClickExpand}
-      selectedWorkspace="test"
-    />
+  const isExpanded = false;
+
+  const { getByTestId } = render(
+    <MenuItems isExpanded={isExpanded} expandMenu={onClickExpand} />
   );
-
   const links = getByTestId('sidebar-links');
-  const workspaceName = getByText('test');
-
   const workspacesId = genMenuId(routes.workspaces);
   const accountId = genMenuId(routes.account);
 
-  expect(workspaceName).toBeInTheDocument();
   expect(getByTestId(workspacesId)).toBeInTheDocument();
   expect(getByTestId(accountId)).toBeInTheDocument();
   expect(links.children.length).toBe(3);
 
-  fireEvent.click(links.children[0]);
+  fireEvent.click(links.children[1]);
 
   expect(onClickExpand).toHaveBeenCalled();
 });
