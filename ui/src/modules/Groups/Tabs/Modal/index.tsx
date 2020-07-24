2f415cd8358abc74c2f9e73b30577181e8463851
luisaborgeszup <luisa.borges@zup.com.br>
2020-07-24 12:05:42 -0300

    Modal user group correction
    Signed-off-by: luisaborgeszup <luisa.borges@zup.com.br>

--- ui/src/modules/Groups/Tabs/Modal/index.tsx
+++ ui/src/modules/Groups/Tabs/Modal/index.tsx
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import map from 'lodash/map';
 import debounce from 'lodash/debounce';
 import Icon from 'core/components/Icon';
 import { User } from 'modules/Users/interfaces/User';
 import { UserChecked } from '../../interfaces/UserChecked';
 import Styled from './styled';
+import useOutsideClick from 'core/hooks/useClickOutside';
+import Button from 'core/components/Button';
 
 interface UserItemProps extends User {
   checked: boolean;
-  onSelected: (id: string, checked: boolean) => void;
+  onSelected: (id?: string, checked?: boolean) => void;
 }
 
 interface UserCheckedProps {
@@ -32,12 +34,14 @@ interface UserCheckedProps {
   onSelected: (id: string, checked: boolean) => void;
 }
 
-interface Props {
+export interface Props {
   users: UserChecked[];
   isOpen: boolean;
-  onClose: () => void;
+  onClose?: () => void;
   onSearch: (name: string) => void;
   onSelected: (id: string, checked: boolean) => void;
+  className?: string;
+  isOutsideClick?: boolean;
 }
 
 const MemberChecked = ({ checked }: UserCheckedProps) => (
@@ -57,43 +61,111 @@ const UserItem = ({
   photoUrl,
   checked,
   onSelected
-}: UserItemProps) => (
-  <Styled.Item.Wrapper onClick={() => onSelected(id, checked)}>
-    <Styled.Item.Profile>
-      <Styled.Item.Photo src={photoUrl} name={name} />
-      <div>
-        <Styled.Item.Name>{name}</Styled.Item.Name>
-        <Styled.Item.Email>{email}</Styled.Item.Email>
-      </div>
-    </Styled.Item.Profile>
-    <MemberChecked checked={checked} onSelected={onSelected} />
-  </Styled.Item.Wrapper>
-);
+}: UserItemProps) => {
+  const userItemRef = useRef<HTMLDivElement>();
+
+  const [isSelected, setIsSelected] = useState(false);
+
+  useOutsideClick(userItemRef, () => {
+    setIsSelected(false);
+  });
+
+  const handleSelected = (id: string, checked: boolean) => {
+    onSelected(id, checked);
+    setIsSelected(true);
+  };
+
+  return (
+    <Styled.Item.Wrapper
+      onClick={() => handleSelected(id, checked)}
+      isSelected={isSelected}
+      ref={userItemRef}
+    >
+      <Styled.Item.Profile>
+        <Styled.Item.Photo src={photoUrl} name={name} />
+        <div>
+          <Styled.Item.Name>{name}</Styled.Item.Name>
+          <Styled.Item.Email>{email}</Styled.Item.Email>
+        </div>
+      </Styled.Item.Profile>
+      <MemberChecked checked={checked} onSelected={onSelected} />
+    </Styled.Item.Wrapper>
+  );
+};
 
 const AddUserModal = ({
   users,
   isOpen,
   onClose,
   onSearch,
-  onSelected
+  className,
+  onSelected,
+  isOutsideClick
 }: Props) => {
+  const [userId, setUserId] = useState('');
+  const [userChecked, setUserChecked] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+
   const handleChange = debounce(onSearch, 500);
 
+  const modalRef = useRef<HTMLDivElement>();
+  const contentRef = useRef<HTMLDivElement>();
+
+  useOutsideClick(modalRef, () => {
+    if (isOutsideClick) {
+      onClose();
+    }
+  });
+
+  useOutsideClick(contentRef, () => setIsDisabled(true));
+
+  const setSelected = (id: string, checked: boolean) => {
+    setUserId(id);
+    setUserChecked(checked);
+    setIsDisabled(false);
+  };
+
   return (
-    <Styled.Wrapper isOpen={isOpen} onClose={onClose}>
-      <Styled.Header>
-        Add or remove people in this user group
-        <Styled.Label>Add a user:</Styled.Label>
-        <Styled.Search
-          label="Enter the user's email"
-          onChange={e => handleChange(e.currentTarget.value)}
-        />
-      </Styled.Header>
-      <Styled.Content>
-        {map(users, user => (
-          <UserItem key={user.id} {...user} onSelected={onSelected} />
-        ))}
-      </Styled.Content>
+    <Styled.Wrapper
+      data-testid="modal-user"
+      className={className}
+      isOpen={isOpen}
+    >
+      <Styled.Background className="modal-user-background" />
+      <Styled.Dialog className="modal-user-dialog" ref={modalRef}>
+        <Styled.Container className="modal-user-content">
+          <Styled.Button.Close>
+            <Icon name="cancel" color="dark" size="22px" onClick={onClose} />
+          </Styled.Button.Close>
+          <Styled.Header>
+            Add or remove people in this user group
+            <Styled.Label>Add a user:</Styled.Label>
+            <Styled.Search
+              label="Enter the user's email"
+              onChange={e => handleChange(e.currentTarget.value)}
+            />
+          </Styled.Header>
+          <Styled.Content ref={contentRef}>
+            {map(users, user => (
+              <UserItem
+                key={user.id}
+                {...user}
+                onSelected={(id: string, checked: boolean) =>
+                  setSelected(id, checked)
+                }
+              />
+            ))}
+          </Styled.Content>
+          <Styled.Button.Update>
+            <Button.Default
+              isDisabled={isDisabled}
+              onClick={() => onSelected(userId, userChecked)}
+            >
+              Update
+            </Button.Default>
+          </Styled.Button.Update>
+        </Styled.Container>
+      </Styled.Dialog>
     </Styled.Wrapper>
   );
 };
