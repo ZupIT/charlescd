ff040092f6217d59d590e52d476dffe5cb99665b
Mateus Cruz <emiteze@hotmail.com>
2020-07-24 11:21:36 -0300

    Fixing ktlint
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

--- moove/application/src/main/kotlin/io/charlescd/moove/application/circle/request/CreateCircleRequest.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/circle/request/CreateCircleRequest.kt
@@ -26,8 +26,8 @@ import java.time.LocalDateTime
 import java.util.*
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 class CreateCircleRequest(
 
