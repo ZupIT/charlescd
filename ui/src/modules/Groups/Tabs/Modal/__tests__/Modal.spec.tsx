e3ef143e325aa5bc65c900a5e3a6ecc5830dfdbc
luisaborgeszup <luisa.borges@zup.com.br>
2020-07-27 09:17:02 -0300

    Modal layout corrected
    Signed-off-by: luisaborgeszup <luisa.borges@zup.com.br>

---
+++ ui/src/modules/Groups/Tabs/Modal/__tests__/Modal.spec.tsx
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { render, wait, fireEvent } from 'unit-test/testUtils';
+import { users } from './fixtures';
+import Modal from '..';
+
+test('render users Modal', async () => {
+  const { getByTestId } = render(
+    <Modal users={users} isOpen onSearch={jest.fn()} onSelected={jest.fn()} />
+  );
+
+  const element = getByTestId('modal-user');
+  const button = getByTestId('button-default-undefined');
+
+  await wait(() => expect(element).toBeInTheDocument());
+  await wait(() => expect(button).toBeInTheDocument());
+});
+
+test('testing on selected', async () => {
+  const onSelected = jest.fn();
+  const { getByText, getByTestId } = render(
+    <Modal users={users} isOpen onSearch={jest.fn()} onSelected={onSelected} />
+  );
+
+  const user = getByText('User 2');
+  const button = getByTestId('button-default-undefined');
+
+  await wait(() => expect(user).toBeInTheDocument());
+  await wait(() => expect(button).toBeInTheDocument());
+
+  fireEvent.click(user);
+  fireEvent.click(button);
+
+  expect(onSelected).toHaveBeenCalled();
+});
+
+test('testing on outside click with isOutsideClick property passed on', async () => {
+  const onClose = jest.fn();
+  const { getByTestId } = render(
+    <div data-testid="external-div">
+      <Modal
+        users={users}
+        isOpen
+        onSearch={jest.fn()}
+        onSelected={jest.fn()}
+        onClose={onClose}
+        isOutsideClick
+      />
+    </div>
+  );
+
+  const element = getByTestId('modal-user');
+  const externalDiv = getByTestId('external-div');
+
+  await wait(() => expect(element).toBeInTheDocument());
+  await wait(() => expect(externalDiv).toBeInTheDocument());
+
+  fireEvent.click(externalDiv);
+
+  expect(onClose).toHaveBeenCalled();
+});
+
+test('testing on outside click without isOutsideClick property passed on', async () => {
+  const { getByTestId } = render(
+    <div data-testid="external-div">
+      <Modal
+        users={users}
+        isOpen
+        onSearch={jest.fn()}
+        onSelected={jest.fn()}
+        onClose={jest.fn()}
+      />
+    </div>
+  );
+
+  const element = getByTestId('modal-user');
+  const externalDiv = getByTestId('external-div');
+  const button = getByTestId('button-default-undefined');
+
+  await wait(() => expect(element).toBeInTheDocument());
+  await wait(() => expect(externalDiv).toBeInTheDocument());
+
+  fireEvent.click(externalDiv);
+
+  expect(button).toHaveAttribute('disabled');
+});
