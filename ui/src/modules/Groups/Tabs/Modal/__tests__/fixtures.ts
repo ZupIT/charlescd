8d273e3b95e3fc47e679f230f4c8f6465e8c772b
luisaborgeszup <luisa.borges@zup.com.br>
2020-07-27 09:17:02 -0300

    Modal layout corrected
    Signed-off-by: luisaborgeszup <luisa.borges@zup.com.br>

---
+++ ui/src/modules/Groups/Tabs/Modal/__tests__/fixtures.ts
@@ -0,0 +1,24 @@
+export const users = [
+  {
+    id: 'a7c3e4b6-4be3-4d62-8140-e2d23214e03f',
+    name: 'User 2',
+    email: 'user.2@zup.com.br',
+    photoUrl:
+      'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png',
+    checked: true
+  },
+  {
+    id: '13ea193b-f9d2-4wed-b1ce-471a7ae871c2',
+    name: 'User 3',
+    email: 'user.3@zup.com.br',
+    photoUrl: '',
+    checked: false
+  },
+  {
+    id: '8b81e7a7-33f1-46cb-aedf-73222bf8769f',
+    name: 'User 4',
+    email: 'user.4@zup.com.br',
+    photoUrl: '',
+    checked: true
+  }
+];
