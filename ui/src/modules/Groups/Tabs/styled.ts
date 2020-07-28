911a8130181d21016cbd5f86716c8ece29c085be
Douglas Fernandes <douglas.fernandes@zup.com.br>
2020-07-28 18:21:34 -0300

    Initial modal structure

--- ui/src/modules/Groups/Tabs/styled.ts
+++ ui/src/modules/Groups/Tabs/styled.ts
@@ -16,6 +16,7 @@
 
 import styled from 'styled-components';
 import Page from 'core/components/Page';
+import Modal from 'core/components/Modal';
 
 const ScrollX = styled(Page.Content)`
   overflow-y: hidden;
@@ -37,11 +38,16 @@ const Actions = styled.div`
   }
 `;
 
+const ModalPassword = styled(Modal.Default)`
+  min-height: 282px;
+`;
+
 const Tab = styled.div``;
 
 export default {
   ScrollX,
   Wrapper,
   Actions,
+  ModalPassword,
   Tab
 };
