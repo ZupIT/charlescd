a926e810163ba46b0651b3e2d55cf79cf05cbf30
bCatanant <bruno.catanant@zup.com.br>
2020-07-29 17:55:58 -0300

    Fix braces and adding testing
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

--- ui/src/modules/Metrics/Deploys/Release/Summary.tsx
+++ ui/src/modules/Metrics/Deploys/Release/Summary.tsx
@@ -39,15 +39,15 @@ const Summary = ({ legend, isLoading }: Props) => {
         </div>
       ) : (
         <Styled.ReleaseHistoryLegend>
-          <Styled.Dot status={'deployed'} />
+          <Styled.Dot status="deployed" />
           <Text.h5 color="dark">Deployed: {legend?.deployed}</Text.h5>
-          <Styled.Dot status={'deploying'} />
+          <Styled.Dot status="deploying" />
           <Text.h5 color="dark">Deploying: {legend?.deploying}</Text.h5>
-          <Styled.Dot status={'error'} />
+          <Styled.Dot status="error" />
           <Text.h5 color="dark">Error: {legend?.failed}</Text.h5>
-          <Styled.Dot status={'notDeployed'} />
+          <Styled.Dot status="notDeployed" />
           <Text.h5 color="dark">Undeployed: {legend?.notDeployed}</Text.h5>
-          <Styled.Dot status={'undeploying'} />
+          <Styled.Dot status="undeploying" />
           <Text.h5 color="dark">Undeploying: {legend?.undeploying}</Text.h5>
         </Styled.ReleaseHistoryLegend>
       )}
