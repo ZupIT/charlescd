244958d85d1e53312e65b63a929831655ea27829
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2020-07-28 21:34:11 -0300

    Organize code
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

---
+++ butler/src/app/v2/core/integrations/spinnaker/interfaces/deployment.interface.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DeploymentStatusEnum } from '../../../../../v1/api/deployments/enums'
+import { Component } from './component.interface'
+import { CdConfiguration } from './index'
+
+export interface Deployment {
+    id: string
+
+    authorId: string
+
+    callbackUrl: string
+
+    status: DeploymentStatusEnum
+
+    createdAt: Date
+
+    finishedAt: Date | null
+
+    cdConfiguration: CdConfiguration
+
+    circleId: string | null
+
+    components?: Component[]
+}
--- butler/src/app/v2/interfaces/connector-result.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface ConnectorResult {
-    status: string
-}
