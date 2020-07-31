40491b3703257393b692591d008bc419220e0ea5
e-km <eduardo.miyoshi@zup.com.br>
2020-07-31 09:25:33 -0300

    remove unsed class from KeycloakCustomClientService constructor

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/service/KeycloakCustomClientService.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/service/KeycloakCustomClientService.kt
@@ -2,12 +2,11 @@ package io.charlescd.moove.infrastructure.service
 
 import io.charlescd.moove.domain.service.KeycloakCustomService
 import io.charlescd.moove.infrastructure.service.client.KeycloakCustomClient
-import org.keycloak.admin.client.Keycloak
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class KeycloakCustomClientService(val keycloakCustomClient: KeycloakCustomClient, val keycloak: Keycloak) : KeycloakCustomService {
+class KeycloakCustomClientService(val keycloakCustomClient: KeycloakCustomClient) : KeycloakCustomService {
 
     @Value("\${charlescd.keycloak.realm}")
     lateinit var realm: String
