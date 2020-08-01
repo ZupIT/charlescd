ac9909d524387497c6965472b7e7b9dd143f0865
e-km <eduardo.miyoshi@zup.com.br>
2020-08-01 08:05:31 -0300

    Refactoring Password Generator

--- moove/application/src/main/kotlin/io/charlescd/moove/application/UserPasswordGeneratorService.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/UserPasswordGeneratorService.kt
@@ -1,30 +1,24 @@
 package io.charlescd.moove.application
 
 import javax.inject.Named
-import org.passay.AllowedCharacterRule
-import org.passay.CharacterRule
-import org.passay.EnglishCharacterData
-import org.passay.PasswordGenerator
 
 @Named
-class UserPasswordGeneratorService {
+class UserPasswordGeneratorService(
+    val numberLowerCase: Int? = 2,
+    val numberUpperCase: Int? = 2,
+    val numberDigits: Int? = 2,
+    val numberSpecialChars: Int? = 2,
+    val passwordLength: Int? = 10
+) {
     fun create(): String {
-        val rules = listOf(
-            CharacterRule(EnglishCharacterData.LowerCase, 2),
-            CharacterRule(EnglishCharacterData.UpperCase, 2),
-            CharacterRule(EnglishCharacterData.Digit, 2),
-            CharacterRule(SpecialChars(), 2)
-        )
-        return PasswordGenerator().generatePassword(10, rules)
-    }
-}
+        val passwordGen: PasswordGen = PasswordGen.Builder()
+            .numberDigits(numberDigits!!)
+            .numberLowerCase(numberLowerCase!!)
+            .numberUpperCase(numberUpperCase!!)
+            .numberSpecialChars(numberSpecialChars!!)
+            .passwordLength(passwordLength!!)
+            .build()
 
-class SpecialChars : org.passay.CharacterData {
-    override fun getErrorCode(): String {
-        return AllowedCharacterRule.ERROR_CODE
-    }
-
-    override fun getCharacters(): String {
-        return "!@#$^*()_"
+        return passwordGen.create()
     }
 }
