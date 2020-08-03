bd7c4a939d774f4f156c32ce78450d4ec6129198
Mateus Cruz <emiteze@hotmail.com>
2020-08-03 14:12:27 -0300

    Fixing imports of requests and responses
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/service/client/request/DeployRequest.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/service/client/request/DeployRequest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.charlescd.moove.infrastructure.service.client
+package io.charlescd.moove.infrastructure.service.client.request
 
 data class DeployRequest(
     val deploymentId: String,
