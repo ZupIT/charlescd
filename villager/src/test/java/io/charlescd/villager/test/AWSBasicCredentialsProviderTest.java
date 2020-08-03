07251c5f0a9e679a9d65acfda3780db587ae1c99
LucasSales <lucas.sales@zup.com.br>
2020-08-03 16:01:25 -0300

    aws basic auth refresh test
    Signed-off-by: LucasSales <lucas.sales@zup.com.br>

--- villager/src/test/java/io/charlescd/villager/test/AWSBasicCredentialsProviderTest.java
+++ villager/src/test/java/io/charlescd/villager/test/AWSBasicCredentialsProviderTest.java
@@ -2,6 +2,7 @@ package io.charlescd.villager.test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 import io.charlescd.villager.infrastructure.integration.registry.authentication.AWSBasicCredentialsProvider;
@@ -13,6 +14,19 @@ public class AWSBasicCredentialsProviderTest {
       final var accessKey = "AYSGAJSHAGSJAHS";
       final var secretKey = "DKLJHFfdLKDSJFHLKDSJFHLKSDJF";
       final var authentication = new AWSBasicCredentialsProvider(accessKey, secretKey).getCredentials();
+
+      assertThat(authentication.getAWSAccessKeyId(), is(accessKey));
+      assertThat(authentication.getAWSSecretKey(), is(secretKey));
+    }
+
+    @Test
+    public void testRefreshBasicCredentials() {
+      final var accessKey = "AYSGAJSHAGSJAHS";
+      final var secretKey = "DKLJHFfdLKDSJFHLKDSJFHLKSDJF";
+      final var authenticationProvider = new AWSBasicCredentialsProvider(accessKey, secretKey);
+      final var authentication = authenticationProvider.getCredentials();
+      // updates credentials, in some implementations this method must be called from time to time. In this specific implementation, refresh has no use.
+      authenticationProvider.refresh();
 
       assertThat(authentication.getAWSAccessKeyId(), is(accessKey));
       assertThat(authentication.getAWSSecretKey(), is(secretKey));
