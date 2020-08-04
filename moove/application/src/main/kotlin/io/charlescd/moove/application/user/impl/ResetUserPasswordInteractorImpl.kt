f4a2457ecf08096ec80b548427e713b757092211
e-km <eduardo.miyoshi@zup.com.br>
2020-08-04 10:49:03 -0300

    keycloak reset password by id

--- moove/application/src/main/kotlin/io/charlescd/moove/application/user/impl/ResetUserPasswordInteractorImpl.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/user/impl/ResetUserPasswordInteractorImpl.kt
@@ -1,6 +1,7 @@
 package io.charlescd.moove.application.user.impl
 
 import io.charlescd.moove.application.UserPasswordGeneratorService
+import io.charlescd.moove.application.UserService
 import io.charlescd.moove.application.user.ResetUserPasswordInteractor
 import io.charlescd.moove.application.user.response.UserNewPasswordResponse
 import io.charlescd.moove.domain.service.KeycloakService
@@ -10,11 +11,13 @@ import javax.inject.Named
 @Named
 class ResetUserPasswordInteractorImpl(
     private val passGenerator: UserPasswordGeneratorService,
-    private val keycloakService: KeycloakService
+    private val keycloakService: KeycloakService,
+    private val userService: UserService
 ) : ResetUserPasswordInteractor {
     override fun execute(id: UUID): UserNewPasswordResponse {
         val newPassword = passGenerator.create()
-        keycloakService.resetPassword(id.toString(), newPassword)
+        val user = userService.find(id.toString())
+        keycloakService.resetPassword(user.email, newPassword)
         return UserNewPasswordResponse(newPassword)
     }
 }
