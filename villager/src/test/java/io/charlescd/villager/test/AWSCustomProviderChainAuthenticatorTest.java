eccdfdf8df542d8562b459c50d91b6f391a7985c
LucasSales <lucas.sales@zup.com.br>
2020-08-04 11:45:08 -0300

    remove unused imports
    Signed-off-by: LucasSales <lucas.sales@zup.com.br>

--- villager/src/test/java/io/charlescd/villager/test/AWSCustomProviderChainAuthenticatorTest.java
+++ villager/src/test/java/io/charlescd/villager/test/AWSCustomProviderChainAuthenticatorTest.java
@@ -1,22 +1,32 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.charlescd.villager.test;
 
 import com.amazonaws.services.ecr.AmazonECR;
-import com.amazonaws.services.ecr.AmazonECRClientBuilder;
 import com.amazonaws.services.ecr.model.AuthorizationData;
-import com.amazonaws.services.ecr.model.GetAuthorizationTokenRequest;
 import com.amazonaws.services.ecr.model.GetAuthorizationTokenResult;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 
 import java.util.ArrayList;
