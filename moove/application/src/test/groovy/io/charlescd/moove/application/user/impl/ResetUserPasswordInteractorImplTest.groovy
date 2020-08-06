0f082e20507662730ed68ce4d80a63b4de7124c8
e-km <eduardo.miyoshi@zup.com.br>
2020-08-06 13:49:40 -0300

    user reset password refactoring

---
+++ moove/application/src/test/groovy/io/charlescd/moove/application/user/impl/ResetUserPasswordInteractorImplTest.groovy
@@ -0,0 +1,78 @@
+package io.charlescd.moove.application.user.impl
+
+import io.charlescd.moove.application.UserPasswordGeneratorService
+import io.charlescd.moove.application.UserService
+import io.charlescd.moove.application.user.ResetUserPasswordInteractor
+import io.charlescd.moove.domain.User
+import io.charlescd.moove.domain.repository.UserRepository
+import io.charlescd.moove.domain.service.KeycloakService
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+
+class ResetUserPasswordInteractorImplTest extends Specification {
+
+    private ResetUserPasswordInteractor resetUserPasswordInteractor
+
+    private KeycloakService keycloakService = Mock(KeycloakService)
+
+    private UserRepository userRepository = Mock(UserRepository)
+
+    /**
+     * ^                 # start-of-string
+     * (?=.*[0-9])       # a digit must occur at least once
+     * (?=.*[a-z])       # a lower case letter must occur at least once
+     * (?=.*[A-Z])       # an upper case letter must occur at least once
+     * (?=.*[@#$%^&+=])  # a special character must occur at least once
+     * (?=\S+$)          # no whitespace allowed in the entire string
+     * .{8,}             # anything, at least eight places though
+     * $                 # end-of-string
+     */
+    private static final String PASSWORD_CHECK = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$^*()_])(?=\\S+\$).{8,}\$"
+
+    void setup() {
+        resetUserPasswordInteractor = new ResetUserPasswordInteractorImpl(new UserPasswordGeneratorService(), keycloakService, new UserService(userRepository))
+    }
+
+    def "should generate a valid password"() {
+        given:
+        def matchValidation = PASSWORD_CHECK
+        def userId = UUID.randomUUID()
+        def user = new User(userId.toString(), "user name", "user@zup.com.br", "http://image.com.br/photo.png",
+                [], false, LocalDateTime.now())
+
+        when:
+        def response = resetUserPasswordInteractor.execute(userId)
+
+        then:
+        1 * userRepository.findById(userId.toString()) >> Optional.of(user)
+        1 * keycloakService.resetPassword(user.getEmail(), _)
+        assert response != null
+        assert response.newPassword.size() == 10
+        assert response.newPassword.matches(matchValidation)
+
+    }
+
+    def "should generate 10 valid passwords"() {
+        given:
+        def matchValidation = PASSWORD_CHECK
+        def numberOfPasswords = 10
+        def userId = UUID.randomUUID()
+        def user = new User(userId.toString(), "user name", "user@zup.com.br", "http://image.com.br/photo.png",
+                [], false, LocalDateTime.now())
+
+        when:
+        def passwords = []
+        for (i in 1..numberOfPasswords) {
+            passwords.add(resetUserPasswordInteractor.execute(userId))
+        }
+
+        then:
+        numberOfPasswords * userRepository.findById(userId.toString()) >> Optional.of(user)
+        numberOfPasswords * keycloakService.resetPassword(user.getEmail(), _)
+        passwords.stream().forEach({ response ->
+            assert response.newPassword.size() == 10
+            assert response.newPassword.matches(matchValidation)
+        })
+    }
+}
