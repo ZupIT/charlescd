456859cd2e8ab10d306bc5d62d84d7b1be0154b3
arthur pucci <arthur.pucci@zup.com.br>
2020-08-06 19:43:01 -0300

    fixing migrations and adding delete method to datasource
    Signed-off-by: arthur pucci <arthur.pucci@zup.com.br>

--- compass/datasource/datasource.go
+++ compass/datasource/datasource.go
@@ -1,6 +1,10 @@
 package datasource
 
-import "compass/util"
+import (
+	"compass/util"
+
+	"github.com/jinzhu/gorm"
+)
 
 type DataSource struct {
 	util.BaseModel
@@ -18,4 +22,21 @@ func (main Main) FindAllByWorkspace(workspaceID string) ([]DataSource, error) {
 		return []DataSource{}, db.Error
 	}
 	return dataSources, nil
+}
+
+func findById(id string, db *gorm.DB) (DataSource, error) {
+	dataSource := DataSource{}
+	result := db.Where("id = ?", id).Find(&dataSource)
+	if result.Error != nil {
+		return DataSource{}, db.Error
+	}
+	return dataSource, nil
+}
+
+func (main Main) Delete(id string, workspaceID string) error {
+	db := main.db.Where("id = ? AND worspace_id = ?", id, workspaceID).Delete(DataSource{})
+	if db.Error != nil {
+		return db.Error
+	}
+	return nil
 }
