000311b10a34e04fbad063de59f1c66a07ea12b9
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-08-07 17:09:07 -0300

    Create datasource plugin relation
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- compass/web/api/v1/datasource.go
+++ compass/web/api/v1/datasource.go
@@ -4,7 +4,6 @@ import (
 	"compass/internal/datasource"
 	"compass/web/api"
 	"errors"
-	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -21,7 +20,7 @@ type DataSourceApi struct {
 }
 
 func (v1 V1) NewDataSourceApi(dataSourceMain datasource.UseCases) DataSourceApi {
-	apiPath := "/datasource"
+	apiPath := "/datasources"
 	dataSourceAPI := DataSourceApi{dataSourceMain}
 	v1.Router.GET(v1.getCompletePath(apiPath), api.HttpValidator(dataSourceAPI.findAllByWorkspace))
 	v1.Router.POST(v1.getCompletePath(apiPath), api.HttpValidator(dataSourceAPI.create))
@@ -34,19 +33,10 @@ func (v1 V1) NewDataSourceApi(dataSourceMain datasource.UseCases) DataSourceApi 
 func (dataSourceApi DataSourceApi) findAllByWorkspace(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	dataSources, dbErr := dataSourceApi.dataSourceMain.FindAllByWorkspace(r.Header.Get("workspaceId"))
 	if dbErr != nil {
-		log.Print(dbErr)
 		api.NewRestError(w, http.StatusInternalServerError, errors.New("Error doing the process"))
 		return
 	}
 
-	/* dataSourcesRepresentation, parseErr := parse(dataSources)
-
-	if parseErr != nil {
-		log.Print(parseErr)
-		api.NewRestError(w, http.StatusInternalServerError, errors.New("Error doing the process"))
-		return
-	}
-	*/
 	api.NewRestSuccess(w, http.StatusOK, dataSources)
 }
 
