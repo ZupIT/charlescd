5dab3764fe1d332338e1098e2a696122fd496b7a
arthur pucci <arthur.pucci@zup.com.br>
2020-08-07 11:49:43 -0300

    set as health api
    Signed-off-by: arthur pucci <arthur.pucci@zup.com.br>

--- compass/datasource/main.go
+++ compass/datasource/main.go
@@ -1,17 +1,15 @@
 package datasource
 
 import (
-	"io"
-
 	"github.com/jinzhu/gorm"
 )
 
 type UseCases interface {
-	Parse(dataSource io.ReadCloser) (DataSource, error)
+	// Parse(dataSource io.ReadCloser) (DataSource, error)
 	FindAllByWorkspace(workspaceID string) ([]DataSource, error)
 	Delete(id string, workspaceID string) error
-	Save(dataSource DataSource) (DataSource, error)
-	// FindById(id string) (DataSource, error)
+	// Save(dataSource DataSource) (DataSource, error)
+	SetAsHealth(id string, workspaceID string) error
 }
 
 type Main struct {
