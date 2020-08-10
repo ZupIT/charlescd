b81f9f67de04038f317bb276fde3cc4e31d996b7
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-08-10 17:47:41 -0300

    Create metric endpoints
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- compass/internal/metricsgroup/main.go
+++ compass/internal/metricsgroup/main.go
@@ -8,11 +8,15 @@ import (
 
 type UseCases interface {
 	Parse(metricsGroup io.ReadCloser) (MetricsGroup, error)
+	ParseMetric(metric io.ReadCloser) (Metric, error)
 	FindAll() ([]MetricsGroup, error)
 	Save(metricsGroup MetricsGroup) (MetricsGroup, error)
+	SaveMetric(metric Metric) (Metric, error)
 	FindById(id string) (MetricsGroup, error)
 	Update(id string, metricsGroup MetricsGroup) (MetricsGroup, error)
+	UpdateMetric(id string, metric Metric) (Metric, error)
 	Remove(id string) error
+	RemoveMetric(id string) error
 }
 
 type Main struct {
