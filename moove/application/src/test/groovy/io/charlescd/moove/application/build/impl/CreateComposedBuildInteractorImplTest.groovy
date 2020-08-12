6ac4947dcbcbb60e3341e6c00e5585940d717c3b
Thalles Freitas <thalles.freitas@zup.com.br>
2020-08-12 16:34:44 -0300

    put release name in components on  composed build
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- moove/application/src/test/groovy/io/charlescd/moove/application/build/impl/CreateComposedBuildInteractorImplTest.groovy
+++ moove/application/src/test/groovy/io/charlescd/moove/application/build/impl/CreateComposedBuildInteractorImplTest.groovy
@@ -217,6 +217,66 @@ class CreateComposedBuildInteractorImplTest extends Specification {
         response.features[0].branchName == createComposedBuildRequest.releaseName
     }
 
+    def 'should create componentes snapshots with the release name '() {
+        def workspaceId = '1a58c78a-6acb-11ea-bc55-0242ac130003'
+        def author = getDummyUser()
+        def gitRepositoryAddress = 'http://git-repository-address.com'
+        def helmRepository = 'http://helm-repository.com'
+
+        def createComposedBuildRequest = getDummyCreateComposedBuildRequest(author)
+        def listOfModulesId = new ArrayList()
+        listOfModulesId.add(createComposedBuildRequest.modules[0].id)
+
+        def listOfLabels = new ArrayList()
+        listOfLabels.add(new Label('b803558d-3c70-4321-ace5-ae8317902c05', 'Label name', LocalDateTime.now(), author, '#FFFFFF'))
+
+        def component = new Component('1a58c67c-6acb-11ea-bc55-0242ac130003', '1a58c280-6acb-11ea-bc55-0242ac130003', 'testing-module',
+                LocalDateTime.now(), workspaceId, 10, 01, 'host', 'gateway')
+
+        def listOfComponents = new ArrayList()
+        listOfComponents.add(component)
+
+        def module = new Module('1a58c280-6acb-11ea-bc55-0242ac130003', 'Module name', gitRepositoryAddress,
+                LocalDateTime.now(), helmRepository, author, listOfLabels,
+                new GitConfiguration('68b04fa2-032c-4bdc-9228-6e2e8a5fb791', 'Git Config',
+                        new GitCredentials('Credential address', 'username',
+                                'password', 'Access Token', GitServiceProvider.GITHUB),
+                        LocalDateTime.now(), author, workspaceId), listOfComponents, workspaceId)
+
+        def listOfModules = new ArrayList()
+        listOfModules.add(module)
+
+        when:
+        def response = createComposedBuildInteractor.execute(createComposedBuildRequest, workspaceId)
+        then:
+        1 * userRepository.findById(author.id) >> Optional.of(author)
+        1 * moduleRepository.findByIds(listOfModulesId) >> listOfModules
+        1 * buildRepository.save(_) >> { argument ->
+            def buildSaved = argument[0]
+            assert buildSaved instanceof Build
+            assert buildSaved.features[0].modules[0].components[0].artifact.version == buildSaved.tag
+            assert buildSaved.features[0].modules[0].components[0].artifact.version == createComposedBuildRequest.releaseName
+            return buildSaved
+        }
+        notThrown()
+        response != null
+        response.status == BuildStatusEnum.BUILT.name()
+        response.tag == createComposedBuildRequest.releaseName
+        response.deployments.size() == 0
+        response.features.size() == 1
+        response.features[0].modules.size() == 1
+        response.features[0].modules[0].name == module.name
+        response.features[0].modules[0].gitRepositoryAddress == gitRepositoryAddress
+        response.features[0].modules[0].helmRepository == helmRepository
+        response.features[0].authorId == author.id
+        response.features[0].authorName == author.name
+        response.features[0].branches.size() == 1
+        response.features[0].branches[0] == gitRepositoryAddress + '/tree/' + createComposedBuildRequest.releaseName
+        response.features[0].branchName == createComposedBuildRequest.releaseName
+
+
+    }
+
     private CreateComposedBuildRequest getDummyCreateComposedBuildRequest(User author) {
         def componentRequestList = new ArrayList<CreateComposedBuildRequest.ComponentRequest>()
         componentRequestList.add(new CreateComposedBuildRequest.ComponentRequest('1a58c67c-6acb-11ea-bc55-0242ac130003', 'v-0102', 'Artifact'))
