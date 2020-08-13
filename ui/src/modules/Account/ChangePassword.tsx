3f2a92dfd11bb18694cf90ba1e5dd2ae14e7d0dc
Leandro Queiroz <leandroqueiroz92@gmail.com>
2020-08-13 15:47:46 -0300

    change password api and handle modal on close
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>
    Signed-off-by: Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>

--- ui/src/modules/Account/ChangePassword.tsx
+++ ui/src/modules/Account/ChangePassword.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Text from 'core/components/Text';
 import Button from 'core/components/Button';
 import isEmpty from 'lodash/isEmpty';
@@ -23,7 +23,11 @@ import { validationResolver } from 'core/components/CheckPassword';
 import { useChangePassword } from './hooks/useChangePassword';
 import Styled from './styled';
 
-const ChangePassword = () => {
+interface Props {
+  onSubmit: () => void;
+}
+
+const ChangePassword = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
@@ -39,13 +43,19 @@ const ChangePassword = () => {
   const confirmPass = watch('confirmPassword') as string;
   const { updatePassword, status } = useChangePassword();
 
-  const onSubmit = () => {
+  useEffect(() => {
+    if (status.isResolved) {
+      onSubmit();
+    }
+  }, [status.isResolved, onSubmit]);
+
+  const onSubmitForm = () => {
     const { oldPassword, newPassword } = getValues();
     updatePassword({ oldPassword, newPassword });
   };
 
   return (
-    <Styled.ChangePassword onSubmit={handleSubmit(onSubmit)}>
+    <Styled.ChangePassword onSubmit={handleSubmit(onSubmitForm)}>
       <Text.h2 color="light">Change password</Text.h2>
       <Styled.Modal.Info color="dark">
         Fill in the fields below to change your password:
