15ea36eedb6b18cb1c30a99d9f8f72d43bf34505
Gabriel Leite <gabriel.leite@zup.com.br>
2020-08-14 16:57:11 -0300

    plugin test
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

--- compass/internal/plugin/plugin_test.go
+++ compass/internal/plugin/plugin_test.go
@@ -78,3 +78,55 @@ func (s *Suite) TestFindAll() {
 	require.NoError(s.T(), err)
 	require.Contains(s.T(), res, expected)
 }
+
+func (s *Suite) TestFindById() {
+	var id = uuid.New()
+	var timeNow = time.Now()
+	var (
+		baseModel = util.BaseModel{ID: id, CreatedAt: timeNow}
+		name      = "test-name"
+		src       = "localhost:8080"
+	)
+
+	rows := sqlmock.
+		NewRows([]string{"id", "name", "src", "created_at"}).
+		AddRow(id, name, src, timeNow)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta(
+		`SELECT * FROM "plugins"  WHERE (id = $1) ORDER BY "plugins"."id" ASC LIMIT 1`)).
+		WithArgs(id).
+		WillReturnRows(rows)
+
+	res, err := s.repository.FindById(id.String())
+
+	expected := Plugin{
+		BaseModel: baseModel,
+		Name:      name,
+		Src:       src,
+	}
+
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), res, expected)
+}
+
+func (s *Suite) TestFindByIdError() {
+	var id = uuid.New()
+	var timeNow = time.Now()
+	var (
+		name = "test-name"
+		src  = "localhost:8080"
+	)
+
+	rows := sqlmock.
+		NewRows([]string{"id", "name", "src", "created_at"}).
+		AddRow(id, name, src, timeNow)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta(
+		`SELECT * FROM "plugins"  WHERE (id = $1) ORDER BY "plugins"."id" ASC LIMIT 1`)).
+		WithArgs("1234567").
+		WillReturnRows(rows)
+
+	_, err := s.repository.FindById(id.String())
+
+	require.Error(s.T(), err)
+}
