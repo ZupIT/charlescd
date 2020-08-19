2864d37115a0a91b9c03edb34bc19c6221458bc6
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2020-08-19 15:00:00 -0300

    Remove hardcoded user/pass (#404)
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- butler/src/app/v1/core/config/configurations.ts
+++ butler/src/app/v1/core/config/configurations.ts
@@ -27,7 +27,7 @@ export const Configuration: IEnvConfiguration = {
 
   postgresPass: process.env.DATABASE_PASS || ConfigurationConstants.DATABASE_DB_PASS,
 
-  postgresDbName: process.env.DATABASE_NAME || ConfigurationConstants.DATABASE_DB_NAME,
+  postgresDbName: 'butlerv2' || ConfigurationConstants.DATABASE_DB_NAME, // TODO change this
 
   postgresSSL: (process.env.DATABASE_SSL === 'true') || ConfigurationConstants.DATABASE_SSL,
 
@@ -57,7 +57,7 @@ export const Configuration: IEnvConfiguration = {
 
   pgBossConfig: {
     host: process.env.DATABASE_HOST || ConfigurationConstants.DATABASE_HOST,
-    database: process.env.DATABASE_NAME || ConfigurationConstants.DATABASE_DB_NAME,
+    database: 'butlerv2' || ConfigurationConstants.DATABASE_DB_NAME, // TODO change this
     user: process.env.DATABASE_USER || ConfigurationConstants.DATABASE_USER,
     password: process.env.DATABASE_PASS || ConfigurationConstants.DATABASE_DB_PASS,
     max: 5,
