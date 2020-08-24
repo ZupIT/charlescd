27d26bd743a1877d770da7b84a15354c70a0cb58
Mateus Cruz <emiteze@hotmail.com>
2020-08-24 16:08:29 -0300

    Adding test for gitlab factory legacy
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

---
+++ moove/commons/src/test/kotlin/io/charlescd/moove/commons/integration/git/factory/GitLabClientFactoryLegacyTest.kt
@@ -0,0 +1,14 @@
+package io.charlescd.moove.commons.integration.git.factory
+
+import org.junit.Test
+
+class GitLabClientFactoryLegacyTest {
+
+    
+
+    @Test
+    fun `when using OAuth2Token credential should return GitHub client`(){
+
+    }
+
+}
