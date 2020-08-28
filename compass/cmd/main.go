88829479988c44237e4c3a2c92697c5ff3a46f5b
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-08-28 16:29:49 -0300

    Create first structure integration tests
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- compass/cmd/main.go
+++ compass/cmd/main.go
@@ -7,53 +7,22 @@ import (
 	"compass/internal/metric"
 	"compass/internal/metricsgroup"
 	"compass/internal/plugin"
-	"compass/pkg/logger"
+	"log"
 
 	utils "compass/internal/util"
 	v1 "compass/web/api/v1"
-	"fmt"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
 func main() {
 	godotenv.Load()
 
-	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		configuration.GetConfiguration("DB_HOST"),
-		configuration.GetConfiguration("DB_PORT"),
-		configuration.GetConfiguration("DB_USER"),
-		configuration.GetConfiguration("DB_NAME"),
-		configuration.GetConfiguration("DB_PASSWORD"),
-		configuration.GetConfiguration("DB_SSL"),
-	))
+	db, err := configuration.GetDBConnection("migrations")
 	if err != nil {
-		logger.Fatal("Failed to connect database", err)
+		log.Fatal(err)
 	}
 	defer db.Close()
-
-	driver, err := postgres.WithInstance(db.DB(), &postgres.Config{})
-	if err != nil {
-		logger.Fatal("", err)
-	}
-
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://migrations",
-		configuration.GetConfiguration("DB_NAME"), driver)
-
-	if err != nil {
-		logger.Fatal("", err)
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		logger.Fatal("", err)
-	}
 
 	if utils.IsDeveloperRunning() {
 		db.LogMode(true)
