bddca7d4a1293a72d7daca7d632d3b7643a74e20
Cassio Godinho <cassiopgodinho@gmail.com>
2020-09-01 13:57:51 -0300

    add page indication on paginated response (#450)
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- butler/src/app/v2/api/deployments/dto/execution/paginated-executions.dto.ts
+++ butler/src/app/v2/api/deployments/dto/execution/paginated-executions.dto.ts
@@ -2,4 +2,8 @@ import { Execution } from '../../entity/execution.entity'
 
 export interface PaginatedExecutions {
   executions: Execution[]
+  page: number
+  size: number
+  totalPages: number
+  last: boolean
 }
