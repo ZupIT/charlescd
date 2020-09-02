d1803aa40b6120134dd63d4f77daf1de6e97279a
Thalles Freitas <thalles.freitas@zup.com.br>
2020-09-02 11:24:07 -0300

    fix query
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcModuleRepository.kt
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/repository/JdbcModuleRepository.kt
@@ -272,8 +272,9 @@ class JdbcModuleRepository(
                            latency_threshold,
                            host_value,
                            gateway_name,
+                           namespace
                            created_at)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)
             """
 
         this.jdbcTemplate.batchUpdate(
