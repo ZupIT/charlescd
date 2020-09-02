97faae9fee8b561aaa2669a58acd0c09aabd4f42
bCatanant <bruno.catanant@zup.com.br>
2020-09-02 17:41:45 -0300

    add unit test to front end
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

---
+++ ui/src/modules/Circles/Comparation/Item/MetricsGroups/__tests__/fixtures.ts
@@ -0,0 +1,12 @@
+export const MetricsGroupChartData = [
+  {
+    id: "d4b69bf8-34cd-4bf0-81c3-781202f16dd6",
+    metric: "test 12",
+    result: [
+      {
+        total: 10,
+        period: 1599074229
+      }
+    ]
+  }
+];
