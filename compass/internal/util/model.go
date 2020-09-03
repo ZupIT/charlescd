652e8cbb8fcd278a855c0d774a5e769a5f2762f1
Gabriel Leite <gabriel.leite@zup.com.br>
2020-09-03 14:37:58 -0300

    test fix
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

--- compass/internal/util/model.go
+++ compass/internal/util/model.go
@@ -7,8 +7,6 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-const BigString = `That's is a big Field-Value, probably with more than 100 characters. We are testing the validate method.`
-
 type BaseModel struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"createdAt"`
