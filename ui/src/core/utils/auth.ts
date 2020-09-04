85c74b31c237e08b30a18beb6206fa8aacaa06ec
Douglas Fernandes <ifdouglas@icloud.com>
2020-09-04 16:20:17 -0300

    Add things

--- ui/src/core/utils/auth.ts
+++ ui/src/core/utils/auth.ts
@@ -93,9 +93,10 @@ export function saveSessionData(accessToken: string, refreshToken: string) {
 export const logout = () => {
   clearSession();
 
-  const idmUrl = window.ENVIRONMENT?.REACT_APP_IDM_URI;
-  const idmClientId = window.ENVIRONMENT?.REACT_APP_IDM_CLIENT_ID;
-  const idmResponseType = window.ENVIRONMENT?.REACT_APP_IDM_RESPONSE_TYPE;
+  const idmUrl = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_URI;
+  const idmClientId = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_CLIENT_ID;
+  const idmResponseType =
+    window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_RESPONSE_TYPE;
   const callback = `${idmUrl}?client_id=${idmClientId}&response_type=${idmResponseType}&redirect_uri=http%3A%2F%2Flocalhost:3000`;
 
   window.location.href = callback;
