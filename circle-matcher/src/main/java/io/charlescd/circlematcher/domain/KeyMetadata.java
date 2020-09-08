fc0ba2ab9a6fd4d03f0991440af69d68f53db8b6
Thalles Freitas <thalles.freitas@zup.com.br>
2020-09-08 14:59:03 -0300

    fix test
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- circle-matcher/src/main/java/io/charlescd/circlematcher/domain/KeyMetadata.java
+++ circle-matcher/src/main/java/io/charlescd/circlematcher/domain/KeyMetadata.java
@@ -18,10 +18,8 @@ package io.charlescd.circlematcher.domain;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-
 import java.time.LocalDateTime;
 
 public class KeyMetadata {
