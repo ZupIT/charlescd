f2ea59b536c3903e52a2c0df6ac0ff6dd88211c8
Thalles Freitas <thalles.freitas@zup.com.br>
2020-09-11 14:58:27 -0300

    add unit test
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- moove/application/src/test/groovy/io/charlescd/moove/application/deployment/impl/DeploymentCallbackInteractorImplTest.groovy
+++ moove/application/src/test/groovy/io/charlescd/moove/application/deployment/impl/DeploymentCallbackInteractorImplTest.groovy
@@ -29,6 +29,7 @@ import io.charlescd.moove.domain.repository.WorkspaceRepository
 import io.charlescd.moove.domain.service.CircleMatcherService
 import io.charlescd.moove.infrastructure.service.CircleMatcherClientService
 import io.charlescd.moove.infrastructure.service.client.CircleMatcherClient
+import io.charlescd.moove.infrastructure.service.client.request.CircleMatcherRequest
 import spock.lang.Specification
 
 import java.time.LocalDateTime
@@ -286,6 +287,179 @@ class DeploymentCallbackInteractorImplTest extends Specification {
         }
 
         notThrown()
+    }
+    def 'when callback is in default circle should not update status in circle matcher'() {
+        given:
+        def deploymentId = "314d7293-47d0-4d68-900c-02b834a15cef"
+        def request = new DeploymentCallbackRequest(DeploymentRequestStatus.SUCCEEDED)
+
+        def author = new User('4e806b2a-557b-45c5-91be-1e1db909bef6', 'User name', 'user@email.com', 'user.photo.png',
+                new ArrayList<Workspace>(), false, LocalDateTime.now())
+
+        def circle = new Circle("9aec1a44-77e7-49db-9998-54835cb4aae8", "Default", "8997c35d-7861-4198-9c9b-a2491bf08911", author,
+                LocalDateTime.now(), MatcherTypeEnum.REGULAR, null, null, null, true, "1a58c78a-6acb-11ea-bc55-0242ac130003")
+
+        def currentDeployment = new Deployment(deploymentId, author, LocalDateTime.now(), null, DeploymentStatusEnum.UNDEPLOYING, circle,
+                "97f508ad-cdbd-45df-969f-07781cc00513", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+
+        when:
+        this.deploymentCallbackInteractor.execute(deploymentId, request)
+
+        then:
+        1 * this.deploymentRepository.findById(deploymentId) >> Optional.of(currentDeployment)
+        1 * this.deploymentRepository.update(_)
+        0 * this.circleMatcherClient.update(_, _, _)
+    }
+
+    def 'when callback of deploy is SUCCEEDED should update status to active in circle matcher'() {
+        given:
+        def deploymentId = "314d7293-47d0-4d68-900c-02b834a15cef"
+        def request = new DeploymentCallbackRequest(DeploymentRequestStatus.SUCCEEDED)
+
+        def author = new User('4e806b2a-557b-45c5-91be-1e1db909bef6', 'User name', 'user@email.com', 'user.photo.png',
+                new ArrayList<Workspace>(), false, LocalDateTime.now())
+        def workspaceId = '1a58c78a-6acb-11ea-bc55-0242ac130003'
+
+        def workspace = new Workspace(workspaceId, "Women", author, LocalDateTime.now(), [],
+                WorkspaceStatusEnum.COMPLETE, "7a973eed-599b-428d-89f0-9ef6db8fd39d",
+                "http://matcher-uri.com.br", "833336cd-742c-4f62-9594-45ac0a1e807a",
+                "c5147c49-1923-44c5-870a-78aaba646fe4", null)
+
+        def circle = new Circle("9aec1a44-77e7-49db-9998-54835cb4aae8", "Circle", "8997c35d-7861-4198-9c9b-a2491bf08911", author,
+                LocalDateTime.now(), MatcherTypeEnum.REGULAR, null, null, null, false, "1a58c78a-6acb-11ea-bc55-0242ac130003")
+
+        def currentDeployment = new Deployment(deploymentId, author, LocalDateTime.now(), null, DeploymentStatusEnum.UNDEPLOYING, circle,
+                "97f508ad-cdbd-45df-969f-07781cc00513", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        def previousDeployment = new Deployment("44b87381-6616-462a-9437-27608246bc1b", author, LocalDateTime.now(), null, DeploymentStatusEnum.DEPLOYED, circle,
+                "6ba1d6f1-d443-42d9-b9cc-89097d76ab70", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        when:
+        this.deploymentCallbackInteractor.execute(deploymentId, request)
+
+        then:
+        1 * this.deploymentRepository.findById(deploymentId) >> Optional.of(currentDeployment)
+
+        1 * this.workspaceRepository.find(workspaceId) >> Optional.of(workspace)
+
+        1 * this.deploymentRepository.find(circle.id, DeploymentStatusEnum.DEPLOYED) >> Optional.of(previousDeployment)
+
+        1 * this.deploymentRepository.update(_)
+
+        1 * this.circleMatcherClient.update(_, _, _, ) >> { arguments ->
+            def matcherUrl = arguments[0]
+            def  reference = arguments[1]
+            def circleMatcherRequest = arguments[2]
+
+            assert circleMatcherRequest instanceof CircleMatcherRequest
+            assert circleMatcherRequest instanceof CircleMatcherRequest
+            assert matcherUrl.toString() == workspace.circleMatcherUrl
+            assert reference == circle.reference
+            assert circleMatcherRequest.status == true
+        }
+
+    }
+    def 'when callback of deploy is UNDEPLOYED should update status to inactive in circle matcher'() {
+        given:
+        def deploymentId = "314d7293-47d0-4d68-900c-02b834a15cef"
+        def request = new DeploymentCallbackRequest(DeploymentRequestStatus.UNDEPLOYED)
+
+        def author = new User('4e806b2a-557b-45c5-91be-1e1db909bef6', 'User name', 'user@email.com', 'user.photo.png',
+                new ArrayList<Workspace>(), false, LocalDateTime.now())
+        def workspaceId = '1a58c78a-6acb-11ea-bc55-0242ac130003'
+
+        def workspace = new Workspace(workspaceId, "Women", author, LocalDateTime.now(), [],
+                WorkspaceStatusEnum.COMPLETE, "7a973eed-599b-428d-89f0-9ef6db8fd39d",
+                "http://matcher-uri.com.br", "833336cd-742c-4f62-9594-45ac0a1e807a",
+                "c5147c49-1923-44c5-870a-78aaba646fe4", null)
+
+        def circle = new Circle("9aec1a44-77e7-49db-9998-54835cb4aae8", "Circle", "8997c35d-7861-4198-9c9b-a2491bf08911", author,
+                LocalDateTime.now(), MatcherTypeEnum.REGULAR, null, null, null, false, "1a58c78a-6acb-11ea-bc55-0242ac130003")
+
+        def currentDeployment = new Deployment(deploymentId, author, LocalDateTime.now(), null, DeploymentStatusEnum.UNDEPLOYING, circle,
+                "97f508ad-cdbd-45df-969f-07781cc00513", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        def previousDeployment = new Deployment("44b87381-6616-462a-9437-27608246bc1b", author, LocalDateTime.now(), null, DeploymentStatusEnum.DEPLOYED, circle,
+                "6ba1d6f1-d443-42d9-b9cc-89097d76ab70", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        when:
+        this.deploymentCallbackInteractor.execute(deploymentId, request)
+
+        then:
+        1 * this.workspaceRepository.find(workspaceId) >> Optional.of(workspace)
+
+        1 * this.deploymentRepository.findById(deploymentId) >> Optional.of(currentDeployment)
+
+        1 * this.circleMatcherClient.update(_, _, _, ) >> { arguments ->
+            def matcherUrl = arguments[0]
+            def  reference = arguments[1]
+            def circleMatcherRequest = arguments[2]
+
+            assert circleMatcherRequest instanceof CircleMatcherRequest
+            assert circleMatcherRequest instanceof CircleMatcherRequest
+            assert matcherUrl.toString() == workspace.circleMatcherUrl
+            assert reference == circle.reference
+            assert circleMatcherRequest.status == false
+        }
+    }
+    def 'when callback of deploy is FAILED should not update status in circle matcher'() {
+        given:
+        def deploymentId = "314d7293-47d0-4d68-900c-02b834a15cef"
+        def request = new DeploymentCallbackRequest(DeploymentRequestStatus.FAILED)
+
+        def author = new User('4e806b2a-557b-45c5-91be-1e1db909bef6', 'User name', 'user@email.com', 'user.photo.png',
+                new ArrayList<Workspace>(), false, LocalDateTime.now())
+        def workspaceId = '1a58c78a-6acb-11ea-bc55-0242ac130003'
+
+        def workspace = new Workspace(workspaceId, "Women", author, LocalDateTime.now(), [],
+                WorkspaceStatusEnum.COMPLETE, "7a973eed-599b-428d-89f0-9ef6db8fd39d",
+                "http://matcher-uri.com.br", "833336cd-742c-4f62-9594-45ac0a1e807a",
+                "c5147c49-1923-44c5-870a-78aaba646fe4", null)
+
+        def circle = new Circle("9aec1a44-77e7-49db-9998-54835cb4aae8", "Circle", "8997c35d-7861-4198-9c9b-a2491bf08911", author,
+                LocalDateTime.now(), MatcherTypeEnum.REGULAR, null, null, null, false, "1a58c78a-6acb-11ea-bc55-0242ac130003")
+
+        def currentDeployment = new Deployment(deploymentId, author, LocalDateTime.now(), null, DeploymentStatusEnum.UNDEPLOYING, circle,
+                "97f508ad-cdbd-45df-969f-07781cc00513", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        def previousDeployment = new Deployment("44b87381-6616-462a-9437-27608246bc1b", author, LocalDateTime.now(), null, DeploymentStatusEnum.DEPLOYED, circle,
+                "6ba1d6f1-d443-42d9-b9cc-89097d76ab70", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        when:
+        this.deploymentCallbackInteractor.execute(deploymentId, request)
+
+        then:
+        0 * this.workspaceRepository.find(workspaceId) >> Optional.of(workspace)
+
+        1 * this.deploymentRepository.findById(deploymentId) >> Optional.of(currentDeployment)
+
+        0 * this.circleMatcherClient.update(_, _, _, )
+    }
+
+    def 'when callback of deploy is UNDEPLOY_FAILED should not update status in circle matcher'() {
+        given:
+        def deploymentId = "314d7293-47d0-4d68-900c-02b834a15cef"
+        def request = new DeploymentCallbackRequest(DeploymentRequestStatus.UNDEPLOY_FAILED)
+
+        def author = new User('4e806b2a-557b-45c5-91be-1e1db909bef6', 'User name', 'user@email.com', 'user.photo.png',
+                new ArrayList<Workspace>(), false, LocalDateTime.now())
+        def workspaceId = '1a58c78a-6acb-11ea-bc55-0242ac130003'
+
+        def workspace = new Workspace(workspaceId, "Women", author, LocalDateTime.now(), [],
+                WorkspaceStatusEnum.COMPLETE, "7a973eed-599b-428d-89f0-9ef6db8fd39d",
+                "http://matcher-uri.com.br", "833336cd-742c-4f62-9594-45ac0a1e807a",
+                "c5147c49-1923-44c5-870a-78aaba646fe4", null)
+
+        def circle = new Circle("9aec1a44-77e7-49db-9998-54835cb4aae8", "Circle", "8997c35d-7861-4198-9c9b-a2491bf08911", author,
+                LocalDateTime.now(), MatcherTypeEnum.REGULAR, null, null, null, false, "1a58c78a-6acb-11ea-bc55-0242ac130003")
+
+        def currentDeployment = new Deployment(deploymentId, author, LocalDateTime.now(), null, DeploymentStatusEnum.UNDEPLOYING, circle,
+                "97f508ad-cdbd-45df-969f-07781cc00513", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        def previousDeployment = new Deployment("44b87381-6616-462a-9437-27608246bc1b", author, LocalDateTime.now(), null, DeploymentStatusEnum.DEPLOYED, circle,
+                "6ba1d6f1-d443-42d9-b9cc-89097d76ab70", "be8fce55-c2cf-4213-865b-69cf89178008", null)
+        when:
+        this.deploymentCallbackInteractor.execute(deploymentId, request)
+
+        then:
+        0 * this.workspaceRepository.find(workspaceId) >> Optional.of(workspace)
+
+        1 * this.deploymentRepository.findById(deploymentId) >> Optional.of(currentDeployment)
+
+        0 * this.circleMatcherClient.update(_, _, _, )
     }
 
 }
