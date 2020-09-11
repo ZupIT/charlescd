da103e3e3e69c774d013141e66779b97fc962c55
bCatanant <bruno.catanant@zup.com.br>
2020-09-11 12:25:03 -0300

    Change on click expand for a reroute
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

--- ui/src/modules/Metrics/Circles/History/CircleRow.tsx
+++ ui/src/modules/Metrics/Circles/History/CircleRow.tsx
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
-import React, { useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Text from 'core/components/Text';
 import Styled from './styled';
 import { getStatus } from '../../helpers';
 import CircleReleasesTable from './CircleReleasesTable';
 import { CircleHistory } from '../interfaces';
+import routes from 'core/constants/routes';
 import { humanizeDateFromSeconds, dateTimeFormatter } from 'core/utils/date';
 
 type Props = {
@@ -27,14 +29,18 @@ type Props = {
 };
 
 const CircleRow = ({ circle }: Props) => {
-  const [activeRow, setActiveRow] = useState(false);
+  const history = useHistory();
+  // const [activeRow, setActiveRow] = useState(false);
+  // Screen wasn't a lot of useful data
 
   return (
     <Styled.CircleRow>
       <Styled.StatusLine status={getStatus(circle?.status)} />
       <Styled.TableRow
-        onClick={() => setActiveRow(!activeRow)}
         data-testid={`circle-row-${circle.id}`}
+        onClick={() =>
+          history.push(`${routes.circlesComparation}?circle=${circle.id}`)
+        }
       >
         <Styled.TableColumn>
           <Text.h5 color="light">{circle.name}</Text.h5>
@@ -50,7 +56,7 @@ const CircleRow = ({ circle }: Props) => {
           </Text.h5>
         </Styled.TableColumn>
       </Styled.TableRow>
-      {activeRow && (
+      {false && (
         <Styled.ReleasesWrapper>
           <CircleReleasesTable circleId={circle.id} />
         </Styled.ReleasesWrapper>
