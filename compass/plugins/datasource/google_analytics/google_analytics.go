7d91c18bdd1d4f1c10fe19d901996c94ce4dacd7
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-09-15 14:51:33 -0300

    Test ga
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- compass/plugins/datasource/google_analytics/google_analytics.go
+++ compass/plugins/datasource/google_analytics/google_analytics.go
@@ -19,15 +19,15 @@ func GetMetrics(datasourceConfiguration []byte) (datasource.MetricList, error) {
 	serviceAccountJSON := `
 	{
 		"type": "service_account",
-		"project_id": "ga-example-cf9a0",
-		"private_key_id": "8ea6082b261d004d3a5ca7bd891c868f2166131d",
-		"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDRvMsH9o0HQ0MH\nkGXRMyPMu94Obcw2ZBQmV5sHx+mdJW1399QZufLBYEe4XMkslolhdwpTAa0/sh+n\nAr+21UHOJeOgN7gLkXqdtuS9fy2/wIjzFZE091VNQcGS+P2M2155DHTBsvrZXlbZ\ny+9LwnoLLbgHlqARElSonPyG7bJSyet6JkiRoxP+wZ2pIZcZ8IBdN00DVS+YqqMs\nvFMBIRb5jz0Jo+eUOL1GwlhXBPKKt+/DMomZr9CS/IPtQFIy7BIOQ9WKLbishvdS\ngf/dQuf0XZc5vZcoIaug/paXIYmOA80TbfZ1vdqM2mi5AXjtEnZUpu1mgyAZ8vVh\nowWp4pG9AgMBAAECggEAEXRSuTDtf6M7Ws/LiWNmIU2pJy5ABm7Btocn4bfjWy6Y\nM5muTw3IhAtNLKycGVfVywSrGUlUb66VL8LszXxl45AHp3e7m0raF3H3C0YgOor9\nWq16zhufP7SvNVn/T8+Zxx0gQlZwxdIyDCIKohVnLrL5U7hXTdRmszmNUnVJl9YL\nYlKeQ2yvhHWNco1kSZo2nn05r2SYGUVZ4m3E1+TyRBpTVoQVrvfPORhcXJxR8OQi\nYg/lSP5pj5tsJQXB2lz/1MncMevcho9Omj2t4uP/GUfL+EVp5huzsiNAY5J7CGgS\nGaF3D5Kv00LNDPKufRtaVevKxQxl3d0068oxIayBgQKBgQDz9LGgQkwY7ujoufDv\nq8F5ibWiwG6Hfm+VVbWSYZ/dsnWPR/dYpxdzPbwzgTjDEzLEjVp2pODhETNB/u2A\newdMKe4/EMEKas5w4lgBRFJnDxx+WjQT0YGfAyeucm5IQqPErTniBqmZhBQNzuBJ\nJV60n6pxKqkqDmeOp6mtYFB7DQKBgQDcF57fyeW401cRfb+WfG3pyRfz1MVyyYXp\ntT9My7rzMmt4lUw7a8rqDSyQjyu5sH/aF5pVkQhEjsntI9ZkcOIipsQgq408FWtN\n8itNhTsirYszV9JCTgHgmKG/jNygQV9aFd2ub49ymnDpzkLSGgn2B69hcI4Q+ahf\nQ3M/IokFcQKBgQDHowPE8NwTP1dsM9W+XGDM0+vHDxCkYsxveZ9H9gn8Q7E+mpcI\nflyLWwbrYyEPSmOuQzq3gr/gjtHSfQrgrE1Rf7LO+yfuHW9pI2D/UGjam5wmriyV\n10nq1Ysgj9Y93gLkzRlkJS2fSl2a2yPh1+oDh/HUACRFHVqTfBM3pWzW9QKBgGwu\nPAw5u+2mC7TwPzyjuo7gJiLWoZqzTVw7l3qS5ThmvwaNM+Q9LobupMoFZXOnOqCj\nW1vVpt+z47LpG5dRGQX5PIvmZsxypsSwVPKMmy2HadV5xEKM/0U7IL37afJnT4wD\nFWyJL1Qgb4GsvZFx9RV2X5EgduQHIuh4SexteDYxAoGANnvBAK7tmCWzQmN8utSB\nUCRlRI63XTt9gcchmP5P8BPB0iqiRU4riuoGbzGxEHieMeEk2fk568on5c2UzyCl\nJ+H4SDmnq7o+EXUBf7lOEjGodfmsCl3vpnkd1fr+5sDYIk2UVrNZ90lE6Dg88PXg\n+3SAJxK2AKTr+NU4AwllRVU=\n-----END PRIVATE KEY-----\n",
-		"client_email": "compass@ga-example-cf9a0.iam.gserviceaccount.com",
-		"client_id": "107625970373171168281",
+		"project_id": "compass-ga-289614",
+		"private_key_id": "45158815bc955daf0d19fde535b57f373791278c",
+		"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDguEE2e4KPWrK\nau4to0TFKEzZXiLg9uGWc+rZwFW+W8i3ND67e9EcIlsmiaKuf05rwR8O5HPvxJAl\nU1HPINPlDg0pFZkHiBQnDnG4uE+HgNLZUHE2j83tvR1ErUO+Ni6mAsgjVssunCDt\n++ILPdtcGgqrthvH8vfM22PeX+VWlYdizloh+ojr1E2jtwvRupZncRAi0J2AuBJC\n/6mcQRxKnCNbkekBsIELSVRmnoxnTRjTwhOU9xxh+sXy6reyxvRC8unMBcU2iYxZ\nh3jgeK5m+HWpVK2gElVMwxoi3bd+Peyjko/xyuLbOHaTet6l+mtC+oA4NMWYB703\nfSnUaYmXAgMBAAECggEAM32Inap3kqLSOTUoPUS+m94dPzIXpX/fsawdsfOBoXyy\nw+0yiFX3oJ07vE6LVcGZea+G6nkoP4FZJTtoVFCk/bJRrPxPLTiddhvLv4ZrC5jV\n7v3Mkz4UUgR4bNnk+Xdzma0p4IS/RitAUO0Bf0xvNTZ2kPqJHoPS11UAXFSNJdwN\n0JRW3HBeFbKP5rKQzqBd6TT6HugdE/V/N3qH0Apmmn71Z0hdQKFfPlwQ5c9bpE2Y\nTedyDrnyPo5b69uosXCxBoSsuCzQVo+kg2OKIKxDNVxOOWD8m9SMg5qGctE9rVan\nljfwfZxGYFRToHSCFrmpOGm8vxnL8EqEZOhDtz3iIQKBgQDgAz3witjelP4Y55h3\ntnBgRmBBh56obYDRDXRXMbQXpTZjjlg5Mt+iv5JR75TMgBXfeTaEJ3+3IdaerMOw\nNxfEmqnfz95rOSzWsDQm1fIUcyvjMCDaFw3X6wi+eoeh5sXNVb3VOe2xboBRFA1A\nLIV3T1pz91JQhrpHUctp2zQLLQKBgQDfbcVXXXnobMdKfVBcrTgbw6QCdedxEcD3\nkZhidLjw4GfTww3V4bPb3gcdBB24usLBGnmGb9bmKaWhKPgm6R+x1aVdU/7h3COH\n/khDUxuTV8YckFofZ01W4CuGePAXoUDfz2kCTdVr2o33CEu0fxjCFd9zxKxsfk+W\nD7+BNjvSUwKBgQCoF6atzfJoKxL0ayyMf/iZNZXa9kJGjkzAEcGAAErsB0vlrpVi\n7lrDi87m7skQHvjKxPD0f5MlPx4F2QvKObz7CTd5O0jh2nhb7MJlddr+H2IKSz4L\niIsZv0LDc7w876bzqmgVDS/gGVPuAwbiZcYMvh3YsPgydrZm9+iUWSkXTQKBgBJN\n0uHyZX9WEgmXNJMzGaqkn3YYJKrNsopVoi9GsrQBHmBp1WNVRz/W86w73xd4LWZW\nfeg2l1tpxAjs6098v7z47MA1lmb4euy0N0VUtKiggzlp1ghGlT5txQMSO1EkQ8VS\nH58xsy9Py1uWkWUrGGMmGZVJYnrM6kY36g9y2vnDAoGBAIh14he7Qq66Oz/MdGet\nolOrt+yLGvDBQp4eF6E59NgjpPcwNEZHQofACXYdlnvPMY5CNf44m0+P6vjQ/EyM\n50HZVdp88llGkNLoOwTG/q/M359swoxlfdtxlgHnZZb8EXVAeEOyAgUzeDZyxcqi\nxkowTIKx2zDCviX+uyEEJUFk\n-----END PRIVATE KEY-----\n",
+		"client_email": "compass-admin@compass-ga-289614.iam.gserviceaccount.com",
+		"client_id": "114068190375801630658",
 		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 		"token_uri": "https://oauth2.googleapis.com/token",
 		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/compass%40ga-example-cf9a0.iam.gserviceaccount.com"
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/compass-admin%40compass-ga-289614.iam.gserviceaccount.com"
 	}
 	`
 
@@ -55,15 +55,15 @@ func Query(datasourceConfiguration, query, period, interval []byte, filters []da
 	serviceAccountJSON := `
 	{
 		"type": "service_account",
-		"project_id": "ga-example-cf9a0",
-		"private_key_id": "8ea6082b261d004d3a5ca7bd891c868f2166131d",
-		"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDRvMsH9o0HQ0MH\nkGXRMyPMu94Obcw2ZBQmV5sHx+mdJW1399QZufLBYEe4XMkslolhdwpTAa0/sh+n\nAr+21UHOJeOgN7gLkXqdtuS9fy2/wIjzFZE091VNQcGS+P2M2155DHTBsvrZXlbZ\ny+9LwnoLLbgHlqARElSonPyG7bJSyet6JkiRoxP+wZ2pIZcZ8IBdN00DVS+YqqMs\nvFMBIRb5jz0Jo+eUOL1GwlhXBPKKt+/DMomZr9CS/IPtQFIy7BIOQ9WKLbishvdS\ngf/dQuf0XZc5vZcoIaug/paXIYmOA80TbfZ1vdqM2mi5AXjtEnZUpu1mgyAZ8vVh\nowWp4pG9AgMBAAECggEAEXRSuTDtf6M7Ws/LiWNmIU2pJy5ABm7Btocn4bfjWy6Y\nM5muTw3IhAtNLKycGVfVywSrGUlUb66VL8LszXxl45AHp3e7m0raF3H3C0YgOor9\nWq16zhufP7SvNVn/T8+Zxx0gQlZwxdIyDCIKohVnLrL5U7hXTdRmszmNUnVJl9YL\nYlKeQ2yvhHWNco1kSZo2nn05r2SYGUVZ4m3E1+TyRBpTVoQVrvfPORhcXJxR8OQi\nYg/lSP5pj5tsJQXB2lz/1MncMevcho9Omj2t4uP/GUfL+EVp5huzsiNAY5J7CGgS\nGaF3D5Kv00LNDPKufRtaVevKxQxl3d0068oxIayBgQKBgQDz9LGgQkwY7ujoufDv\nq8F5ibWiwG6Hfm+VVbWSYZ/dsnWPR/dYpxdzPbwzgTjDEzLEjVp2pODhETNB/u2A\newdMKe4/EMEKas5w4lgBRFJnDxx+WjQT0YGfAyeucm5IQqPErTniBqmZhBQNzuBJ\nJV60n6pxKqkqDmeOp6mtYFB7DQKBgQDcF57fyeW401cRfb+WfG3pyRfz1MVyyYXp\ntT9My7rzMmt4lUw7a8rqDSyQjyu5sH/aF5pVkQhEjsntI9ZkcOIipsQgq408FWtN\n8itNhTsirYszV9JCTgHgmKG/jNygQV9aFd2ub49ymnDpzkLSGgn2B69hcI4Q+ahf\nQ3M/IokFcQKBgQDHowPE8NwTP1dsM9W+XGDM0+vHDxCkYsxveZ9H9gn8Q7E+mpcI\nflyLWwbrYyEPSmOuQzq3gr/gjtHSfQrgrE1Rf7LO+yfuHW9pI2D/UGjam5wmriyV\n10nq1Ysgj9Y93gLkzRlkJS2fSl2a2yPh1+oDh/HUACRFHVqTfBM3pWzW9QKBgGwu\nPAw5u+2mC7TwPzyjuo7gJiLWoZqzTVw7l3qS5ThmvwaNM+Q9LobupMoFZXOnOqCj\nW1vVpt+z47LpG5dRGQX5PIvmZsxypsSwVPKMmy2HadV5xEKM/0U7IL37afJnT4wD\nFWyJL1Qgb4GsvZFx9RV2X5EgduQHIuh4SexteDYxAoGANnvBAK7tmCWzQmN8utSB\nUCRlRI63XTt9gcchmP5P8BPB0iqiRU4riuoGbzGxEHieMeEk2fk568on5c2UzyCl\nJ+H4SDmnq7o+EXUBf7lOEjGodfmsCl3vpnkd1fr+5sDYIk2UVrNZ90lE6Dg88PXg\n+3SAJxK2AKTr+NU4AwllRVU=\n-----END PRIVATE KEY-----\n",
-		"client_email": "compass@ga-example-cf9a0.iam.gserviceaccount.com",
-		"client_id": "107625970373171168281",
+		"project_id": "compass-ga-289614",
+		"private_key_id": "45158815bc955daf0d19fde535b57f373791278c",
+		"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDguEE2e4KPWrK\nau4to0TFKEzZXiLg9uGWc+rZwFW+W8i3ND67e9EcIlsmiaKuf05rwR8O5HPvxJAl\nU1HPINPlDg0pFZkHiBQnDnG4uE+HgNLZUHE2j83tvR1ErUO+Ni6mAsgjVssunCDt\n++ILPdtcGgqrthvH8vfM22PeX+VWlYdizloh+ojr1E2jtwvRupZncRAi0J2AuBJC\n/6mcQRxKnCNbkekBsIELSVRmnoxnTRjTwhOU9xxh+sXy6reyxvRC8unMBcU2iYxZ\nh3jgeK5m+HWpVK2gElVMwxoi3bd+Peyjko/xyuLbOHaTet6l+mtC+oA4NMWYB703\nfSnUaYmXAgMBAAECggEAM32Inap3kqLSOTUoPUS+m94dPzIXpX/fsawdsfOBoXyy\nw+0yiFX3oJ07vE6LVcGZea+G6nkoP4FZJTtoVFCk/bJRrPxPLTiddhvLv4ZrC5jV\n7v3Mkz4UUgR4bNnk+Xdzma0p4IS/RitAUO0Bf0xvNTZ2kPqJHoPS11UAXFSNJdwN\n0JRW3HBeFbKP5rKQzqBd6TT6HugdE/V/N3qH0Apmmn71Z0hdQKFfPlwQ5c9bpE2Y\nTedyDrnyPo5b69uosXCxBoSsuCzQVo+kg2OKIKxDNVxOOWD8m9SMg5qGctE9rVan\nljfwfZxGYFRToHSCFrmpOGm8vxnL8EqEZOhDtz3iIQKBgQDgAz3witjelP4Y55h3\ntnBgRmBBh56obYDRDXRXMbQXpTZjjlg5Mt+iv5JR75TMgBXfeTaEJ3+3IdaerMOw\nNxfEmqnfz95rOSzWsDQm1fIUcyvjMCDaFw3X6wi+eoeh5sXNVb3VOe2xboBRFA1A\nLIV3T1pz91JQhrpHUctp2zQLLQKBgQDfbcVXXXnobMdKfVBcrTgbw6QCdedxEcD3\nkZhidLjw4GfTww3V4bPb3gcdBB24usLBGnmGb9bmKaWhKPgm6R+x1aVdU/7h3COH\n/khDUxuTV8YckFofZ01W4CuGePAXoUDfz2kCTdVr2o33CEu0fxjCFd9zxKxsfk+W\nD7+BNjvSUwKBgQCoF6atzfJoKxL0ayyMf/iZNZXa9kJGjkzAEcGAAErsB0vlrpVi\n7lrDi87m7skQHvjKxPD0f5MlPx4F2QvKObz7CTd5O0jh2nhb7MJlddr+H2IKSz4L\niIsZv0LDc7w876bzqmgVDS/gGVPuAwbiZcYMvh3YsPgydrZm9+iUWSkXTQKBgBJN\n0uHyZX9WEgmXNJMzGaqkn3YYJKrNsopVoi9GsrQBHmBp1WNVRz/W86w73xd4LWZW\nfeg2l1tpxAjs6098v7z47MA1lmb4euy0N0VUtKiggzlp1ghGlT5txQMSO1EkQ8VS\nH58xsy9Py1uWkWUrGGMmGZVJYnrM6kY36g9y2vnDAoGBAIh14he7Qq66Oz/MdGet\nolOrt+yLGvDBQp4eF6E59NgjpPcwNEZHQofACXYdlnvPMY5CNf44m0+P6vjQ/EyM\n50HZVdp88llGkNLoOwTG/q/M359swoxlfdtxlgHnZZb8EXVAeEOyAgUzeDZyxcqi\nxkowTIKx2zDCviX+uyEEJUFk\n-----END PRIVATE KEY-----\n",
+		"client_email": "compass-admin@compass-ga-289614.iam.gserviceaccount.com",
+		"client_id": "114068190375801630658",
 		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 		"token_uri": "https://oauth2.googleapis.com/token",
 		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/compass%40ga-example-cf9a0.iam.gserviceaccount.com"
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/compass-admin%40compass-ga-289614.iam.gserviceaccount.com"
 	}
 	`
 
@@ -75,7 +75,7 @@ func Query(datasourceConfiguration, query, period, interval []byte, filters []da
 	batchGetCall := analyticsReportingService.Reports.BatchGet(&analyticsreporting.GetReportsRequest{
 		ReportRequests: []*analyticsreporting.ReportRequest{
 			{
-				ViewId: "228784277",
+				ViewId: "228901918",
 				Metrics: []*analyticsreporting.Metric{
 					{
 						Alias: string(query),
