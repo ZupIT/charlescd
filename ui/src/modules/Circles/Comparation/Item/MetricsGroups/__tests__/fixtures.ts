8235e8720ff19fd9bde36cafdbce5f3942b9e9fd
bCatanant <bruno.catanant@zup.com.br>
2020-09-15 18:08:29 -0300

    add more tests
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

--- ui/src/modules/Circles/Comparation/Item/MetricsGroups/__tests__/fixtures.ts
+++ ui/src/modules/Circles/Comparation/Item/MetricsGroups/__tests__/fixtures.ts
@@ -38,6 +38,14 @@ export const MetricsGroupData = [{
   status: "ACTIVE"
 }];
 
+export const MetricsGroupWithoutMetricData = [{
+  id: "d4b69bf8-34cd-4bf0-81c3-781202f17fe7",
+  circleId: "d4b69bf8-34cd-4bf0-81c3-781202f16dd6",
+  name: "test 1",
+  metrics: [],
+  status: "ACTIVE"
+}];
+
 export const OptionsValues = [
   {
     "label": "1",
