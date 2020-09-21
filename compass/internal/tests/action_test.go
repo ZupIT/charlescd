39ca01b1283e390bb5a64297c247b61ed539088b
Gabriel Leite <gabriel.leite@zup.com.br>
2020-09-21 11:09:32 -0300

    action tests done
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

--- compass/internal/tests/action_test.go
+++ compass/internal/tests/action_test.go
@@ -3,6 +3,9 @@ package tests
 import (
 	"compass/internal/action"
 	"compass/internal/configuration"
+	"compass/internal/util"
+	"encoding/json"
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -60,4 +63,111 @@ func (s *ActionSuite) TestParseAction() {
 
 	require.NoError(s.T(), err)
 	require.NotNil(s.T(), res)
+}
+
+func (s *ActionSuite) TestParseActionError() {
+	stringReader := strings.NewReader(``)
+	stringReadCloser := ioutil.NopCloser(stringReader)
+
+	_, err := s.repository.Parse(stringReadCloser)
+
+	require.Error(s.T(), err)
+}
+
+func (s *ActionSuite) TestValidateAction() {
+	action := action.Action{
+		BaseModel: util.BaseModel{},
+		DeletedAt: nil,
+	}
+	res := s.repository.Validate(action)
+
+	require.NotEmpty(s.T(), res)
+}
+
+func (s *ActionSuite) TestFindByIdAction() {
+	actionStruct := action.Action{
+		Nickname:      "ActionName",
+		Type:          "CircleUp",
+		Configuration: json.RawMessage(`{"config": "some-config"}`),
+		WorkspaceId:   uuid.New().String(),
+		DeletedAt:     nil,
+	}
+
+	s.DB.Create(&actionStruct)
+	res, err := s.repository.FindById(actionStruct.ID.String())
+
+	require.NoError(s.T(), err)
+	actionStruct.BaseModel = res.BaseModel
+	require.Equal(s.T(), actionStruct, res)
+}
+
+func (s *ActionSuite) TestFindByIdActionError() {
+	_, err := s.repository.FindById(uuid.New().String())
+	require.Error(s.T(), err)
+}
+
+func (s *ActionSuite) TestFindByAllAction() {
+	actionStruct := action.Action{
+		Nickname:      "ActionName",
+		Type:          "CircleUp",
+		Configuration: json.RawMessage(`{"config": "some-config"}`),
+		WorkspaceId:   uuid.New().String(),
+		DeletedAt:     nil,
+	}
+
+	s.DB.Create(&actionStruct)
+	res, err := s.repository.FindAll()
+
+	require.NoError(s.T(), err)
+	require.NotEmpty(s.T(), res)
+}
+
+func (s *ActionSuite) TestFindByAllActionError() {
+	s.DB.Close()
+	_, err := s.repository.FindAll()
+
+	require.Error(s.T(), err)
+}
+
+func (s *ActionSuite) TestSaveAction() {
+	actionStruct := action.Action{
+		Nickname:      "ActionName",
+		Type:          "CircleUp",
+		Configuration: json.RawMessage(`{"config": "some-config"}`),
+		WorkspaceId:   uuid.New().String(),
+		DeletedAt:     nil,
+	}
+
+	res, err := s.repository.Save(actionStruct)
+
+	require.NoError(s.T(), err)
+	actionStruct.BaseModel = res.BaseModel
+	require.Equal(s.T(), actionStruct, res)
+}
+
+func (s *ActionSuite) TestSaveActionError() {
+	actionStruct := action.Action{}
+	_, err := s.repository.Save(actionStruct)
+
+	require.Error(s.T(), err)
+}
+
+func (s *ActionSuite) TestDeleteAction() {
+	actionStruct := action.Action{
+		Nickname:      "ActionName",
+		Type:          "CircleUp",
+		Configuration: json.RawMessage(`{"config": "some-config"}`),
+		WorkspaceId:   uuid.New().String(),
+		DeletedAt:     nil,
+	}
+
+	s.DB.Create(&actionStruct)
+	err := s.repository.Delete(actionStruct.ID.String())
+	require.NoError(s.T(), err)
+}
+
+func (s *ActionSuite) TestDeleteActionError() {
+	s.DB.Close()
+	err := s.repository.Delete(uuid.New().String())
+	require.Error(s.T(), err)
 }
