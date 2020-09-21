5af0a683909dc422fd93d5b3bf53bb71b8088c74
e-km <eduardo.miyoshi@zup.com.br>
2020-09-21 18:25:45 -0300

    remove MooveErrorCode duplication
    Signed-off-by: e-km <eduardo.miyoshi@zup.com.br>

--- moove/domain/src/main/kotlin/io/charlescd/moove/domain/MooveErrorCode.kt
+++ moove/domain/src/main/kotlin/io/charlescd/moove/domain/MooveErrorCode.kt
@@ -56,6 +56,5 @@ enum class MooveErrorCode(val key: String) {
     MODULE_MUST_HAVE_AT_LEAST_ONE_COMPONENT("module.must.have.at.least.one.component"),
     CREATE_USER_ERROR_EMAIL_ALREADY_EXISTS("create.user.error.email.already.exists"),
     USER_PASSWORD_DOES_NOT_MATCH("user.password.does.not.match"),
-    CANNOT_RESET_YOUR_OWN_PASSWORD("cannot.reset.your.own.password"),
-    forbidden("forbidden")
+    CANNOT_RESET_YOUR_OWN_PASSWORD("cannot.reset.your.own.password")
 }
