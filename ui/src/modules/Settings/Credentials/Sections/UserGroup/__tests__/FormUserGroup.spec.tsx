8f61c0548c434aa627a366b3de054f05c1cbf6e3
Leandro Queiroz <leandroqueiroz92@gmail.com>
2020-09-23 17:52:25 -0300

    add user group form test
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

---
+++ ui/src/modules/Settings/Credentials/Sections/UserGroup/__tests__/FormUserGroup.spec.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { render, screen, wait, act } from 'unit-test/testUtils';
+import { FetchMock } from 'jest-fetch-mock';
+import selectEvent from 'react-select-event';
+import FormUserGroup from '../Form';
+
+test('render Form User Group', async () => {
+  (fetch as FetchMock)
+    .mockResponse(JSON.stringify({
+      content: [
+        { id: '1', name: 'Maintainer' }
+      ]
+    }));
+
+  render(
+    <FormUserGroup onFinish={jest.fn()} />
+  );
+  
+  await wait(() => expect(screen.getByTestId('button-default-add'))
+    .toHaveTextContent('Add'));
+    
+  const selectUserGroup = screen.getByText('Select a user group');
+  await act(async() => selectEvent.select(selectUserGroup, 'Maintainer'));
+
+  expect(screen.getByTestId('button-default-add')).not.toBeDisabled();
+});
