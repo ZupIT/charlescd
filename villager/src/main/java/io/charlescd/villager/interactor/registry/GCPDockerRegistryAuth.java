fbea7724a9f88ea62bd502b295cda0c622a9a8c6
pedroborgeszup <pedro.borges@zup.com.br>
2020-09-24 15:22:18 -0300

    adding gcr support and tests
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- villager/src/main/java/io/charlescd/villager/interactor/registry/GCPDockerRegistryAuth.java
+++ villager/src/main/java/io/charlescd/villager/interactor/registry/GCPDockerRegistryAuth.java
@@ -14,10 +14,28 @@
  * limitations under the License.
  */
 
-package io.charlescd.villager.infrastructure.integration.registry;
+package io.charlescd.villager.interactor.registry;
 
-public enum RegistryType {
+public class GCPDockerRegistryAuth implements DockerRegistryAuth {
+    private String organization;
+    private String username;
+    private String jsonKey;
 
-    AWS, AZURE, GCP, UNSUPPORTED
+    public GCPDockerRegistryAuth(String organization, String username, String jsonKey) {
+        this.organization = organization;
+        this.username = username;
+        this.jsonKey = jsonKey;
+    }
 
+    public String getOrganization() {
+        return organization;
+    }
+
+    public String getUsername() {
+        return  username;
+    }
+
+    public String getJsonKey() {
+        return jsonKey;
+    }
 }
