4135c1e6ce71dfbb0ef91cfc049130e19024541e
Thalles Freitas <thalles.freitas@zup.com.br>
2020-09-25 10:55:44 -0300

    fix cov
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- circle-matcher/src/test/groovy/io/charlescd/circlematcher/infrastructure/RedisClientConfigurationTest.groovy
+++ circle-matcher/src/test/groovy/io/charlescd/circlematcher/infrastructure/RedisClientConfigurationTest.groovy
@@ -1,28 +1,22 @@
 package io.charlescd.circlematcher.infrastructure
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import spock.lang.Specification
 
 class RedisClientConfigurationTest extends Specification {
-
+    def redisConnectionFactory = Mock(RedisConnectionFactory)
     def "should create the correct redis template"() {
         given:
-        def template = new RedisTemplate<String, Object>();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer =  new Jackson2JsonRedisSerializer<>(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        def clientCofiguration = new RedisClientConfiguration()
         when:
-        template.setHashKeySerializer(jackson2JsonRedisSerializer);
+        def template = clientCofiguration.createTemplate(redisConnectionFactory);
         then:
         assert template.getHashKeySerializer() != null
-        assert template.getHashKeySerializer() instanceof Jackson2JsonRedisSerializer
+        assert template.getHashKeySerializer() instanceof StringRedisSerializer
+        assert template.getValueSerializer() != null
+        assert template.getValueSerializer() instanceof Jackson2JsonRedisSerializer
 
     }
 
