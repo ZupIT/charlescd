99a7b640e109f72d474b8afa039e07accd83dfaa
pedroborgeszup <pedro.borges@zup.com.br>
2020-09-25 17:12:34 -0300

    fix patch circle validation and tests
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- moove/application/src/main/kotlin/io/charlescd/moove/application/circle/request/PatchCircleRequest.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/circle/request/PatchCircleRequest.kt
@@ -58,11 +58,14 @@ data class PatchCircleRequest(override val patches: List<PatchOperation>) : Base
     private fun validateRulesValues(patch: PatchOperation) {
         Assert.notNull(patch.value, "Rules cannot be null.")
         jacksonObjectMapper().convertValue<NodePart>(patch.value!!).let { node ->
-            Assert.notNull(node.clauses, "Clauses cannot be null")
-            node.clauses!!.forEach { clause ->
-                Assert.notNull(clause.content?.key, "Key cannot be null")
-                Assert.notNull(clause.content?.condition, "Condition cannot be null")
-                Assert.isTrue(clause.content?.key!!.isNotBlank(), "Key cannot be blank")
+            Assert.notNull(node.clauses, "Clauses cannot be null.")
+            node.clauses!!.forEach { clauses ->
+                Assert.notNull(clauses.clauses, "Clause cannot be null.")
+                clauses.clauses!!.forEach { clause ->
+                    Assert.notNull(clause.content?.key, "Key cannot be null.")
+                    Assert.notNull(clause.content?.condition, "Condition cannot be null.")
+                    Assert.isTrue(clause.content?.key!!.isNotBlank(), "Key cannot be blank.")
+                }
             }
         }
     }
