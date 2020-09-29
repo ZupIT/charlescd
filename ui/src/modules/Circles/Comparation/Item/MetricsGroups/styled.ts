cc9de24250ac7f732fe9358071b066bed72e60ba
bCatanant <bruno.catanant@zup.com.br>
2020-09-29 16:28:21 -0300

    refactor
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

--- ui/src/modules/Circles/Comparation/Item/MetricsGroups/styled.ts
+++ ui/src/modules/Circles/Comparation/Item/MetricsGroups/styled.ts
@@ -34,10 +34,6 @@ interface ButtonIconProps {
   isActive: boolean;
 }
 
-interface FilterOpenProps {
-  isOpen: boolean;
-}
-
 interface ThresholdIconProps {
   color: string;
   hasTreshold: boolean;
@@ -170,144 +166,8 @@ const ButtonModal = styled(ButtonComponent.Default)`
   margin-top: 20px;
 `;
 
-const MetricsGroupsCard = styled.div`
-  background-color: ${({ theme }) =>
-    theme.circleGroupMetrics.content.background};
-  margin-top: 15px;
-  border-radius: 5px;
-  width: 520px;
-  padding: 20px;
-`;
-
-const MetricsGroupsCardHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 5px;
-
-  span {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    padding-right: 20px;
-  }
-`;
-
-const MetricsGroupsCardContent = styled.div`
-  overflow-y: auto;
-  overflow-x: hidden;
-  max-height: 315px;
-`;
-
-const MetricCardTableHead = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-top: 20px;
-
-  span {
-    margin: 0 90px 10px 10px;
-  }
-`;
-
-const MetricCardBody = styled.div`
-  background-color: ${({ theme }) => theme.circleGroupMetrics.content.card};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 5px;
-  border-radius: 5px;
-  height: 40px;
-`;
-
-const MetricDropdown = styled.div`
-  margin-right: 10px;
-`;
-
-const MetricNickname = styled(Text.h5)`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 125px;
-  margin-left: 10px;
-`;
-
-const MetricConditionThreshold = styled.div`
-  display: flex;
-  width: 125px;
-  margin-right: 45px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-
-  span {
-    margin-right: 5px;
-  }
-`;
-
 const RuleWrapper = styled.div`
   max-width: 80%;
-`;
-
-const MetricLastValueText = styled(Text.h5)`
-  margin-right: 10px;
-  position: relative;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 60px;
-  left: 5px;
-  top: 2px;
-`;
-
-const MetricLastValue = styled.div<ThresholdIconProps>`
-  display: flex;
-
-  svg {
-    color: ${({ theme, color, hasTreshold }) =>
-      hasTreshold
-        ? 'transparent'
-        : theme.circleGroupMetrics.execution.status[color]};
-  }
-`;
-
-const MonitoringMetricsContent = styled.div``;
-
-const MonitoringMetricsFilter = styled.div<FilterOpenProps>`
-  padding-top: ${({ isOpen }) => isOpen && '10px'};
-  display: flex;
-
-  > * {
-    margin-right: 20px;
-  }
-`;
-
-const MonitoringMetricsPeriodFilter = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding: 0px 80px;
-`;
-
-const ButtonIconRoundedPeriod = styled(ButtonIconRoundedComponent)`
-  height: 17px;
-  width: 37px;
-  padding: 10px 25px;
-  display: flex;
-  justify-content: center;
-
-  span {
-    font-weight: normal;
-    font-size: ${HEADINGS_FONT_SIZE.h6};
-  }
-
-  ${({ isActive }: ButtonIconProps) =>
-    isActive &&
-    css`
-      border: solid 1px ${({ theme }) => theme.radio.checked.color};
-
-      span {
-        color: ${({ theme }) => theme.radio.checked.color};
-      }
-    `};
 `;
 
 const FieldErrorWrapper = styled.div`
@@ -315,21 +175,6 @@ const FieldErrorWrapper = styled.div`
 
   span {
     margin-left: 5px;
-  }
-`;
-
-const MultiSelect = styled(SelectComponent.MultiCheck)`
-  width: 130px;
-  height: 35px;
-  bottom: 10px;
-
-  div:first-child {
-    background: transparent;
-    border-bottom: none;
-  }
-
-  svg > * + * {
-    display: none;
   }
 `;
 
@@ -355,22 +200,7 @@ export default {
     Title: ModalTitle,
     Button: ButtonModal
   },
-  MetricsGroupsCard,
-  MetricsGroupsCardHeader,
-  MetricsGroupsCardContent,
-  MetricCardTableHead,
-  MetricCardBody,
-  MetricConditionThreshold,
-  MetricNickname,
-  MetricLastValue,
-  MetricLastValueText,
-  MetricDropdown,
   TrashIcon,
   RuleWrapper,
-  MonitoringMetricsContent,
-  MonitoringMetricsFilter,
-  MonitoringMetricsPeriodFilter,
-  ButtonIconRoundedPeriod,
-  FieldErrorWrapper,
-  MultiSelect
+  FieldErrorWrapper
 };
