1983e74387a84243c36ca543587d153bf927e6a4
Maycon Pacheco <mayconjrpacheco@gmail.com>
2020-09-29 15:23:48 -0300

    Create plugin readme
    Signed-off-by: Maycon Pacheco <mayconjrpacheco@gmail.com>

--- compass/build-plugins.sh
+++ compass/build-plugins.sh
@@ -1,1 +1,14 @@
-for each in ./plugins/datasource/*/ ; do go build -buildmode=plugin -o ./plugins "$each"*.go; done
+for category in ./plugins/*/
+do
+    category=${category%*/}
+    category=${category##*/}
+
+  for plugin in ./plugins/"$category"/*/
+  do
+    plugin=${plugin%*/}
+    plugin=${plugin##*/}
+    go build -buildmode=plugin -o ./dist/"$category"/"$plugin"/"$plugin".so ./plugins/"$category"/"$plugin"/*.go
+    cp ./plugins/"$category"/"$plugin"/readme.yaml ./dist/"$category"/"$plugin"/readme.yaml
+    echo Plugin "$plugin" builded
+  done
+done
