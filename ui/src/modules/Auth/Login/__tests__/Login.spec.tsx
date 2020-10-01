d8b1da6d15b0fe0c86d90846a489cf2503642186
Douglas Fernandes <douglas.fernandes@zup.com.br>
2020-10-01 16:57:29 -0300

    Improve tests

--- ui/src/modules/Auth/Login/__tests__/Login.spec.tsx
+++ ui/src/modules/Auth/Login/__tests__/Login.spec.tsx
@@ -18,6 +18,16 @@ import React from 'react';
 import { render, screen, act, wait, fireEvent } from 'unit-test/testUtils';
 import userEvent from '@testing-library/user-event';
 import Login from '..';
+import routes from 'core/constants/routes';
+import { FetchMock } from 'jest-fetch-mock';
+
+const originalWindow = window;
+beforeEach(() => {
+  Object.assign(window, originalWindow);
+  const location = window.location
+  delete global.window.location
+  global.window.location = Object.assign({}, location)
+})
 
 test('render Login page', () => {
   render(<Login />);
@@ -33,8 +43,29 @@ test('render Login page', () => {
   expect(buttonSubmit).toBeDisabled();
 });
 
-test('render Login page', async () => {
+test.only('render Login page', async () => {
+  window.location = {
+    ...window.location,
+    href: '',
+    pathname: '/workspaces',
+  };
+
   render(<Login />);
+
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({
+    access_token: 'abcdefghijklmn', refresh_token: 'opqrstuvwxyz'
+  }));
+  
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({
+    circles: [{ id: '123', name: 'circle' }]
+  }));
+  
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({
+    id: '1',
+    name: 'charlescd',
+    email: 'charlescd@zup.com.br',
+    workspaces: [{ id: '1', name: 'workspace' }]
+  }));
 
   const inputEmail = screen.queryByTestId('input-email-email');
   const inputPassword = screen.queryByTestId('input-password-password');
@@ -44,4 +75,10 @@ test('render Login page', async () => {
   await act(async () => userEvent.type(inputPassword, '123mudar'));
 
   expect(buttonSubmit).not.toBeDisabled();
+
+  fireEvent.click(buttonSubmit);
+
+  await wait();
+
+  expect(window.location.href).toEqual(routes.workspaces);
 });
