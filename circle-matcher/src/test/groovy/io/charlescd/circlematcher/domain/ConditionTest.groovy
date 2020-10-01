f26237bc2fc7812ca6d93db33a55acabff39366c
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2020-10-01 17:50:03 -0300

    Test less than condition
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/ConditionTest.groovy
+++ circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/ConditionTest.groovy
@@ -88,4 +88,18 @@ class ConditionTest extends Specification {
 
         assert expression == "toStr(getPath(input, 'region')).endsWith(toStr('north'))"
     }
+
+    def "Less than expression should parse to number and then compare only the first value"() {
+
+        given:
+        def key = "age"
+        def values = ["43", "32"]
+        when:
+
+        def expression = Condition.LESS_THAN.expression(OpUtils.inputValue(key), values)
+
+        then:
+
+        assert expression == "toNumber(getPath(input, 'age')) < toNumber(43)"
+    }
 }
