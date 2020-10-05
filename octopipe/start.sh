09cef80966cc5bffa8c54f9a4456ce949b5d9bfd
Cassio Godinho <cassiopgodinho@gmail.com>
2020-10-05 15:18:50 -0300

    add overrides to start script to run butler
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- octopipe/start.sh
+++ octopipe/start.sh
@@ -2,6 +2,6 @@
 set -e
 
 touch kubeconfig.yaml
-docker-compose up -d
+docker-compose -f docker-compose.yaml -f ../butler/docker-compose.yml up -d
 ./wait-for-it.sh -h localhost -p 5000 -- docker build . -f mock.Dockerfile -t localhost:5000/my-app && docker push localhost:5000/my-app
-docker-compose logs -f
+docker-compose -f docker-compose.yaml -f ../butler/docker-compose.yml logs -f
