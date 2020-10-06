c2207b3dbfac41037f7dcb36804b5da77fb9d474
barbararochazup <barbara.rocha@zup.com.br>
2020-10-06 17:40:31 -0300

    adding isProtected card info to board respose with unit tests

--- moove/legacy-moove/src/main/kotlin/io/charlescd/moove/legacy/moove/service/HypothesisServiceLegacy.kt
+++ moove/legacy-moove/src/main/kotlin/io/charlescd/moove/legacy/moove/service/HypothesisServiceLegacy.kt
@@ -27,12 +27,15 @@ import io.charlescd.moove.commons.representation.*
 import io.charlescd.moove.legacy.moove.request.hypothesis.*
 import io.charlescd.moove.legacy.repository.*
 import io.charlescd.moove.legacy.repository.entity.*
+import org.gitlab4j.api.models.ProtectedBranch
+import org.springframework.beans.factory.annotation.Value
 import java.time.LocalDateTime
 import java.util.*
 import javax.transaction.Transactional
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import kotlin.streams.toList
 
 @Service
 class HypothesisServiceLegacy(
@@ -42,6 +45,11 @@ class HypothesisServiceLegacy(
     private val cardColumnRepository: CardColumnRepository,
     private val cardRepository: CardRepository
 ) {
+
+
+    @Value("\${charlescd.protected.branches}")
+    lateinit var protectedBranches: Array<String>
+
     fun findValidatedBuildsByHypothesisId(id: String, workspaceId: String): List<SimpleBuildRepresentation> {
         return this.hypothesisRepository.findByIdAndWorkspaceId(id, workspaceId)
             .orElseThrow { NotFoundExceptionLegacy("hypothesis", id) }
@@ -237,12 +245,13 @@ class HypothesisServiceLegacy(
         return if (isItDeployedReleasesColumn(cardColumn)) {
             getCardsWithValidDeployments(cardColumn, hypothesis)
         } else {
+            val isProtectedCard = isProtectedCard(hypothesis)
             return cardColumn.copy(
                 id = cardColumn.id,
                 name = cardColumn.name,
                 cards = hypothesis.cards.filter { it.column.id == cardColumn.id && it.status == CardStatus.ACTIVE }
                     .sortedBy { it.index }
-                    .map { it.toSimpleRepresentation() },
+                    .map { it.toSimpleRepresentation(isProtectedCard) },
                 builds = hypothesis.builds.filter { it.column?.id == cardColumn.id && it.status != BuildStatus.ARCHIVED }
                     .orderDeploymentsByDate()
                     .sortedByDescending { it.createdAt }
@@ -384,5 +393,14 @@ class HypothesisServiceLegacy(
             it.name == ColumnConstants.BUILDS_COLUMN_NAME ||
                     it.name == ColumnConstants.DEPLOYED_RELEASES_COLUMN_NAME
         } ?: throw RuntimeException("Invalid column")
+    }
+
+    private fun isProtectedCard(hypothesis: Hypothesis) : Boolean {
+        val branchNames = hypothesis.cards.filterIsInstance<SoftwareCard>().toList().stream().map { it.feature.branchName }.toList()
+        return branchNames.stream().filter{ isProtectedBranch(it)}.findAny().isPresent
+    }
+
+    private fun isProtectedBranch(branchName: String): Boolean {
+        return protectedBranches.contains(branchName)
     }
 }
