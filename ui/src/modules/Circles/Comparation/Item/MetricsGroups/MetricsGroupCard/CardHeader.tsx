e99564223753f76f95c80a74b7d45ae353a1f06f
Washington Soares <washingtonudi@gmail.com>
2020-10-07 08:22:40 -0300

    Refactor MetricsGroup header
    Signed-off-by: Washington Soares <washingtonudi@gmail.com>

---
+++ ui/src/modules/Circles/Comparation/Item/MetricsGroups/MetricsGroupCard/CardHeader.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Text from 'core/components/Text';
+import Dropdown from 'core/components/Dropdown';
+import Styled from './styled';
+import { MetricsGroup } from '../types';
+
+type Props = {
+  metricGroup: MetricsGroup;
+  handleAddMetric: Function;
+  handleDeleteMetricsGroup: Function;
+};
+
+const CardHeader = ({
+  metricGroup,
+  handleAddMetric,
+  handleDeleteMetricsGroup
+}: Props) => (
+  <Styled.MetricsGroupsCardHeader>
+    <Text.h2 color="light" title={metricGroup.name}>
+      {metricGroup.name}
+    </Text.h2>
+    <Dropdown icon="vertical-dots" size="16px">
+      <Dropdown.Item
+        icon="add"
+        name="Add metric"
+        onClick={() => handleAddMetric(metricGroup)}
+      />
+      <Dropdown.Item
+        icon="delete"
+        name="Delete"
+        onClick={() => handleDeleteMetricsGroup(metricGroup.id)}
+      />
+    </Dropdown>
+  </Styled.MetricsGroupsCardHeader>
+);
+
+export default CardHeader;
