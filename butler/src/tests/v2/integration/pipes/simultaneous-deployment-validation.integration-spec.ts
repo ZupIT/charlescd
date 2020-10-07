cb89f80da2bb58573062bd4be2947d6a8c9c8cf7
Cassio Godinho <cassiopgodinho@gmail.com>
2020-10-07 15:01:03 -0300

    ignore lint on test file
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- butler/src/tests/v2/integration/pipes/simultaneous-deployment-validation.integration-spec.ts
+++ butler/src/tests/v2/integration/pipes/simultaneous-deployment-validation.integration-spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BadRequestException, INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { EntityManager } from 'typeorm'
