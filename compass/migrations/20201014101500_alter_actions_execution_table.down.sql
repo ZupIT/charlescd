41031fdffebea740a9b3a961e98c60ccba8f17de
arthur pucci <arthur.pucci@zup.com.br>
2020-10-14 10:43:52 -0300

    Removing unnecessary migrations
    Signed-off-by: arthur pucci <arthur.pucci@zup.com.br>

--- compass/migrations/20201014101500_alter_actions_execution_table.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE actions_executions
-    RENAME COLUMN group_action_id TO metric_action_id;
--- compass/migrations/20201014101500_alter_actions_execution_table.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE actions_executions
-    RENAME COLUMN metric_action_id TO group_action_id;
