63586b2c93e12ffc20b8e232b019f0b74b237b55
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2020-10-16 16:15:23 -0300

    fix ui x-circle-id handling
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- ui/src/modules/Auth/Login/hook.ts
+++ ui/src/modules/Auth/Login/hook.ts
@@ -43,7 +43,7 @@ export const useCircleMatcher = (): {
           saveCircleId(circle?.id);
         }
       } catch (e) {
-        saveCircleId('');
+        console.info('No circle was detected for this user')
       }
     },
     [getCircleMatcher]
