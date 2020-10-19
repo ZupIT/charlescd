d24e42285896ffe1f3a7c134e5e5dff5d7aef93a
barbararochazup <barbara.rocha@zup.com.br>
2020-10-19 13:48:17 -0300

    fixing test
    Signed-off-by: barbararochazup <barbara.rocha@zup.com.br>

--- moove/legacy-moove/src/main/kotlin/io/charlescd/moove/legacy/moove/service/CardService.kt
+++ moove/legacy-moove/src/main/kotlin/io/charlescd/moove/legacy/moove/service/CardService.kt
@@ -37,6 +37,7 @@ import java.util.*
 import javax.transaction.Transactional
 import org.hibernate.Hibernate
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
