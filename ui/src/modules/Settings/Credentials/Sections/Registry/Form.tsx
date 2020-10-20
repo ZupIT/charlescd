71f1f96f54c65e5e1bb5310722199aba21d93c12
Luane Aquino <luane.aquino.br@gmail.com>
2020-10-20 16:35:11 -0300

    add ui elements with fixed values
    Signed-off-by: Luane Aquino <luane.aquino.br@gmail.com>

--- ui/src/modules/Settings/Credentials/Sections/Registry/Form.tsx
+++ ui/src/modules/Settings/Credentials/Sections/Registry/Form.tsx
@@ -21,6 +21,7 @@ import Form from 'core/components/Form';
 import RadioGroup from 'core/components/RadioGroup';
 import Text from 'core/components/Text';
 import Popover, { CHARLES_DOC } from 'core/components/Popover';
+import ConnectionStatus from './ConnectionStatus';
 import { getProfileByKey } from 'core/utils/profile';
 import { useRegistry } from './hooks';
 import { radios } from './constants';
@@ -37,9 +38,13 @@ const FormRegistry = ({ onFinish }: Props) => {
   const { responseAdd, save, loadingSave, loadingAdd } = useRegistry();
   const [registryType, setRegistryType] = useState('');
   const [awsUseSecret, setAwsUseSecret] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
   const { register, handleSubmit, reset, control } = useForm<Registry>();
   const profileId = getProfileByKey('id');
   const dispatch = useDispatch();
+  // TODO remove later
+  const response = true;
+  const [loading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (responseAdd) onFinish();
@@ -72,6 +77,14 @@ const FormRegistry = ({ onFinish }: Props) => {
     }
 
     save(registry);
+  };
+
+  const onClick = () => {
+    console.log('[onClick]');
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   };
 
   const renderAwsFields = () => {
@@ -138,6 +151,14 @@ const FormRegistry = ({ onFinish }: Props) => {
           control={control}
           theme="monokai"
         />
+        {response && <ConnectionStatus status="FAILED" />}
+        <Styled.TestConnectionButton
+          type="button"
+          onClick={() => onClick()}
+          isLoading={loading}
+        >
+          Test connection
+        </Styled.TestConnectionButton>
       </>
     );
   };
@@ -194,6 +215,7 @@ const FormRegistry = ({ onFinish }: Props) => {
         id="submit-registry"
         type="submit"
         isLoading={loadingSave || loadingAdd}
+        isDisabled={isDisabled}
       >
         Save
       </Button.Default>
