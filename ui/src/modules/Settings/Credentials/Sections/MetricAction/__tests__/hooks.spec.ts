2debe6fc5023e0e1e4951e31bc6b74f83c0f01e3
Washington Soares <washingtonudi@gmail.com>
2020-10-21 10:45:56 -0300

    Add action unit tests
    Signed-off-by: Washington Soares <washingtonudi@gmail.com>

---
+++ ui/src/modules/Settings/Credentials/Sections/MetricAction/__tests__/hooks.spec.ts
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { renderHook, act } from '@testing-library/react-hooks';
+import { wait } from 'unit-test/testUtils';
+import { FetchMock } from 'jest-fetch-mock';
+import { useActionData, useDeleteAction, usePlugins } from '../hooks';
+import { Action } from '../types';
+import * as actions from 'core/components/Notification/state/actions';
+
+
+beforeEach(() => {
+  (fetch as FetchMock).resetMocks();
+});
+
+jest.mock('core/state/hooks', () => ({
+  useDispatch: () => jest.fn()
+}));
+
+test('get action data', async () => {
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({}));
+
+  const { result } = renderHook(() => useActionData());
+  const { current } = result;
+
+  let response: Action[];
+
+  await act(async () => {
+    response = await current.getActionData();
+  });
+
+  await wait(() => expect(response).toMatchObject({}));
+});
+
+test('error get action data', async () => {
+  (fetch as FetchMock).mockRejectedValue(new Response(JSON.stringify({})));
+
+  const { result: userResult } = renderHook(() => useActionData());
+
+  let response: Action[];
+  await act(async () => {
+    response = await userResult.current.getActionData();
+  });
+
+  await wait(() => expect(response).toBeUndefined());
+});
+
+test('should delete action', async () => {
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({}));
+
+  const { result } = renderHook(() => useDeleteAction());
+  const { current } = result;
+
+  let response: Action;
+
+  await act(async () => {
+    response = await current.deleteAction('123');
+  });
+
+  await wait(() => expect(response).toMatchObject({}));
+});
+
+test('error delete action', async () => {
+  (fetch as FetchMock).mockRejectedValue(new Response(JSON.stringify({})));
+
+  const { result: userResult } = renderHook(() => useDeleteAction());
+
+  let response: Action;
+  await act(async () => {
+    response = await userResult.current.deleteAction('123');
+  });
+
+  await wait(() => expect(response).toBeUndefined());
+});
+
+test('get plugins', async () => {
+  (fetch as FetchMock).mockResponseOnce(JSON.stringify({}));
+
+  const { result } = renderHook(() => usePlugins());
+  const { current } = result;
+
+  let response: any;
+
+  await act(async () => {
+    response = await current.getPlugins('foobar');
+  });
+
+  await wait(() => expect(response).toMatchObject({}));
+});
+
+test('error get plugin', async () => {
+  (fetch as FetchMock).mockRejectedValue(new Response(JSON.stringify({})));
+
+  const { result: userResult } = renderHook(() => usePlugins());
+
+  let response: any;
+  await act(async () => {
+    response = await userResult.current.getPlugins('foobar');
+  });
+
+  await wait(() => expect(response).toBeUndefined());
+});
