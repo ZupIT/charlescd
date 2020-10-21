a67767fe7cb033f14fd2e9ef2fe560f5597edbe4
Douglas Fernandes <douglas.fernandes@zup.com.br>
2020-10-21 10:18:00 -0300

    Remove incorrect file
    Signed-off-by: Douglas Fernandes <douglas.fernandes@zup.com.br>

--- ui/src/modules/Hypotheses/Board/Card/__tests__/Card.spec.tsx
+++ ui/src/modules/Hypotheses/Board/Card/__tests__/Card.spec.tsx
@@ -19,9 +19,86 @@ import { render, screen } from 'unit-test/testUtils';
 import { act } from 'react-dom/test-utils';
 import { FetchMock } from 'jest-fetch-mock';
 import { dark as cardBoardTheme } from 'core/assets/themes/card/board';
-import { propsAction, propsFeature } from './mocks'
 import CardBoard from '..'
 import userEvent from '@testing-library/user-event';
+import { DraggableProvidedDraggableProps, DraggableProvidedDragHandleProps } from 'react-beautiful-dnd'
+import { Card } from '../../interfaces'
+import { CARD_TYPE_ACTION, CARD_TYPE_FEATURE } from '../constants'
+import { Props as CardBoardProps } from '..'
+
+const card: Omit<Card, 'type' | 'feature'> = {
+  id: '123',
+  hypothesisId: '098',
+  name: 'Card',
+  createdAt: '2020-01-01 12:00',
+  labels: [],
+  index: 0,
+  isProtected: false
+}
+
+const cardAction: Card = {
+  ...card,
+  feature: null,
+  type: CARD_TYPE_ACTION,
+}
+
+const cardFeature: Card = {
+  ...card,
+  type: CARD_TYPE_FEATURE,
+  feature: {
+    id: '456',
+    name: 'feature',
+    branches: ['https://github.com/charlescd/tree/feature'],
+    branchName: 'feature',
+    author: {
+      id: '123',
+      name: 'Charles',
+      email: 'charlescd@zup.com.br',
+      isRoot: false,
+      createdAt: '2020-01-01 12:00'
+    },
+    modules: [{
+      id: '789',
+      name: 'ZupIT/charlescd',
+      gitRepositoryAddress: '',
+      helmRepository: ''
+    }]
+  }
+}
+
+const draggableProps: DraggableProvidedDraggableProps = {
+  "data-rbd-draggable-context-id": "0",
+  "data-rbd-draggable-id": "123",
+  onTransitionEnd: null,
+  style: {
+    transform: null,
+    transition: null
+  },
+}
+
+const dragHandleProps: DraggableProvidedDragHandleProps = {
+  "aria-labelledby": "",
+  "data-rbd-drag-handle-context-id": "0",
+  "data-rbd-drag-handle-draggable-id": "123",
+  draggable: false,
+  onDragStart: jest.fn(),
+  tabIndex: 0
+}
+
+const propsFeature: CardBoardProps = {
+  card: cardFeature,
+  columnId: '123',
+  draggableProps,
+  dragHandleProps
+}
+
+const propsAction: CardBoardProps = {
+  card: cardAction,
+  columnId: '123',
+  draggableProps,
+  dragHandleProps
+}
+
 
 beforeEach(() => {
   (fetch as FetchMock).resetMocks();
