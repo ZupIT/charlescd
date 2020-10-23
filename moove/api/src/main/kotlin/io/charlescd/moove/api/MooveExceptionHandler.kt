ab6e4a7248673794e8344269c82b304a0112fd6b
pedroborgeszup <pedro.borges@zup.com.br>
2020-10-23 13:54:40 -0300

    lint
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- moove/api/src/main/kotlin/io/charlescd/moove/api/MooveExceptionHandler.kt
+++ moove/api/src/main/kotlin/io/charlescd/moove/api/MooveExceptionHandler.kt
@@ -24,6 +24,9 @@ import io.charlescd.moove.domain.MooveErrorCode
 import io.charlescd.moove.domain.exceptions.BusinessException
 import io.charlescd.moove.domain.exceptions.ForbiddenException
 import io.charlescd.moove.domain.exceptions.NotFoundException
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+import kotlin.collections.LinkedHashMap
 import org.slf4j.LoggerFactory
 import org.springframework.context.MessageSource
 import org.springframework.dao.DataAccessException
@@ -38,9 +41,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.multipart.support.MissingServletRequestPartException
-import java.util.*
-import javax.servlet.http.HttpServletRequest
-import kotlin.collections.LinkedHashMap
 
 @ControllerAdvice
 class MooveExceptionHandler(private val messageSource: MessageSource) {
