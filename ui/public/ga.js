47c64f7bff268089054d02c08780664fb10df10d
Douglas Fernandes <douglas.fernandes@zup.com.br>
2020-10-23 09:32:58 -0300

    Fill GA script and noscript

---
+++ ui/public/ga.js
@@ -0,0 +1,10 @@
+(function(w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  const f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-M8J2Q9J');
