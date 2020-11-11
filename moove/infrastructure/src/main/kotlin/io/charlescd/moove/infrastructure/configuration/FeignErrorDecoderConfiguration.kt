d263d8a3d0f6c1838208d8be010f02fd844b75ae
Thalles Freitas <thalles.freitas@zup.com.br>
2020-11-10 23:30:46 -0300

    fix pr comment
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

---
+++ moove/infrastructure/src/main/kotlin/io/charlescd/moove/infrastructure/configuration/FeignErrorDecoderConfiguration.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.charlescd.moove.infrastructure.configuration
+
+import feign.Response
+import feign.codec.ErrorDecoder
+import io.charlescd.moove.domain.MooveErrorCode
+import io.charlescd.moove.domain.exceptions.BusinessException
+import io.charlescd.moove.domain.exceptions.NotFoundException
+import org.springframework.beans.factory.ObjectFactory
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
+
+@Configuration
+class FeignErrorDecoderConfiguration(
+) {
+    @Bean
+    fun errorDecoder(): ErrorDecoder {
+        return CustomErrorDecoder()
+    }
+}
+
+class CustomErrorDecoder : ErrorDecoder {
+    @Throws(Exception::class)
+    override fun decode(methodKey: String?, response: Response?): Exception {
+        return when (response?.status()){
+            400 -> IllegalArgumentException(response.reason())
+            422 -> BusinessException.of(MooveErrorCode.INVALID_PAYLOAD, response.reason())
+            else -> RuntimeException(response?.reason())
+        }
+    }
+
+}
