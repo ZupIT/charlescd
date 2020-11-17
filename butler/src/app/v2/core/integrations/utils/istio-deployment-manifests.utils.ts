a30fb104b69d834821dc598cc809d0baadce33dd
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2020-11-17 13:07:19 -0300

    do not repeat open sea destination rules
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- butler/src/app/v2/core/integrations/utils/istio-deployment-manifests.utils.ts
+++ butler/src/app/v2/core/integrations/utils/istio-deployment-manifests.utils.ts
@@ -59,18 +59,12 @@ const IstioDeploymentManifestsUtils = {
   getDestinationRulesSubsets: (newComponent: DeploymentComponent, circleId: string, activeByName: Component[]): Subset[] => {
     const subsets: Subset[] = []
     subsets.push(IstioManifestsUtils.getDestinationRulesSubsetObject(newComponent, circleId))
-
     activeByName.forEach(component => {
-      const activeCircleId = component.deployment?.circleId
-      if (DeploymentUtils.isDistinctAndNotDefault(component, circleId)) {
+      const activeCircleId = component.deployment.circleId
+      if (DeploymentUtils.isDistinctCircle(component, circleId)) {
         subsets.push(IstioManifestsUtils.getDestinationRulesSubsetObject(component, activeCircleId))
       }
     })
-
-    const defaultComponent: Component | undefined = activeByName.find(component => component.deployment && component.deployment.defaultCircle)
-    if (defaultComponent && defaultComponent.deployment) {
-      subsets.push(IstioManifestsUtils.getDestinationRulesSubsetObject(defaultComponent, defaultComponent.deployment.circleId))
-    }
     return subsets
   },
 
@@ -94,6 +88,7 @@ const IstioDeploymentManifestsUtils = {
     }
     return rules
   },
+
   getDefaultCircleHTTPRules: (newComponent: DeploymentComponent, activeByName: Component[], circleId: string): Http[] => {
     const rules: Http[] = []
 
