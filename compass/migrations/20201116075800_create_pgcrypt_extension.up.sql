4821e4a57395a6bcb3a1d881c558392666f1a8e8
Gabriel Leite <gabriel.leite@zup.com.br>
2020-11-23 16:36:58 -0300

    migration problem
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

--- compass/migrations/20201116075800_create_pgcrypt_extension.up.sql
+++ compass/migrations/20201116075800_create_pgcrypt_extension.up.sql
@@ -1,5 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+DELETE FROM actions_configurations;
+DELETE FROM metrics_group_actions;
+DELETE FROM actions_executions;
+DELETE FROM actions;
+DELETE FROM metric_executions;
+DELETE FROM metric_group_bies;
+DELETE FROM metrics;
+DELETE FROM metrics_groups;
 DELETE FROM data_sources;
 
 ALTER TABLE data_sources DROP COLUMN data;
