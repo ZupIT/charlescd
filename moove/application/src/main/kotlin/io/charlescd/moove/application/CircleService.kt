b465629bd29c1459e8a672d185a8fe065d3a53e7
Thalles Freitas <thalles.freitas@zup.com.br>
2020-11-25 19:20:12 -0300

    add tests
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- moove/application/src/main/kotlin/io/charlescd/moove/application/CircleService.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/CircleService.kt
@@ -91,13 +91,12 @@ class CircleService(private val circleRepository: CircleRepository) {
 
     fun checkIfPercentageCircleCanDeploy(circle: Circle, workspaceId: String) {
         val percentageCircles = this.circleRepository.findCirclesPercentage(workspaceId, null, active = true, pageRequest = null).content
-        val isAlreadyDeployed = percentageCircles.map {
-            circle -> circle.id
-        }.let {
-            circlesIds -> circlesIds.contains(circle.id)
-        }
+         val isAlreadyDeployed = percentageCircles.map{
+         circle -> circle.id
+        }.contains(circle.id)
+        val sumPercentage = percentageCircles.takeIf{it.isNotEmpty()}?.map{ it -> it.percentage}?.reduce{ acc,value -> acc?.plus(value ?: 0) } ?: 0
         if (!isAlreadyDeployed) {
-            checkIfLimitOfPercentageReached(circle.percentage!!, workspaceId)
+           verifyLimitReached(sumPercentage, circle.percentage!!)
         }
     }
 }
