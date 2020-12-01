1559c348b8940e0c1f9b27c7d080f57179df0636
Cassio Godinho <cassiopgodinho@gmail.com>
2020-12-01 10:28:02 -0300

    basic typing for hook request
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

---
+++ butler/src/resources/k8s-crds/hook.ts
@@ -0,0 +1,29 @@
+interface K8sObect<T> {
+  [key: string]: T
+}
+
+type Spec = K8sObect<string | K8sObect<string>>
+
+interface DeploymentHookRequest {
+  controller: string
+  parent: Spec  // Are we typing the k8s object?
+  // child obect example
+  // {
+  //   "Pod.v1": {
+  //     "pod-name-1": {},
+  //     "pod-name-2": {}
+  //   },
+  //   "Deployment.v1": {
+  //     "deployment-1": {},
+  //     "deployment-2": {}
+  //   }
+  // }
+  children: { 'Pod.v1': Spec }
+  finalizing: boolean // defaults to false
+}
+
+
+interface DeploymentHookResponse {
+  status: Record<string, string> // we can put any object that we want on the CRDs status field
+  children: Spec[] // array of k8s manifests that we want metacontroller to apply on the cluster
+}
