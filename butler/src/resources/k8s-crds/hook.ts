fe19e40c9ff813e9954e25b822b0221172808df4
Cassio Godinho <cassiopgodinho@gmail.com>
2020-12-01 10:38:39 -0300

    add response comments
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- butler/src/resources/k8s-crds/hook.ts
+++ butler/src/resources/k8s-crds/hook.ts
@@ -23,6 +23,7 @@ interface DeploymentHookRequest {
 }
 
 
+// If the response status is not 200 metacontroller wont apply our desired changes, maybe we can use this behaviour to wait for deployments healthchecks before applying the routes CRD
 interface DeploymentHookResponse {
   status: Record<string, string> // we can put any object that we want on the CRDs status field
   children: Spec[] // array of k8s manifests that we want metacontroller to apply on the cluster
