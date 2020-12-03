575a31051a56bd17aa173b260f97819a3d97eadb
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2020-12-03 15:51:55 -0300

    Create unit test for deep rule validation
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/validation/NodeValidationTest.groovy
+++ circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/validation/NodeValidationTest.groovy
@@ -1,5 +1,6 @@
 package io.charlescd.circlematcher.domain.validation
 
+import io.charlescd.circlematcher.domain.Content
 import io.charlescd.circlematcher.domain.LogicalOperatorType
 import io.charlescd.circlematcher.domain.Node
 import io.charlescd.circlematcher.domain.NodeType
@@ -9,7 +10,6 @@ import spock.lang.Specification
 class NodeValidationTest extends Specification {
 
     def "Validate simple RULE node type"() {
-
         given:
         def value = "user@zup.com.br"
         def values = new ArrayList()
@@ -25,7 +25,6 @@ class NodeValidationTest extends Specification {
     }
 
     def "Validate CLAUSE node with simple RULE type"() {
-
         given:
         def value = "user@zup.com.br"
         def values = new ArrayList()
@@ -42,7 +41,6 @@ class NodeValidationTest extends Specification {
     }
 
     def "Invalid CLAUSE node without simple RULE type"() {
-
         given:
         def value = "user@zup.com.br"
         def values = new ArrayList()
@@ -54,5 +52,22 @@ class NodeValidationTest extends Specification {
         def valid = validator.isValid(node, null)
         then:
         assert !valid
+    }
+
+    def "Validate CLAUSE node with deep RULE type"() {
+        given:
+        def ruleName1 = new Node(NodeType.RULE, null, null, new Content("name", "EQUAL", ["tester1"]))
+        def ruleAge = new Node(NodeType.RULE, null, null, new Content("age", "EQUAL", ["12"]))
+        def ruleName2 = new Node(NodeType.RULE, null, null, new Content("name", "EQUAL", ["tester"]))
+
+        def nameAndAge = new Node(NodeType.CLAUSE, LogicalOperatorType.AND, [ruleName1, ruleAge], null)
+        def name = new Node(NodeType.CLAUSE, LogicalOperatorType.OR, [ruleName2], null)
+
+        def node = new Node(NodeType.CLAUSE, LogicalOperatorType.OR, [nameAndAge, name], null)
+        when:
+        def validator = new NodeValidator()
+        def valid = validator.isValid(node, null)
+        then:
+        assert valid
     }
 }
