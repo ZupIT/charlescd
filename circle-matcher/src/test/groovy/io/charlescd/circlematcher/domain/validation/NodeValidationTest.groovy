9c9c8dfcd5d6af17b7c5e08ec75f8c0d5e99c5d6
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2020-12-03 16:43:14 -0300

    Add unit tests for invalid nodes without rules
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/validation/NodeValidationTest.groovy
+++ circle-matcher/src/test/groovy/io/charlescd/circlematcher/domain/validation/NodeValidationTest.groovy
@@ -1,6 +1,8 @@
 package io.charlescd.circlematcher.domain.validation
 
-
+import io.charlescd.circlematcher.domain.LogicalOperatorType
+import io.charlescd.circlematcher.domain.Node
+import io.charlescd.circlematcher.domain.NodeType
 import io.charlescd.circlematcher.utils.TestUtils
 import spock.lang.Specification
 
@@ -62,5 +64,22 @@ class NodeValidationTest extends Specification {
         def valid = validator.isValid(node, null)
         then:
         assert valid
+    }
+
+    def "Invalid CLAUSE node without deep RULE type"() {
+        given:
+        def ruleName1 = new Node(NodeType.CLAUSE, LogicalOperatorType.AND, null, TestUtils.createContent(["tester1"]))
+        def ruleAge = new Node(NodeType.CLAUSE, LogicalOperatorType.OR, null, TestUtils.createContent(["12"]))
+        def ruleName2 = new Node(NodeType.CLAUSE, LogicalOperatorType.AND, null, TestUtils.createContent(["tester"]))
+
+        def nameAndAge = TestUtils.createClauseNode([ruleName1, ruleAge])
+        def name = TestUtils.createClauseNode([ruleName2])
+
+        def node = TestUtils.createClauseNode([nameAndAge, name])
+        when:
+        def validator = new NodeValidator()
+        def valid = validator.isValid(node, null)
+        then:
+        assert !valid
     }
 }
