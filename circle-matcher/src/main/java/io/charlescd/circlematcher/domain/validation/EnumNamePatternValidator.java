5d1d022ff77d543989704b90d9b8544e2825e39a
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2020-12-07 16:01:52 -0300

    Create EnumName constraint validator
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

---
+++ circle-matcher/src/main/java/io/charlescd/circlematcher/domain/validation/EnumNamePatternValidator.java
@@ -0,0 +1,31 @@
+package io.charlescd.circlematcher.domain.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class EnumNamePatternValidator implements ConstraintValidator<EnumNameConstraint, Enum<?>> {
+
+    private Pattern pattern;
+
+    @Override
+    public void initialize(EnumNameConstraint enumNameConstraint) {
+        try {
+            pattern = Pattern.compile(enumNameConstraint.regexp());
+        } catch (PatternSyntaxException patternSyntaxException) {
+            throw new IllegalArgumentException("Given regex is invalid", patternSyntaxException);
+        }
+    }
+
+    @Override
+    public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+
+        Matcher matcher = pattern.matcher(value.name());
+        return matcher.matches();
+    }
+}
