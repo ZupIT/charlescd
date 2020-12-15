9e2c7a195c1a39fcd94b5f2d9dc003254b74d585
thalles freitas <thalles.freitas@zup.com.br>
2020-12-15 15:36:42 -0300

    remove parameter list
    Signed-off-by: thalles freitas <thalles.freitas@zup.com.br>

--- circle-matcher/src/main/java/io/charlescd/circlematcher/domain/exception/BusinessException.java
+++ circle-matcher/src/main/java/io/charlescd/circlematcher/domain/exception/BusinessException.java
@@ -1,15 +1,7 @@
 package io.charlescd.circlematcher.domain.exception;
 
-import java.util.List;
-
 public class BusinessException extends RuntimeException {
     private MatcherErrorCode errorCode;
-    private List<String> parameters;
-
-    public BusinessException(MatcherErrorCode matcherErrorCode, List<String> parameters) {
-        this.parameters = parameters;
-        this.errorCode = matcherErrorCode;
-    }
 
     public BusinessException(MatcherErrorCode matcherErrorCode) {
         this.errorCode = matcherErrorCode;
