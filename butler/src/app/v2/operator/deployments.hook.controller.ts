871a3002b60358e727e7d03dab70f177653804b8
Cassio Godinho <cassiopgodinho@gmail.com>
2021-01-04 09:09:26 -0300

    wip
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- butler/src/app/v2/operator/deployments.hook.controller.ts
+++ butler/src/app/v2/operator/deployments.hook.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body, UsePipes, ValidationPipe, HttpCode } from '@nestjs/common'
 import { HookParams } from './params.interface'
-import { isEmpty } from 'lodash'
+
 @Controller('deploymentsHook')
 export class DeploymentsHookController {
 
@@ -8,18 +8,17 @@ export class DeploymentsHookController {
   @HttpCode(200)
   @UsePipes(new ValidationPipe({ transform: true }))
   public async reconcile(@Body() params: HookParams) : Promise<{status?: unknown, children: Record<string, unknown>[]}> {
-    // console.log(JSON.stringify(params.children))
+    console.log(JSON.stringify(params.children))
     const deploymentId = params.parent.spec.deploymentId
     // const specs = `select manifests from v2deployments where id = deploymentId`
-    let specs = deploymentsSpec
+    const specs = deploymentsSpec
     const deploymentNames = Object.keys(params.children['Deployment.apps/v1'])
     const allReady = deploymentNames.every(d => {
       const conditions = params.children['Deployment.apps/v1'][d].status.conditions
       return conditions.some(condition => condition.type === 'Available' && condition.status === 'True')
     })
     if (allReady) {
-      specs = specs.concat(routesSpec)
-
+      // k8sClient.createRoutesCrd(deploymentId)
     }
     return { children: specs }
   }
@@ -30,25 +29,21 @@ export class DeploymentsHookController {
   public async finalize(@Body() params: HookParams): Promise<{ status?: unknown, children: Record<string, unknown>[], finalized: boolean, resyncAfterSeconds?: number }> {
     // console.log(JSON.stringify(params))
     const deploymentId = params.parent.spec.deploymentId
-    let finalized = false
-    let specs = deploymentsSpec
-    console.log('=======================')
-    console.log(JSON.stringify(params.children['Service.v1']))
-    console.log('=======================')
-    if (isEmpty(params.children['Service.v1'])) {
-      specs = []
-      // k8sClient.createRoutesResource(params.children)
-      // - id: 123  << remove those
-      //   components:
-      //   - quiz-app-backend
-      //   - quiz-app-frontend
-    }
-    if (isEmpty(params.children['Service.v1']) && isEmpty(params.children['Deployment.apps/v1'])) {
-      // when everything is cleaned up update the database to signal the undeploy
-      // deploymentRepository.update(params.parent.spec.deploymentId, {active: false})
-      finalized = true
-    }
-    console.log(JSON.stringify(specs))
+    const finalized = false
+    // deploymentRepository.update(params.parent.spec.deploymentId, {active: false})
+    // const activeDeployment = deploymentRepository.findActives(params.parent.spec.circleId)
+    // applyRoutingCustomResource(activeDeployment.cdConfigurationId, activeDeployment.components)
+    // const specs = activeDeployment.toK8sSpecs() ???
+    const specs : Record<string, unknown>[] = []
+
+    // wen cant trust that everything went well instantly, we need to keep returning finalized = true until we are sure there are no more routes to this deployment
+    // if (checkRoutes(, deploymentIdk8sClientGetCrdRoutes())) {
+    // when everything is cleaned up update the database to signal the undeploy
+    // finalized = true
+    // }
+
+    // if finalized = true metacontroller wont try to call this hook for this resource anymore, if finalize = false we return a resyncAfterSeconds to signal
+    // in how much time metacontroller will call this same hook
     if (finalized) {
       return { children: specs, finalized: finalized }
     }
@@ -57,34 +52,6 @@ export class DeploymentsHookController {
     }
   }
 }
-
-// example spec to represent istio manifests
-const routesSpec : Record<string, unknown>[] = [
-  {
-    'apiVersion': 'v1',
-    'kind': 'Service',
-    'metadata': {
-      'labels': {
-        'app': 'charlescd-ui',
-        'service': 'charlescd-ui'
-      },
-      'name': 'charlescd-ui',
-      'namespace': 'default'
-    },
-    'spec': {
-      'ports': [
-        {
-          'name': 'http',
-          'port': 3000,
-          'targetPort': 3000
-        }
-      ],
-      'selector': {
-        'app': 'charlescd-ui'
-      }
-    }
-  }
-]
 
 // example spec to represent helm manifests
 const deploymentsSpec : Record<string, unknown>[]= [
@@ -170,6 +137,30 @@ const deploymentsSpec : Record<string, unknown>[]= [
             }
           ]
         }
+      }
+    }
+  },
+  {
+    'apiVersion': 'v1',
+    'kind': 'Service',
+    'metadata': {
+      'labels': {
+        'app': 'charlescd-ui',
+        'service': 'charlescd-ui'
+      },
+      'name': 'charlescd-ui',
+      'namespace': 'default'
+    },
+    'spec': {
+      'ports': [
+        {
+          'name': 'http',
+          'port': 3000,
+          'targetPort': 3000
+        }
+      ],
+      'selector': {
+        'app': 'charlescd-ui'
       }
     }
   }
