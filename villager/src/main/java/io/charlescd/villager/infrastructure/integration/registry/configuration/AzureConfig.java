cda6d8d831033491631316e18c92b080f510ffea
barbararochazup <barbara.rocha@zup.com.br>
2021-01-05 11:27:32 -0300

    fix lint
    Signed-off-by: barbararochazup <barbara.rocha@zup.com.br>

--- villager/src/main/java/io/charlescd/villager/infrastructure/integration/registry/configuration/AzureConfig.java
+++ villager/src/main/java/io/charlescd/villager/infrastructure/integration/registry/configuration/AzureConfig.java
@@ -2,6 +2,7 @@ package io.charlescd.villager.infrastructure.integration.registry.configuration;
 
 import io.charlescd.villager.infrastructure.integration.registry.authentication.CommonBasicAuthenticator;
 import io.charlescd.villager.infrastructure.persistence.DockerRegistryConfigurationEntity;
+
 public class AzureConfig {
 
     public static Object execute(ConfigParameters config) {
