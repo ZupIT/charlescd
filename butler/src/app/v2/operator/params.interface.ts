a590ead0a8e0960b7931558e31d08cc93ee81d5a
Cassio Godinho <cassiopgodinho@gmail.com>
2021-01-15 15:54:28 -0300

    fix k8s services interface
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- butler/src/app/v2/operator/params.interface.ts
+++ butler/src/app/v2/operator/params.interface.ts
@@ -12,23 +12,43 @@ export interface HookParams {
   }
   children: {
     'Deployment.apps/v1': DeploymentSpec,
-    'Service.v1': Record<string, unknown>
+    'Service.v1': ServiceSpec
   }
   finalizing: boolean
 }
 
-interface DeploymentSpec {
+export interface SpecMetadata {
+  labels: {
+    app: string
+    circle_id: string
+    deployment_id: string
+  }
+}
+
+export interface SpecStatus {
+  availableReplicas: number
+  conditions: {
+    lastTransitionTime: string
+    lastUpdateTime: string
+    message: string
+    reason: string
+    status: 'True' | 'False' // TODO: check if this can be other values
+    type: 'Progressing' | 'Available'  // TODO: check if this can be other values
+  }[]
+}
+
+export interface DeploymentSpec {
   [key: string]: {
+    metadata: SpecMetadata
+    status: SpecStatus
+  }
+}
+
+export interface ServiceSpec {
+  [key: string]: {
+    metadata: SpecMetadata
     status: {
-      availableReplicas: number
-      conditions: {
-        lastTransitionTime: string
-        lastUpdateTime: string
-        message: string
-        reason: string
-        status: 'True' | 'False' // TODO: check if this can be other values
-        type: 'Progressing' | 'Available'  // TODO: check if this can be other values
-      }[]
+      loadBalancer: Record<string, unknown>
     }
   }
 }
