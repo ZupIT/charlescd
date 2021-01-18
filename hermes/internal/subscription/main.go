21f800cb0c0725bd4ce1b1933019bc81661f0652
arthur pucci <arthur.pucci@zup.com.br>
2021-01-18 14:44:22 -0300

    Adding copyright to files
    Signed-off-by: arthur pucci <arthur.pucci@zup.com.br>

--- hermes/internal/subscription/main.go
+++ hermes/internal/subscription/main.go
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package subscription
 
 import (
@@ -12,12 +30,12 @@ type UseCases interface {
 	ParseUpdate(subscription io.ReadCloser) ([]byte, errors.Error)
 	Validate(subscription Request) errors.ErrorList
 	Save(subscription Request) (SaveResponse, errors.Error)
-	Update(subscriptionId uuid.UUID,subscription []byte) (UpdateResponse, errors.Error)
+	Update(subscriptionId uuid.UUID, subscription []byte) (UpdateResponse, errors.Error)
 	Delete(subscriptionId uuid.UUID, author string) errors.Error
 	FindById(subscriptionId uuid.UUID) (Response, errors.Error)
 }
 type Main struct {
-	db         *gorm.DB
+	db *gorm.DB
 }
 
 func NewMain(db *gorm.DB) UseCases {
