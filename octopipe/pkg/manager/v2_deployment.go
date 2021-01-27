aee4cc5612c4b138938417fdf6e5c0b96f7af938
Cassio Godinho <cassiopgodinho@gmail.com>
2021-01-27 08:24:07 -0300

    add unused proxy undeployment field to octo struct
    Signed-off-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- octopipe/pkg/manager/v2_deployment.go
+++ octopipe/pkg/manager/v2_deployment.go
@@ -40,6 +40,12 @@ func (manager Manager) ExecuteV2DeploymentPipeline(v2Pipeline V2DeploymentPipeli
 	if err != nil {
 		log.WithFields(log.Fields{"function": "ExecuteV2DeploymentPipeline", "error": err.Error()}).Info("ERROR:RUN_V2_UNUSED_DEPLOYMENTS")
 	}
+
+	err = manager.runV2unusedProxyDeployments(v2Pipeline)
+
+	if err != nil {
+		log.WithFields(log.Fields{"function": "ExecuteV2DeploymentPipeline", "error": err.Error()}).Info("ERROR:RUN_V2_UNUSED_PROXY_DEPLOYMENTS")
+	}
 	log.WithFields(log.Fields{"function": "ExecuteV2DeploymentPipeline"}).Info("FINISH:EXECUTE_V2_DEPLOYMENT_PIPELINE")
 }
 
@@ -73,6 +79,20 @@ func (manager Manager) runV2Rollbacks(v2Pipeline V2DeploymentPipeline) error {
 
 func (manager Manager) runV2ProxyDeployments(v2Pipeline V2DeploymentPipeline) error {
 	log.WithFields(log.Fields{"function": "runV2ProxyDeployments", "proxyDeployments": v2Pipeline.ProxyDeployments}).Info("START:RUN_V2_PROXY_DEPLOYMENTS")
+	errs, _ := errgroup.WithContext(context.Background())
+	for _, proxyDeployment := range v2Pipeline.ProxyDeployments {
+		currentProxyDeployment := map[string]interface{}{} // TODO improve this
+		currentProxyDeployment["default"] = proxyDeployment
+		errs.Go(func() error {
+			return manager.executeV2Manifests(v2Pipeline.ClusterConfig, currentProxyDeployment, v2Pipeline.Namespace, DEPLOY_ACTION)
+		})
+	}
+	log.WithFields(log.Fields{"function": "runV2ProxyDeployments"}).Info("FINISH:RUN_V2_PROXY_DEPLOYMENTS")
+	return errs.Wait()
+}
+
+func (manager Manager) runV2unusedProxyDeployments(v2Pipeline V2DeploymentPipeline) error {
+	log.WithFields(log.Fields{"function": "runV2unusedProxyDeployments", "unusedProxyDeployments": v2Pipeline.UnusedProxyDeployments}).Info("START:RUN_V2_UNUSED_PROXY_DEPLOYMENTS")
 	errs, _ := errgroup.WithContext(context.Background())
 	for _, proxyDeployment := range v2Pipeline.ProxyDeployments {
 		currentProxyDeployment := map[string]interface{}{} // TODO improve this
