31f9d8dad3d4c22f6f2948728ccf720b22c30304
Lucas Sales <lucas.sales@zup.com.br>
2021-01-28 10:47:04 -0300

    add DocumentationLink componente
    Signed-off-by: Lucas Sales <lucas.sales@zup.com.br>

---
+++ ui/src/core/components/DocumentationLink/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const DocumentationLink = styled.a`
+  text-decoration: underline;
+  color: ${({ theme }) => theme.text.link};
+`;
+
+export default {
+    DocumentationLink
+}
