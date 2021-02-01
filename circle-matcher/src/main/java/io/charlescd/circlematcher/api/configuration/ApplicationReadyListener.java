9db4aa0bc4080cb9c786d5feb9441440b1e4ca0a
thalles freitas <thalles.freitas@zup.com.br>
2021-02-01 17:12:35 -0300

    fix conflict
    Signed-off-by: thalles freitas <thalles.freitas@zup.com.br>

---
+++ circle-matcher/src/main/java/io/charlescd/circlematcher/api/configuration/ApplicationReadyListener.java
@@ -0,0 +1,51 @@
+package io.charlescd.circlematcher.api.configuration;
+
+import io.charlescd.circlematcher.domain.KeyMetadata;
+import io.charlescd.circlematcher.domain.Segmentation;
+import io.charlescd.circlematcher.domain.SegmentationType;
+import io.charlescd.circlematcher.infrastructure.repository.KeyMetadataRepository;
+import io.charlescd.circlematcher.infrastructure.repository.SegmentationRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+    private KeyMetadataRepository keyMetadataRepository;
+    private SegmentationRepository segmentationRepository;
+    public ApplicationReadyListener(
+    ){
+    }
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        var oldMetaData = this.keyMetadataRepository.findAllOldMetadata();
+        oldMetaData.forEach(
+                this::updateOldMetadata
+        );
+    }
+
+    private void updateOldMetadata(KeyMetadata keyMetadata) {
+        if (!keyMetadata.getIsDefault()) {
+            var optionalSegmentation = findSegmentation(keyMetadata);
+            this.keyMetadataRepository.remove(keyMetadata);
+            keyMetadata.setActive(true);
+            this.keyMetadataRepository.create(keyMetadata);
+            if (optionalSegmentation.isPresent()) {
+                var segmentation = optionalSegmentation.get();
+                this.segmentationRepository.removeByKey(keyMetadata.getKey());
+                segmentation.setActive(true);
+                this.segmentationRepository.create(keyMetadata.getKey(), segmentation);
+            }
+        }
+    }
+
+    private Optional<Segmentation> findSegmentation(KeyMetadata keyMetadata) {
+        if (keyMetadata.getType() != SegmentationType.SIMPLE_KV) {
+            return this.segmentationRepository.findByKey(keyMetadata.getKey());
+        }
+        return Optional.empty();
+    }
+
+}
