1a60ec9f3e38e1627642812d108ac07d1a7d2f1d
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2021-02-04 14:54:20 -0300

    Code gardening
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- butler/src/app/v2/api/deployments/controller/executions.controller.ts
+++ butler/src/app/v2/api/deployments/controller/executions.controller.ts
@@ -1,15 +1,11 @@
-import { Body, Controller, Get, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
-import { DeploymentNotificationRequestDto } from '../dto/deployment-notification-request.dto'
+import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common'
 import { ExecutionQuery } from '../dto/execution/paginated-execution-query.dto'
 import { PaginatedExecutions } from '../dto/execution/paginated-executions.dto'
-import { Execution } from '../entity/execution.entity'
 import { PaginatedExecutionsUseCase } from '../use-cases/paginated-executions.usecase'
-import { ReceiveNotificationUseCase } from '../use-cases/receive-notification.usecase'
 
 @Controller('v2/executions')
 export class ExecutionsController {
   constructor(
-    private receiveNotificationUseCase: ReceiveNotificationUseCase,
     private paginatedExecutionsUseCase: PaginatedExecutionsUseCase
   ) { }
 
