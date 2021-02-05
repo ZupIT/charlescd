00ae93dec074b3f50743a94882d0b8f1d30ebad9
Gabriel Leite <gabriel.leite@zup.com.br>
2021-02-05 15:05:46 -0300

    Removed GetBytes
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

--- hermes/util/model.go
+++ hermes/util/model.go
@@ -19,8 +19,6 @@
 package util
 
 import (
-	"bytes"
-	"encoding/gob"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
@@ -34,14 +32,4 @@ type BaseModel struct {
 func (baseModel *BaseModel) BeforeCreate(scope *gorm.DB) error {
 	baseModel.ID = uuid.New()
 	return nil
-}
-
-func GetBytes(key interface{}) ([]byte, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(key)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
 }
