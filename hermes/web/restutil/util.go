0ee7a73bc572aad6b5b46b23c651269fe88ab4f8
barbararochazup <barbara.rocha@zup.com.br>
2021-02-08 14:29:45 -0300

    adding contenty-type to api response on hermes
    Signed-off-by: barbararochazup <barbara.rocha@zup.com.br>

--- hermes/web/restutil/util.go
+++ hermes/web/restutil/util.go
@@ -25,5 +25,6 @@ import (
 
 func NewResponse(w http.ResponseWriter, status int, data interface{}) {
 	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
