89f6219007b734fd03ee1f73a556464e985fccb1
Leandro Queiroz <leandroqueiroz92@gmail.com>
2021-02-09 09:39:21 -0300

    reactived logout function
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- ui/src/core/utils/auth.ts
+++ ui/src/core/utils/auth.ts
@@ -23,7 +23,7 @@ import { clearProfile } from './profile';
 import { clearWorkspace } from './workspace';
 import { HTTP_STATUS } from 'core/enums/HttpStatus';
 import { redirectTo } from './routes';
-// import routes from 'core/constants/routes';
+import routes from 'core/constants/routes';
 import { getProfileByKey } from 'core/utils/profile';
 import { microfrontendKey } from './microfrontend';
 
@@ -45,7 +45,7 @@ const IDMUrl = window.CHARLESCD_ENVIRONMENT?.REACT_APP_AUTH_URI;
 const IDMRealm = window.CHARLESCD_ENVIRONMENT?.REACT_APP_AUTH_REALM;
 const IDMClient = window.CHARLESCD_ENVIRONMENT?.REACT_APP_AUTH_CLIENT_ID;
 const IDMUrlLogin = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_LOGIN_URI;
-// const IDMUrlLogout = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_LOGOUT_URI;
+const IDMUrlLogout = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_LOGOUT_URI;
 const IDMUrlRedirect = window.CHARLESCD_ENVIRONMENT?.REACT_APP_IDM_REDIRECT_URI;
 
 export const setAccessToken = (token: string) =>
@@ -120,24 +120,24 @@ export const redirectToIDM = () => {
 };
 
 export const logout = () => {
-  // if (isIDMEnabled()) {
-  //   const refreshToken = getRefreshToken();
-  //   const url = `${IDMUrl}/auth/realms/${IDMRealm}${IDMUrlLogout}`;
+  if (isIDMEnabled()) {
+    const refreshToken = getRefreshToken();
+    const url = `${IDMUrl}/auth/realms/${IDMRealm}${IDMUrlLogout}`;
 
-  //   fetch(url, {
-  //     headers: {
-  //       'Content-Type': 'application/x-www-form-urlencoded'
-  //     },
-  //     body: `client_id=${IDMClient}&refresh_token=${refreshToken}`,
-  //     method: 'POST'
-  //   }).finally(() => {
-  //     clearSession();
-  //     redirectTo(routes.main);
-  //   });
-  // } else {
-  //   clearSession();
-  //   redirectTo(routes.login);
-  // }
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `client_id=${IDMClient}&refresh_token=${refreshToken}`,
+      method: 'POST'
+    }).finally(() => {
+      clearSession();
+      redirectTo(routes.main);
+    });
+  } else {
+    clearSession();
+    redirectTo(routes.login);
+  }
 };
 
 export const checkStatus = (status: number) => {
