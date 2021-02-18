3812f9520d92e31defcbc6e76141571bc05e441b
Mateus Cruz <emiteze@hotmail.com>
2021-02-18 18:47:47 -0300

    Adding domain classes
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

---
+++ gate/internal/domain/user.go
@@ -0,0 +1,15 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID
+	Name      string
+	PhotoUrl  string
+	Email     string
+	IsRoot    bool
+	CreatedAt time.Time
+}
