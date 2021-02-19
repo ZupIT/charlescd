b954881c98a9555c393f37180b823fa3eab0c171
barbararochazup <barbara.rocha@zup.com.br>
2021-02-19 12:36:46 -0300

    adding pagination to history
    Signed-off-by: barbararochazup <barbara.rocha@zup.com.br>

--- hermes/internal/notification/message/main.go
+++ hermes/internal/notification/message/main.go
@@ -32,7 +32,7 @@ type UseCases interface {
 	ParsePayload(request io.ReadCloser) (payloads.PayloadRequest, errors.Error)
 	Validate(message payloads.PayloadRequest) errors.ErrorList
 	Publish(messagesRequest []payloads.Request) ([]payloads.MessageResponse, errors.Error)
-	FindAllBySubscriptionId(subscriptionId uuid.UUID, parameters map[string]string) ([]payloads.FullMessageResponse, errors.Error)
+	FindAllBySubscriptionId(subscriptionId uuid.UUID, parameters map[string]string, page int, size int) ([]payloads.FullMessageResponse, errors.Error)
 	FindAllNotEnqueued() ([]payloads.MessageResponse, errors.Error)
 	FindMostRecent(subscriptionId uuid.UUID) (payloads.StatusResponse, errors.Error)
 }
