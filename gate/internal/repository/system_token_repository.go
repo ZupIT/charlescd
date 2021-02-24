b0ec8d6b4231ee6f523022ed9fa50a62c5dc3548
Mateus Cruz <emiteze@hotmail.com>
2021-02-24 11:25:26 -0300

    Adding create function on repository
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

--- gate/internal/repository/system_token_repository.go
+++ gate/internal/repository/system_token_repository.go
@@ -1,3 +1,40 @@
 package repository
 
-type SystemTokenRepository interface {}
+import (
+	"fmt"
+	"github.com/ZupIT/charlescd/gate/internal/domain"
+	"github.com/ZupIT/charlescd/gate/internal/logging"
+	"github.com/ZupIT/charlescd/gate/internal/repository/models"
+	"github.com/google/uuid"
+	"github.com/nleof/goyesql"
+	"gorm.io/gorm"
+)
+
+type SystemTokenRepository interface {
+	Create(systemToken domain.SystemToken) (domain.SystemToken, error)
+}
+
+type systemTokenRepository struct {
+	db      *gorm.DB
+	queries goyesql.Queries
+}
+
+func NewSystemTokenRepository(db *gorm.DB, queriesPath string) (SystemTokenRepository, error) {
+	queries, err := goyesql.ParseFile(fmt.Sprintf("%s/%s", queriesPath, "system_token_queries.sql"))
+	if err != nil {
+		return systemTokenRepository{}, err
+	}
+
+	return systemTokenRepository{db: db, queries: queries}, nil
+}
+
+func (systemTokenRepository systemTokenRepository) Create(systemToken domain.SystemToken) (domain.SystemToken, error) {
+	systemToken.ID = uuid.New()
+	systemTokenToSave := models.SystemTokenDomainToModel(systemToken)
+
+	if res := systemTokenRepository.db.Save(&systemTokenToSave); res.Error != nil {
+		return domain.SystemToken{}, logging.NewError("Save system token failed", res.Error, nil, "repository.Create.Save")
+	}
+
+	return systemToken, nil
+}
