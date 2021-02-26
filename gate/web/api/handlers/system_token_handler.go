dceedc40a9f1a324205db66f0ee64d40d407dae6
pedroborgeszup <pedro.borges@zup.com.br>
2021-02-26 14:45:40 -0300

    reorganazing
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- gate/web/api/handlers/system_token_handler.go
+++ gate/web/api/handlers/system_token_handler.go
@@ -7,7 +7,33 @@ import (
 	uuidPkg "github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
 )
+
+func GetAllSystemTokens(findAllSystemToken systemTokenInteractor.FindAllSystemToken) echo.HandlerFunc {
+	return func(echoCtx echo.Context) error {
+
+		ctx := echoCtx.Request().Context()
+
+		page, err := strconv.Atoi(echoCtx.QueryParam("page"))
+		if err != nil {
+			return echoCtx.JSON(http.StatusBadRequest, err)
+		}
+
+		size, err := strconv.Atoi(echoCtx.QueryParam("size"))
+		if err != nil {
+			return echoCtx.JSON(http.StatusBadRequest, err)
+		}
+
+		systemTokens, err := findAllSystemToken.Execute(page, size)
+		if err != nil {
+			logging.LogErrorFromCtx(ctx, err)
+			return echoCtx.JSON(http.StatusInternalServerError, err)
+		}
+
+		return echoCtx.JSON(http.StatusOK, representation.PageSystemTokenToPageResponse(systemTokens))
+	}
+}
 
 func GetSystemToken(getSystemToken systemTokenInteractor.GetSystemToken) echo.HandlerFunc {
 	return func(echoCtx echo.Context) error {
@@ -22,7 +48,7 @@ func GetSystemToken(getSystemToken systemTokenInteractor.GetSystemToken) echo.Ha
 		if err != nil {
 			return HandlerError(echoCtx, ctx, err)
 		}
-		return echoCtx.JSON(http.StatusOK, representation.DomainToResponse(user))
+		return echoCtx.JSON(http.StatusOK, representation.SystemTokenToResponse(user))
 	}
 }
 
