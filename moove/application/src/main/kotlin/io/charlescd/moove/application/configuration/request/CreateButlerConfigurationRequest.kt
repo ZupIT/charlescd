f11e4f4f45bef87b77d5e4c613ba979f4511eba8
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2021-02-26 16:53:40 -0300

    Add request and domain models
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

---
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/configuration/request/CreateButlerConfigurationRequest.kt
@@ -0,0 +1,31 @@
+package io.charlescd.moove.application.configuration.request
+
+import io.charlescd.moove.domain.ButlerConfiguration
+import io.charlescd.moove.domain.User
+import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class CreateButlerConfigurationRequest(
+    @field:NotBlank
+    @field:NotNull
+    val butlerUrl: String,
+
+    @field:NotBlank
+    @field:NotNull
+    val namespace: String,
+
+    @field:NotBlank
+    @field:NotNull
+    val gitToken: String
+) {
+
+    fun toButlerConfiguration(workspaceId: String, author: User) = ButlerConfiguration(
+        id = UUID.randomUUID().toString(),
+        author = author,
+        workspaceId = workspaceId,
+        butlerUrl = butlerUrl,
+        namespace = namespace,
+        gitToken = gitToken
+    )
+}
---
+++ moove/domain/src/main/kotlin/io/charlescd/moove/domain/ButlerConfiguration.kt
@@ -0,0 +1,13 @@
+package io.charlescd.moove.domain
+
+import java.time.LocalDateTime
+
+data class ButlerConfiguration(
+    val id: String,
+    val author: User,
+    val workspaceId: String,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val butlerUrl: String,
+    val namespace: String,
+    val gitToken: String
+)
