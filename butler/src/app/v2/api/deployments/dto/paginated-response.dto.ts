e64152184f6c144bdf7ea5d08f996eb52d070683
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2021-03-02 11:17:09 -0300

    Code gardening
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- butler/src/app/v2/api/deployments/dto/paginated-response.dto.ts
+++ butler/src/app/v2/api/deployments/dto/paginated-response.dto.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-export class PaginatedExecutions<T> {
+export class PaginatedResponse<T> {
 
   public readonly last: boolean
 
--- butler/src/app/v2/api/deployments/use-cases/paginated-executions.usecase.ts
+++ butler/src/app/v2/api/deployments/use-cases/paginated-executions.usecase.ts
@@ -1,7 +1,7 @@
 import { InjectRepository } from '@nestjs/typeorm'
 import { ConsoleLoggerService } from '../../../core/logs/console/console-logger.service'
 import { ExecutionQuery } from '../dto/execution/paginated-execution-query.dto'
-import { PaginatedExecutions } from '../dto/paginated-executions.dto'
+import { PaginatedResponse } from '../dto/paginated-response.dto'
 import { Execution } from '../entity/execution.entity'
 import { ExecutionRepository } from '../repository/execution.repository'
 
@@ -15,11 +15,11 @@ export class PaginatedExecutionsUseCase {
     private readonly consoleLoggerService: ConsoleLoggerService
   ) { }
 
-  public async execute(params: ExecutionQuery): Promise<PaginatedExecutions<Execution>>{
+  public async execute(params: ExecutionQuery): Promise<PaginatedResponse<Execution>>{
     this.consoleLoggerService.log('START:START_EXECUTIONS_PAGINATION', { params: params })
     const [executions, total] = await this.executionRepository.listExecutionsAndRelations(this.getPageSize(params.size), params.page, params.active)
     const totalPages = Math.round(Math.ceil(total / params.size))
-    const response = new PaginatedExecutions<Execution>(executions, executions.length, params.page, totalPages)
+    const response = new PaginatedResponse<Execution>(executions, executions.length, params.page, totalPages)
     return response
   }
 
