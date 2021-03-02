eaafbad5bd7c000c2918d280a897bac829edcf63
Mônica Ribeiro <monicarib1@gmail.com>
2021-03-02 10:11:51 -0300

    Implementing revoke handler.g

--- gate/web/api/handlers/system_token_handler.go
+++ gate/web/api/handlers/system_token_handler.go
@@ -1,18 +1,20 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/ZupIT/charlescd/gate/internal/logging"
 	systemTokenInteractor "github.com/ZupIT/charlescd/gate/internal/use_case/system_token"
 	"github.com/ZupIT/charlescd/gate/web/api/handlers/representation"
 	uuidPkg "github.com/google/uuid"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func GetSystemToken(getSystemToken systemTokenInteractor.GetSystemToken) echo.HandlerFunc {
 	return func(echoCtx echo.Context) error {
 		ctx := echoCtx.Request().Context()
 		uuid, parseErr := uuidPkg.Parse(echoCtx.Param("id"))
+
 		if parseErr != nil {
 			logging.LogErrorFromCtx(ctx, parseErr)
 			return echoCtx.JSON(http.StatusBadRequest, logging.NewError("Parse id failed", parseErr, logging.ParseError, nil))
@@ -26,6 +28,22 @@ func GetSystemToken(getSystemToken systemTokenInteractor.GetSystemToken) echo.Ha
 	}
 }
 
+func RevokeSytemToken(revokeSystemToken systemTokenInteractor.RevokeSystemToken) echo.HandlerFunc {
+	return func(echoCtx echo.Context) error {
+		ctx := echoCtx.Request().Context()
+		uuid, parseErr := uuidPkg.Parse(echoCtx.Param("id"))
 
+		if parseErr != nil {
+			logging.LogErrorFromCtx(ctx, parseErr)
+			return echoCtx.JSON(http.StatusBadRequest, logging.NewError("Parse id failed", parseErr, logging.ParseError, nil))
+		}
 
+		err := revokeSystemToken.Execute(uuid)
 
+		if err != nil {
+			return HandlerError(echoCtx, ctx, err)
+		}
+
+		return echoCtx.JSON(http.StatusNoContent)
+	}
+}
