3fca5fb7f96c2c99b7d7eca3b2556e6d93da0641
barbararochazup <barbara.rocha@zup.com.br>
2021-03-03 16:04:20 -0300

    fixing dates zero value and refactoring tests
    Signed-off-by: barbararochazup <barbara.rocha@zup.com.br>

--- gate/tests/unit/system_token_test.go
+++ gate/tests/unit/system_token_test.go
@@ -6,16 +6,17 @@ import (
 	systemTokenInteractor "github.com/ZupIT/charlescd/gate/internal/use_case/system_token"
 	mocks "github.com/ZupIT/charlescd/gate/tests/unit/mocks/repository"
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 type SystemTokenSuite struct {
 	suite.Suite
-	getSystemToken systemTokenInteractor.GetSystemToken
+	getSystemToken             systemTokenInteractor.GetSystemToken
 	systemTokenRepository      *mocks.SystemTokenRepository
+	systemToken                domain.SystemToken
 }
 
 func (st *SystemTokenSuite) SetupSuite() {
@@ -32,22 +33,50 @@ func TestSuite(t *testing.T) {
 }
 
 func (st *SystemTokenSuite) TestGetByID() {
-	st.systemTokenRepository.On("FindById", mock.Anything).Return(domain.SystemToken{ID: uuid.New()}, nil)
-	a, err := st.getSystemToken.Execute(uuid.New())
-	require.NotNil(st.T(), a)
+	systemToken := getDummySystemToken()
+
+	st.systemTokenRepository.On("FindById", systemToken.ID).Return(systemToken, nil)
+	result, err := st.getSystemToken.Execute(systemToken.ID)
+
+	require.NotNil(st.T(), result)
 	require.Nil(st.T(), err)
+	require.Equal(st.T(), result.ID, systemToken.ID)
+	require.True(st.T(), st.systemTokenRepository.AssertCalled(st.T(), "FindById", systemToken.ID))
 }
 
 func (st *SystemTokenSuite) TestErrorGetByIDNotFound() {
-	st.systemTokenRepository.On("FindById", mock.Anything).Return(domain.SystemToken{}, logging.NewError("Not found error", logging.CustomError{} ,  logging.NotFoundError, nil))
-	_, err := st.getSystemToken.Execute(uuid.New())
+	systemToken := getDummySystemToken()
+
+	st.systemTokenRepository.On("FindById", systemToken.ID).
+		Return(systemToken, logging.NewError("Not found error", logging.CustomError{} ,  logging.NotFoundError, nil))
+	result, err := st.getSystemToken.Execute(systemToken.ID)
+
 	require.Error(st.T(), err)
 	require.Equal(st.T(), logging.NotFoundError, logging.GetErrorType(err))
+	require.Equal(st.T(), result.ID, uuid.Nil)
+	require.True(st.T(), st.systemTokenRepository.AssertCalled(st.T(), "FindById", systemToken.ID))
+
 }
 
 func (st *SystemTokenSuite) TestErrorGetByIDInternalError() {
-	st.systemTokenRepository.On("FindById", mock.Anything).Return(domain.SystemToken{}, logging.NewError("Internal error", logging.CustomError{}, logging.InternalError, nil))
-	_, err := st.getSystemToken.Execute(uuid.New())
+	systemToken := getDummySystemToken()
+
+	st.systemTokenRepository.On("FindById", systemToken.ID).
+		Return(systemToken, logging.NewError("Internal error", logging.CustomError{}, logging.InternalError, nil))
+	result, err := st.getSystemToken.Execute(systemToken.ID)
+
 	require.Error(st.T(), err)
 	require.Equal(st.T(), logging.InternalError, logging.GetErrorType(err))
+	require.Equal(st.T(), result.ID, uuid.Nil)
+	require.True(st.T(), st.systemTokenRepository.AssertCalled(st.T(), "FindById", systemToken.ID))
+}
+
+func getDummySystemToken() domain.SystemToken {
+	return domain.SystemToken{
+		ID: uuid.New(),
+		Name: "SystemToken Test",
+		AuthorEmail: "joe.doe@email.com",
+		CreatedAt: time.Now(),
+		Revoked: false,
+	}
 }
--- gate/web/api/handlers/representation/system_token.go
+++ gate/web/api/handlers/representation/system_token.go
@@ -13,25 +13,26 @@ type SystemTokenRequest struct {
 }
 
 type SystemTokenResponse struct {
-	ID uuid.UUID `json:"id"`
-	Name string `json:"name"`
-	Permissions []PermissionResponse `json:"permissions"`
-	CreatedAt time.Time `json:"created_at"`
-	RevokedAt time.Time `json:"revoked_at"`
-	LastUsedAt time.Time `json:"last_used_at"`
-	Author string `json:"author"`
+	ID				uuid.UUID `json:"id"`
+	Name 			string `json:"name"`
+	Permissions 	[]PermissionResponse `json:"permissions"`
+	CreatedAt 		*time.Time `json:"created_at"`
+	RevokedAt  		*time.Time `json:"revoked_at,omitempty"`
+	LastUsedAt 		*time.Time `json:"last_used_atm,omitempty"`
+	Author 			string `json:"author"`
 }
 
-func (systemTokenRequest SystemTokenRequest) RequestToDomain(author domain.User) domain.SystemToken {
+func (systemTokenRequest SystemTokenRequest) RequestToDomain(author string) domain.SystemToken {
+	createdAt := time.Now()
 	return domain.SystemToken{
 		ID:          uuid.New(),
 		Name:        systemTokenRequest.Name,
 		Revoked:     false,
 		Permissions: []domain.Permission{},
-		CreatedAt:   time.Now(),
-		RevokedAt:   time.Time{},
-		LastUsedAt:  time.Time{},
-		Author:      author,
+		CreatedAt:   &createdAt,
+		RevokedAt:   &time.Time{},
+		LastUsedAt:  &time.Time{},
+		AuthorEmail: author,
 	}
 }
 
@@ -43,6 +44,6 @@ func DomainToResponse(systemToken domain.SystemToken) SystemTokenResponse {
 		CreatedAt: systemToken.CreatedAt,
 		RevokedAt: systemToken.RevokedAt,
 		LastUsedAt: systemToken.LastUsedAt,
-		Author: systemToken.Author.Email,
+		Author: systemToken.AuthorEmail,
 	}
 }
