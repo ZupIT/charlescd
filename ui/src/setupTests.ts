a26bf792f6368cef8d10a8b3007a08a76d1e47a8
Washington Soares <washingtonudi@gmail.com>
2021-03-08 11:41:22 -0300

    Add missing file license header
    Signed-off-by: Washington Soares <washingtonudi@gmail.com>

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -18,6 +18,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom/extend-expect';
 import fetch, { FetchMock } from 'jest-fetch-mock';
 import storageMock from 'unit-test/local-storage';
