f6c7003233c1831e5b313351db6761a80b612fcf
Leandro Rodrigues <leandro.rodrigues@zup.com.br>
2021-03-08 15:13:17 -0300

    Create workspace fixture
    Signed-off-by: Leandro Rodrigues <leandro.rodrigues@zup.com.br>

--- moove/infrastructure/src/test/groovy/io/charlescd/moove/fixture/Fixtures.groovy
+++ moove/infrastructure/src/test/groovy/io/charlescd/moove/fixture/Fixtures.groovy
@@ -3,4 +3,6 @@ package io.charlescd.moove.fixture
 class Fixtures {
 
     static UserFixture user() { UserFixture.create() }
+
+    static WorkspaceFixture workspace() { WorkspaceFixture.create() }
 }
