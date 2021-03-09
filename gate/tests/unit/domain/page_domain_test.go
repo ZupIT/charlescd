013766295743790e9963169b28a04a3cf76e473a
Pedro Borges <pedromcborges11@gmail.com>
2021-03-09 11:07:41 -0300

    tests
    Signed-off-by: Pedro Borges <pedromcborges11@gmail.com>

--- gate/tests/unit/domain/page_domain_test.go
+++ gate/tests/unit/domain/page_domain_test.go
@@ -29,6 +29,22 @@ func (d *DomainSuite) TestPageEmpty() {
 	require.Equal(d.T(), true, isLast)
 }
 
+func (d *DomainSuite) TestPageFillDefaults() {
+	d.page = getDummyPage(0, 0, "", 50)
+
+	d.page.FillDefaults()
+
+	totalPages := d.page.TotalPages()
+	offset := d.page.Offset()
+	isLast := d.page.IsLast()
+
+	require.Equal(d.T(), 3, totalPages)
+	require.Equal(d.T(), 0, offset)
+	require.Equal(d.T(), false, isLast)
+	require.Equal(d.T(), 20, d.page.PageSize)
+	require.Equal(d.T(), "created_at desc", d.page.Sort)
+}
+
 func getDummyPage(pageNumber int, pageSize int, sort string, total int64) domain.Page {
 	return domain.Page{
 		PageNumber: pageNumber,
