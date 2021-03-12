9150f21633eb6e17088c246d29aadef8933379a8
pedroborgeszup <pedro.borges@zup.com.br>
2021-03-12 12:06:31 -0300

    removing error treatment when .env not found
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- gate/internal/configuration/configuration.go
+++ gate/internal/configuration/configuration.go
@@ -1,18 +1,8 @@
 package configuration
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 )
-
-func LoadConfigurations() error {
-	err := godotenv.Load("./resources/.env")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func Get(name string) string {
 	return os.Getenv(name)
