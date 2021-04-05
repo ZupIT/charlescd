2776d83641ae57285439dd4641f9b1125c42d4c0
pedroborgeszup <pedro.borges@zup.com.br>
2021-04-05 17:35:03 -0300

    removing unused func
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- gate/internal/use_case/authorization/authorize_input.go
+++ gate/internal/use_case/authorization/authorize_input.go
@@ -1,17 +1,6 @@
 package authorization
 
-import (
-	"github.com/ZupIT/charlescd/gate/internal/domain"
-)
-
 type Input struct {
 	Path   string
 	Method string
-}
-
-func (input Input) InputToDomain() domain.Authorization {
-	return domain.Authorization{
-		Path:   input.Path,
-		Method: input.Method,
-	}
 }
