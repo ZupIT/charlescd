212e3d1c76e22af1eb958071b130692ef23709ec
pedroborgeszup <pedro.borges@zup.com.br>
2021-04-12 11:47:34 -0300

    UserGroup Author to uuid
    Signed-off-by: pedroborgeszup <pedro.borges@zup.com.br>

--- gate/internal/repository/models/user_group.go
+++ gate/internal/repository/models/user_group.go
@@ -1,11 +1,14 @@
 package models
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type UserGroup struct {
 	Id        string
 	Name      string
-	Author    User `gorm:"column:user_id"`
+	Author    uuid.UUID `gorm:"column:user_id"`
 	CreatedAt *time.Time
 	Users     []User `gorm:"many2many:user_groups_users;"`
 }
