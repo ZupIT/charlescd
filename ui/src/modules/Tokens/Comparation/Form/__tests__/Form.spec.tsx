be7197ac71271a65c28e8e48ed4ea477175f8bab
Douglas Fernandes <douglas.fernandes@zup.com.br>
2021-04-14 10:05:44 -0300

    adding mode prop

--- ui/src/modules/Tokens/Comparation/Form/__tests__/Form.spec.tsx
+++ ui/src/modules/Tokens/Comparation/Form/__tests__/Form.spec.tsx
@@ -35,21 +35,21 @@ const token: Token = {
 };
 
 test('Render Token Form in view mode and show Author', async () => {
-  render(<Form data={token} />);
+  render(<Form mode="view" data={token} />);
 
   const TokenAuthor = await screen.findByText(`Created by ${token.author}`);
   expect(TokenAuthor).toBeInTheDocument();
 });
 
 test('Render Token Form in view mode and token never used', async () => {
-  render(<Form data={token} />);
+  render(<Form mode="view" data={token} />);
 
   const MessageTokenNotUsedYet = await screen.findByText('This token has not been used yet.');
   expect(MessageTokenNotUsedYet).toBeInTheDocument();
 });
 
 test('Render Token Form in view mode and token have been used', async () => {
-  render(<Form data={{ ...token, last_used_at: '2021-04-12T22:16:26.359112Z'} } />);
+  render(<Form mode="view" data={{ ...token, last_used_at: '2021-04-12T22:16:26.359112Z'} } />);
 
   const MessageTokenNotUsedYet = await screen.findByText('Last used at 12/04/2021 • 19:16:26');
   expect(MessageTokenNotUsedYet).toBeInTheDocument();
@@ -57,7 +57,7 @@ test('Render Token Form in view mode and token have been used', async () => {
 
 test('Render Token Form in create mode', async () => {
   (fetch as FetchMock).mockResponseOnce(JSON.stringify({ name: 'token' }));
-  render(<Form />);
+  render(<Form mode="create" />);
 
   const ContentToken = await screen.findByTestId('contentIcon-token');
   const ContentWorkspaces = screen.queryByTestId('contentIcon-workspaces');
