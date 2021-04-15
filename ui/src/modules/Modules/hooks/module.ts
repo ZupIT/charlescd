cf731c7d11aac4c55bba822a36db434ff8e63381
Leandro Queiroz <leandroqueiroz92@gmail.com>
2021-04-15 11:03:31 -0300

    update the list of modules when editing
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- ui/src/modules/Modules/hooks/module.ts
+++ ui/src/modules/Modules/hooks/module.ts
@@ -206,6 +206,7 @@ export const useUpdateModule = (): {
       setModuleId(id);
       try {
         await updateModulePromise(id, module);
+        dispatch(resetModulesAction());
         getAllModules();
         setStatus('resolved');
       } catch (error) {
