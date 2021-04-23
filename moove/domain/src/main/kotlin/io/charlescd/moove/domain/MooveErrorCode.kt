653d44f9cd2ceb0e1c3e301e0d0b3105d8538495
Mateus Cruz <emiteze@hotmail.com>
2021-04-23 14:33:42 -0300

    Adding error code and messages for invalid butler url
    Signed-off-by: Mateus Cruz <emiteze@hotmail.com>

--- moove/domain/src/main/kotlin/io/charlescd/moove/domain/MooveErrorCode.kt
+++ moove/domain/src/main/kotlin/io/charlescd/moove/domain/MooveErrorCode.kt
@@ -54,6 +54,7 @@ enum class MooveErrorCode(val key: String) {
     USER_ALREADY_DISASSOCIATED("user.already.disassociated"),
     MISSING_DEFAULT_CIRCLE("missing.default.circle"),
     INVALID_CIRCLE_MATCHER_URL_ERROR("invalid.circle.matcher.url.error"),
+    INVALID_BUTLER_URL_ERROR("invalid.butler.url.error"),
     MODULE_MUST_HAVE_AT_LEAST_ONE_COMPONENT("module.must.have.at.least.one.component"),
     CREATE_USER_ERROR_EMAIL_ALREADY_EXISTS("create.user.error.email.already.exists"),
     USER_PASSWORD_DOES_NOT_MATCH("user.password.does.not.match"),
