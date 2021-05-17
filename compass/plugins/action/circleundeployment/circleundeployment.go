697d5a3c4a9f3274502e17fac08a8abd51c426e8
Gabriel Leite <gabriel.leite@zup.com.br>
2021-05-17 18:42:01 -0300

    Plugin done, next step is to test...
    Signed-off-by: Gabriel Leite <gabriel.leite@zup.com.br>

---
+++ compass/plugins/action/circleundeployment/circleundeployment.go
@@ -0,0 +1,118 @@
+/*
+ *
+ *  Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package circleundeployment
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/ZupIT/charlescd/compass/pkg/logger"
+	"strings"
+)
+
+type executionConfiguration struct {
+	CircleId    string `json:"circleId"`
+	WorkspaceID string `json:"workspaceId"`
+}
+
+type actionConfiguration struct {
+	MooveURL string `json:"mooveUrl"`
+}
+
+func Do(actionConfig []byte, executionConfig []byte) error {
+	var ac *actionConfiguration
+	err := json.Unmarshal(actionConfig, &ac)
+	if err != nil {
+		logger.Error("ACTION_PARSE_ERROR", "DoUndeploymentAction", err, fmt.Sprintf("ActionConfig: %s", string(actionConfig)))
+		return err
+	}
+
+	var ec *executionConfiguration
+	err = json.Unmarshal(executionConfig, &ec)
+	if err != nil {
+		logger.Error("EXECUTION_PARSE_ERROR", "DoUndeploymentAction", err, fmt.Sprintf("ExecutionConfig: %s", string(executionConfig)))
+		return err
+	}
+
+	deployment, err := getCurrentDeploymentAtCircle(ec.CircleId, ec.WorkspaceID, ac.MooveURL)
+	if err != nil {
+		dataErr := fmt.Sprintf("MooveUrl: %s, CircleId: %s, WorkspaceId: %s", ac.MooveURL, ec.CircleId, ec.WorkspaceID)
+		logger.Error("DO_CIRCLE_GET", "DoDeploymentAction", err, dataErr)
+		return err
+	}
+
+	if deployment.BuildId == "" {
+		err = errors.New("circle has no active build")
+		dataErr := fmt.Sprintf("CircleId: %s, WorkspaceId: %s", ec.CircleId, ec.WorkspaceID)
+		logger.Error("DO_CIRCLE_GET", "DoUndeploymentAction", err, dataErr)
+		return err
+	}
+
+	err = undeployBuildAtCircle(deployment.Id, ec.WorkspaceID, ac.MooveURL)
+	if err != nil {
+		dataErr := fmt.Sprintf("MooveUrl: %s, WorkspaceId: %s, CircleId: %s, BuildId: %s",
+			ac.MooveURL, ec.WorkspaceID, ec.CircleId, deployment.BuildId)
+		logger.Error("DO_CIRCLE_UNDEPLOYMENT", "DoDeploymentAction", err, dataErr)
+		return err
+	}
+
+	return nil
+}
+
+func ValidateExecutionConfiguration(executionConfig []byte) []error {
+	errs := make([]error, 0)
+	var config executionConfiguration
+	err := json.Unmarshal(executionConfig, &config)
+	if err != nil {
+		logger.Error("VALIDATE_CIRCLE_ACTION_EXECUTION_CONFIG", "ValidateExecutionConfiguration", err, nil)
+		return append(errs, errors.New("error validating execution configuration"))
+	}
+
+	if strings.TrimSpace(config.CircleId) == "" {
+		errs = append(errs, errors.New("circle id is required"))
+	}
+
+	if strings.TrimSpace(config.WorkspaceID) == "" {
+		errs = append(errs, errors.New("workspace id is required"))
+	}
+
+	return errs
+}
+func ValidateActionConfiguration(actionConfig []byte) []error {
+	errs := make([]error, 0)
+	var config actionConfiguration
+	err := json.Unmarshal(actionConfig, &config)
+	if err != nil {
+		logger.Error("VALIDATE_CIRCLE_ACTION_CONFIG", "ValidateActionConfiguration", err, nil)
+		return append(errs, errors.New("error validating action configuration"))
+	}
+
+	if strings.TrimSpace(config.MooveURL) == "" {
+		logger.Error("VALIDATE_CIRCLE_ACTION_CONFIG", "ValidateActionConfiguration", err, fmt.Sprintf("%+v", config))
+		errs = append(errs, errors.New("moove url is required"))
+	}
+
+	err = testConnection(config.MooveURL)
+	if err != nil {
+		logger.Error("VALIDATE_CIRCLE_ACTION_CONFIG", "ValidateActionConfiguration", err, fmt.Sprintf("%+v", config))
+		errs = append(errs, errors.New("moove could not be reached"))
+	}
+
+	return errs
+}
