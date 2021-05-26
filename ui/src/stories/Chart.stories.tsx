f7178e7a8ff6017f12eb5dfb8d1f9001b8575484
Katanant <bruno.catanant@zup.com.br>
2021-05-26 17:48:14 -0300

    new charts
    Signed-off-by: Katanant <bruno.catanant@zup.com.br>

---
+++ ui/src/stories/Chart.stories.tsx
@@ -0,0 +1,30 @@
+import { Story } from '@storybook/react';
+import Chart, { Props } from 'core/components/Chart';
+
+export default {
+  title: 'Components/Chart',
+  component: Chart,
+};
+
+const Template: Story<Props> = (props: Props) => <Chart {...props} />;
+export const chart = Template.bind({});
+chart.args = {
+  series: [{
+    name: 'test 1',
+    data: [
+      {
+        y: 10,
+        x: 1000
+      }
+    ]
+    }, {
+    name: 'test 2',
+    data: [
+      {
+        y: 5,
+        x: 500
+      }
+    ]
+  }],
+  type: 'bar'
+};
