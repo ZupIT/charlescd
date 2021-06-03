502e0d800084a66cb47374ddbbbdb7dd27f22ea5
Luane Aquino <luane.aquino.br@gmail.com>
2021-06-03 10:23:23 -0300

    undo delete chart file
    Signed-off-by: Luane Aquino <luane.aquino.br@gmail.com>

---
+++ ui/src/stories/Chart.stories.tsx
@@ -0,0 +1,32 @@
+import { Story } from '@storybook/react';
+import Chart, { Props } from 'core/components/Chart';
+
+export default {
+  title: 'Components/Chart',
+  component: Chart,
+};
+
+const Template: Story<Props> = (props: Props) => <Chart {...props} />;
+export const chart = Template.bind({});
+chart.args = {
+  series: [{
+    name: 'test 1',
+    data: [
+      {
+        x: 1000,
+        y: 10
+      }
+    ]
+    }, {
+    name: 'test 2',
+    data: [
+      {
+        x: 500,
+        y: 5
+      }
+    ]
+  }],
+  type: 'bar',
+  width: 700,
+  height: 200
+};
