d1f2175a7476eecb3b932432e9137fab523a7eeb
Douglas Fernandes <ifdouglas@icloud.com>
2021-06-08 10:46:10 -0300

    Remove React import
    Signed-off-by: Douglas Fernandes <ifdouglas@icloud.com>

--- ui/src/core/components/Form/Link/index.tsx
+++ ui/src/core/components/Form/Link/index.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import Styled from './styled';
 
 interface Props {
--- ui/src/core/components/Link/index.tsx
+++ ui/src/core/components/Link/index.tsx
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import Styled from './styled';
 
 export interface Props {
   children: ReactNode;
   href: string;
   isDisabled?: boolean;
-  target?: string;
+  target?: '_blank' | '_self';
 }
 
 const Link = ({
