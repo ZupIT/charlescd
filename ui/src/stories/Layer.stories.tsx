5ae01b13a42c9a9ead2e5b27e2743165f6cc487b
Luane Aquino <luane.cavalcanti@zup.com.br>
2021-06-16 11:10:17 -0300

    adjust story
    Signed-off-by: Luane Aquino <luane.cavalcanti@zup.com.br>

--- ui/src/stories/Layer.stories.tsx
+++ ui/src/stories/Layer.stories.tsx
@@ -1,13 +1,27 @@
-import { Story } from '@storybook/react';
+import { Story, Meta } from '@storybook/react';
 import Layer, { Props } from 'core/components/Layer';
 
 export default {
   title: 'Components/Layer',
   component: Layer,
-};
+  decorators: [
+    (Story) => (
+      <div style={{ backgroundColor: '#1c1c1e', color: '#98989e', width: '200px', height: '100px', padding: '10px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Wrapper that adds spacing and animation to a children content.',
+      },
+    },
+  },
+} as Meta;
 
 const Template: Story<Props> = (props: Props) => <Layer {...props} />;
 export const layer = Template.bind({});
 layer.args = {
-  children: 'My content here',
+  children: 'my content inside Layer',
 };
