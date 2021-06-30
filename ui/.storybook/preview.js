44959adc3d41de961a66375d3c71cdf3a74090dc
Luane Aquino <luane.cavalcanti@zup.com.br>
2021-06-30 18:02:37 -0300

    set background dark
    Signed-off-by: Luane Aquino <luane.cavalcanti@zup.com.br>

--- ui/.storybook/preview.js
+++ ui/.storybook/preview.js
@@ -1,7 +1,32 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { ThemeProvider } from 'styled-components';
 import THEME from 'core/assets/themes';
 
 export const parameters = {
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#1C1C1E',
+      },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -9,7 +34,7 @@ export const parameters = {
     },
   },
   layout: 'centered',
-}
+};
 
 export const decorators = [
   (Story) => (
