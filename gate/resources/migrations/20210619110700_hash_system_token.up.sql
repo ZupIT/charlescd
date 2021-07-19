f43e9e3b00c0d8d11338dd01b6fbfd5e5b2744b0
Leandro Queiroz <leandroqueiroz92@gmail.com>
2021-07-19 13:50:16 -0300

    add hash token migration
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

---
+++ gate/resources/migrations/20210619110700_hash_system_token.up.sql
@@ -0,0 +1,11 @@
+ALTER TABLE system_tokens
+	ADD token_hash VARCHAR(64);
+	
+UPDATE system_tokens
+	SET token_hash = encode(sha256(pgp_sym_decrypt(token, '64971923d21a4887a3acf1ad15961bbc', 'cipher-algo=aes256')::bytea), 'hex');
+
+ALTER TABLE system_tokens
+	DROP COLUMN token;
+
+ALTER TABLE system_tokens
+	RENAME COLUMN token_hash TO token;
