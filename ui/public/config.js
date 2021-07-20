7784b2a5d4cb270b35f1aebb10dab7504d29420c
Luane Aquino <luane.cavalcanti@zup.com.br>
2021-07-20 21:41:22 +0900

    resolving dco sign off
    Signed-off-by: Luane Aquino <luane.cavalcanti@zup.com.br>

---
+++ ui/public/config.js
@@ -0,0 +1,1 @@
+window.CHARLESCD_ENVIRONMENT = {PUBLIC_URL:"/",REACT_APP_API_URI:"https://batman.continuousplatform.com",REACT_APP_AUTH_URI:"https://batman.continuousplatform.com./keycloak",REACT_APP_AUTH_REALM:"charlescd",REACT_APP_AUTH_CLIENT_ID:"charlescd-client",REACT_APP_WORKSPACE_ID:"792c69c4-9f58-4236-9936-ffcb04bda011",REACT_APP_MOCK:"0",}
