0ceed86e0d157e56d0a99772d3d781292ca10035
Leandro Queiroz <leandroqueiroz92@gmail.com>
2021-07-20 14:34:10 -0300

    update hash migration
    Signed-off-by: Leandro Queiroz <leandroqueiroz92@gmail.com>

--- gate/resources/migrations/20210620143200_hash_system_token.up.sql
+++ gate/resources/migrations/20210620143200_hash_system_token.up.sql
@@ -5,7 +5,7 @@ UPDATE system_tokens
 	SET token_hash = encode(sha256(pgp_sym_decrypt(token, '64971923d21a4887a3acf1ad15961bbc', 'cipher-algo=aes256')::bytea), 'hex');
 
 ALTER TABLE system_tokens
-	DROP COLUMN token;
+	RENAME COLUMN token TO token_old;
 
 ALTER TABLE system_tokens
 	RENAME COLUMN token_hash TO token;
