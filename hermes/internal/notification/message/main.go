5d277f1b9443d46030a748e283fe3229a95630da
thalles <thalles.freitas@zup.com.br>
2021-07-21 16:33:38 -0300

    improve response on subscription publish
    Signed-off-by: thalles <thalles.freitas@zup.com.br>

--- hermes/internal/notification/message/main.go
+++ hermes/internal/notification/message/main.go
@@ -31,7 +31,7 @@ import (
 type UseCases interface {
 	ParsePayload(request io.ReadCloser) (payloads.PayloadRequest, errors.Error)
 	Validate(message payloads.PayloadRequest) errors.ErrorList
-	Publish(messagesRequest []payloads.Request) ([]payloads.MessageResponse, errors.Error)
+	Publish(messagesRequest []payloads.Request) ([]payloads.FullMessageResponse, errors.Error)
 	FindAllBySubscriptionIdAndFilter(subscriptionId uuid.UUID, parameters map[string]string, page int, size int) ([]payloads.FullMessageResponse, errors.Error)
 	FindAllNotEnqueued() ([]payloads.MessageResponse, errors.Error)
 	FindMostRecent(subscriptionId uuid.UUID) (payloads.StatusResponse, errors.Error)
@@ -42,6 +42,7 @@ type Main struct {
 	amqpClient    *rabbitclient.Client
 	executionMain messageexecutionhistory.UseCases
 }
+
 
 func NewMain(db *gorm.DB, amqpClient *rabbitclient.Client, executionMain messageexecutionhistory.UseCases) UseCases {
 	return Main{db, amqpClient, executionMain}
