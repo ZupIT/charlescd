d4c44902bc34777236b463ecf53d88110b29e773
Luane Aquino <luane.cavalcanti@zup.com.br>
2021-07-28 09:02:14 +0900

    remove unnecessary code
    Signed-off-by: Luane Aquino <luane.cavalcanti@zup.com.br>

--- ui/src/modules/Circles/Comparation/Item/Layer/__tests__/MetricsGroups.spec.tsx
+++ ui/src/modules/Circles/Comparation/Item/Layer/__tests__/MetricsGroups.spec.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import { render, screen, act } from 'unit-test/testUtils';
 import userEvent from '@testing-library/user-event';
 import { circleData, newCircleData } from './fixtures';
