44062f9e7e6fa78711d31b2f19e191645d5e1b08
guilhermemartinsdeoliveirazup <guilherme.oliveira@zup.com.br>
2021-09-08 16:19:53 -0300

    Invalid permissions message fix
    Signed-off-by: guilhermemartinsdeoliveirazup <guilherme.oliveira@zup.com.br>

--- gate/hack/sql/create_tables.sql
+++ gate/hack/sql/create_tables.sql
@@ -22,5 +22,5 @@ INSERT INTO permissions(id, name, created_at) values('09ae6a9a-45d6-44de-8b3a-5a
 INSERT INTO permissions(id, name, created_at) values('7cc71f53-2054-4193-836d-6177fa9bbb48','deploy_write', now());
 INSERT INTO permissions(id, name, created_at) values('70f30948-d8fc-448a-9e3e-050c16a4eb46','circle_read', now());
 INSERT INTO permissions(id ,name, created_at) values('d138ff33-0fb8-4ebf-8c8c-20d61067b183', 'circle_write', now());
-insert into users(id, name, photo_url, email, created_at, is_root) values('c7e6dafe-aa7a-4536-be1b-34eaad4c2915', 'Charles Admin', null, 'charlesadmin@zup.com.br', now(), true);
+insert into users(id, name, photo_url, email, created_at, is_root) values('c7e6dafe-aa7a-4536-be1b-34eaad4c2915', 'Charles Admin', null, 'charlesadmin@admin', now(), true);
 INSERT INTO workspaces(id ,name) values('d138ff33-0fb8-4ebf-8c8c-20d61067b183', 'charles');
