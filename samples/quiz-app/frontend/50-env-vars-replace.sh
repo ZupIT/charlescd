43c4e086a13651a3be8eb35c7ad8caeeea49e7df
Lucas <lucas.machado@zup.com.br>
2021-10-07 14:37:31 -0300

    update env script
    Signed-off-by: Lucas <lucas.machado@zup.com.br>

--- samples/quiz-app/frontend/50-env-vars-replace.sh
+++ samples/quiz-app/frontend/50-env-vars-replace.sh
@@ -12,8 +12,7 @@ QUESTIONS_URL_ENV=${QUESTIONS_URL}
 
 cd /usr/share/nginx/html
 
-grep -rli 'PUBLIC_URL_REPLACE' * | xargs -i@ sed -i "s/PUBLIC_URL_REPLACE/${PUBLIC_URL_ENV}/g" @
-grep -rli 'MATCHER_URL_REPLACE' * | xargs -i@ sed -i "s/MATCHER_URL_REPLACE/${MATCHER_URL_ENV}/g" @
-grep -rli 'QUESTIONS_URL_REPLACE' * | xargs -i@ sed -i "s/QUESTIONS_URL_REPLACE/${QUESTIONS_URL_ENV}/g" @
-grep -rli 'UUID_REPLACE' * | xargs -i@ sed -i "s/UUID_REPLACE/${WORKSPACE_ID_ENV}/g" @
-
+grep -rli 'PUBLIC_URL_REPLACE' * | xargs -i@ sed -i "s|PUBLIC_URL_REPLACE|$PUBLIC_URL_ENV|g" @
+grep -rli 'MATCHER_URL_REPLACE' * | xargs -i@ sed -i "s|MATCHER_URL_REPLACE|$MATCHER_URL_ENV|g" @
+grep -rli 'QUESTIONS_URL_REPLACE' * | xargs -i@ sed -i "s|QUESTIONS_URL_REPLACE|$QUESTIONS_URL_ENV|g" @
+grep -rli 'UUID_REPLACE' * | xargs -i@ sed -i "s|UUID_REPLACE|$WORKSPACE_ID_ENV|g" @
