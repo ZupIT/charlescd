ab3e860a7f748bdf79563c8c1107844e33020657
Lucas <lucas.machado@zup.com.br>
2021-10-07 13:41:14 -0300

    added env in quiz-app-frontend
    Signed-off-by: Lucas <lucas.machado@zup.com.br>

---
+++ samples/quiz-app/frontend/50-env-vars-replace.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+: "${PUBLIC_URL}" # ensure API_HOST exists and exit otherwise
+: "${MATCHER_URL}" # ensure API_HOST exists and exit otherwise
+: "${WORKSPACE_ID}" # ensure API_HOST exists and exit otherwise
+: "${QUESTIONS_URL}" # ensure API_HOST exists and exit otherwise
+
+PUBLIC_URL_ENV="${PUBLIC_URL}"
+MATCHER_URL_ENV="${MATCHER_URL}"
+WORKSPACE_ID_ENV="${WORKSPACE_ID}"
+QUESTIONS_URL_ENV="${QUESTIONS_URL}"
+
+cd /usr/share/nginx/html
+
+grep -rli 'PUBLIC_URL_REPLACE' * | xargs -i@ sed -i "s/PUBLIC_URL_REPLACE/${PUBLIC_URL_ENV}/g" @
+grep -rli 'MATCHER_URL_REPLACE' * | xargs -i@ sed -i "s/MATCHER_URL_REPLACE/${MATCHER_URL_ENV}/g" @
+grep -rli 'QUESTIONS_URL_REPLACE' * | xargs -i@ sed -i "s/QUESTIONS_URL_REPLACE/${QUESTIONS_URL_ENV}/g" @
+grep -rli 'UUID_REPLACE' * | xargs -i@ sed -i "s/UUID_REPLACE/${WORKSPACE_ID_ENV}/g" @
+
