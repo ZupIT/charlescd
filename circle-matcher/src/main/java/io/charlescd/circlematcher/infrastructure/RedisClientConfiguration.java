cee10065366934ba4eae615385646163f5e316d0
guilhermemartinsdeoliveirazup <guilherme.oliveira@zup.com.br>
2021-12-07 11:43:25 -0300

    lexicographical fix on imports
    Signed-off-by: guilhermemartinsdeoliveirazup <guilherme.oliveira@zup.com.br>

--- circle-matcher/src/main/java/io/charlescd/circlematcher/infrastructure/RedisClientConfiguration.java
+++ circle-matcher/src/main/java/io/charlescd/circlematcher/infrastructure/RedisClientConfiguration.java
@@ -22,11 +22,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisClientConfiguration {
